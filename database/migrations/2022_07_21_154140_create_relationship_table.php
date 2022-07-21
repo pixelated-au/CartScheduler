234216b72cddb8c8e93e0ca61e06fb4b893e9055
ian <ian@pixelated.com.au>
2022-07-21 15:52:01 +1000

    Create the relationship table

---
+++ database/migrations/2022_07_21_154140_create_relationship_table.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Project: ${PROJECT_NAME}
+ * Owner: Pixelated
+ * Copyright: 2022
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('relationship', static function (Blueprint $table) {
+            $table->unsignedBigInteger('user1_id');
+            $table->unsignedBigInteger('user2_id');
+
+            $table->foreign('user1_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->foreign('user2_id')->references('id')->on('users')->cascadeOnDelete();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('relationship');
+    }
+};
