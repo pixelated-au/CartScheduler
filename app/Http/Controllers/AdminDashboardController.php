ce240f1970d8e6dd8f4353a63efa088c939e6841
ian <ian@pixelated.com.au>
2022-08-03 15:53:35 +1000

    Fixed permissions

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\Role;
 use App\Models\Location;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class AdminDashboardController extends Controller
 {
     public function __invoke()
     {
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+        if ($user->role !== Role::Admin->value) {
+            abort(403);
+        }
+
         return Inertia::render('Admin/Dashboard', [
             'totalUsers'     => User::all()->count(),
             'totalLocations' => Location::all()->count(),
