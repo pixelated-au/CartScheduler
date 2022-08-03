cf4975c065cc17d9352a886cc086e3095ee82d0c
ian <ian@pixelated.com.au>
2022-08-03 14:57:37 +1000

    Some textual changes

--- resources/views/emails/user-account-created.blade.php
+++ resources/views/emails/user-account-created.blade.php
@@ -3,13 +3,13 @@
 
 # {{ config('app.name') }} Account Created
 
-An account has been created for you on the {{ config('app.name') }} Public Witnessing website.
+Dear {{$user->name}}, an account has been created for you on the {{ config('app.name') }} Public Witnessing website.
 
 If you feel this is an error, please [contact us immediately](mailto:{{ config('mail.support.address') }}?subject=Mistaken%20User%20Account). Otherwise, please use the verification link below.
 
 @component('mail::button', ['url' => config('app.url') . '/set-password/' . $user->id . '/' . $hashedEmail])
-    Verify Account
+    Confirm Account
 @endcomponent
 Thank you,<br>
-{{ config('app.name') }}
+The {{ config('app.name') }} Team
 @endcomponent
