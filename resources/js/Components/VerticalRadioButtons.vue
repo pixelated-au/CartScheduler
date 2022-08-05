bf74c8cb5e0aa667fae89a04a97dd1250fc67985
ian <ian@pixelated.com.au>
2022-08-05 12:33:16 +1000

    Fixed some formatting

--- resources/js/Components/VerticalRadioButtons.vue
+++ resources/js/Components/VerticalRadioButtons.vue
@@ -29,9 +29,11 @@
 
 <template>
     <!--                <ul class="w-48 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white">-->
-    <ul class="w-48 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200">
+    <ul class="w-48 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 pl-0">
         <!--                    <li class="w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">-->
-        <li v-for="option in options" :key="option.value" class="w-full rounded-t-lg border-b border-gray-200">
+        <li v-for="option in options"
+            :key="option.value"
+            class="w-full rounded-t-lg border-b border-gray-200 list-none">
             <div class="flex items-center pl-3">
                 <!--                                   class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">-->
                 <input type="radio"
