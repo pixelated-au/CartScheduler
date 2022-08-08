ad455fc306777f20ef2ef3fff469a860aedf4174
ian <ian@pixelated.com.au>
2022-08-08 14:10:20 +1000

    Added in availability

--- app/Http/Resources/ShiftResource.php
+++ app/Http/Resources/ShiftResource.php
@@ -16,6 +16,8 @@ use Illuminate\Http\Resources\Json\JsonResource;
  * @property mixed day_sunday
  * @property mixed start_time
  * @property mixed end_time
+ * @property mixed available_from
+ * @property mixed available_to
  * @property mixed is_enabled
  */
 class ShiftResource extends JsonResource
@@ -23,18 +25,20 @@ class ShiftResource extends JsonResource
     public function toArray($request): array
     {
         return [
-            'id'            => $this->id,
-            'location_id'   => $this->location_id,
-            'day_monday'    => $this->day_monday,
-            'day_tuesday'   => $this->day_tuesday,
-            'day_wednesday' => $this->day_wednesday,
-            'day_thursday'  => $this->day_thursday,
-            'day_friday'    => $this->day_friday,
-            'day_saturday'  => $this->day_saturday,
-            'day_sunday'    => $this->day_sunday,
-            'start_time'    => $this->start_time,
-            'end_time'      => $this->end_time,
-            'is_enabled'    => $this->is_enabled,
+            'id'             => $this->id,
+            'location_id'    => $this->location_id,
+            'day_monday'     => $this->day_monday,
+            'day_tuesday'    => $this->day_tuesday,
+            'day_wednesday'  => $this->day_wednesday,
+            'day_thursday'   => $this->day_thursday,
+            'day_friday'     => $this->day_friday,
+            'day_saturday'   => $this->day_saturday,
+            'day_sunday'     => $this->day_sunday,
+            'start_time'     => $this->start_time,
+            'end_time'       => $this->end_time,
+            'available_from' => $this->available_from,
+            'available_to'   => $this->available_to,
+            'is_enabled'     => $this->is_enabled,
         ];
     }
 }
