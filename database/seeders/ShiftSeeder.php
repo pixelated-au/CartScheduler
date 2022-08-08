0496851e2afd9fc54cd5135326565f16b3e03a25
ian <ian@pixelated.com.au>
2022-08-08 14:10:10 +1000

    Fixed the seeder

--- database/seeders/ShiftSeeder.php
+++ database/seeders/ShiftSeeder.php
@@ -15,19 +15,25 @@ class ShiftSeeder extends Seeder
 
         foreach ($locations as $location) {
             Shift::factory()->create([
-                'location_id' => $location->id,
-                'start_time'  => '09:00:00',
-                'end_time'    => '12:00:00',
+                'location_id'    => $location->id,
+                'start_time'     => '09:00:00',
+                'end_time'       => '12:00:00',
+                'available_from' => null,
+                'available_to'   => null,
             ]);
             Shift::factory()->create([
-                'location_id' => $location->id,
-                'start_time'  => '12:00:00',
-                'end_time'    => '15:00:00',
+                'location_id'    => $location->id,
+                'start_time'     => '12:00:00',
+                'end_time'       => '15:00:00',
+                'available_from' => null,
+                'available_to'   => null,
             ]);
             Shift::factory()->create([
-                'location_id' => $location->id,
-                'start_time'  => '15:00:00',
-                'end_time'    => '18:00:00',
+                'location_id'    => $location->id,
+                'start_time'     => '15:00:00',
+                'end_time'       => '18:00:00',
+                'available_from' => null,
+                'available_to'   => null,
             ]);
             if (!$temporaryShift) {
                 $temporaryShift = true;
