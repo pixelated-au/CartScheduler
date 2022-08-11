f12a51a603d01bbddcb599adb80ec957bff443a7
ian <ian@pixelated.com.au>
2022-08-12 09:06:17 +1000

    Disabled the reservations table

--- database/migrations/2022_07_21_160640_create_reservations_table.php
+++ database/migrations/2022_07_21_160640_create_reservations_table.php
@@ -1,29 +1,30 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
+/**
+ * @deprecated (I think)
+ */
 return new class extends Migration {
     public function up(): void
     {
-        Schema::create('reservations', static function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('shift_id')->constrained();
-            $table->unsignedBigInteger('publisher_1_id');
-            $table->unsignedBigInteger('publisher_2_id');
-            $table->unsignedBigInteger('publisher_3_id');
-            $table->timestamps();
-
-            $table->foreign('publisher_1_id')->references('id')->on('users')->restrictOnDelete();
-            $table->foreign('publisher_2_id')->references('id')->on('users')->restrictOnDelete();
-            $table->foreign('publisher_3_id')->references('id')->on('users')->restrictOnDelete();
-
-        });
+        //Schema::create('reservations', static function (Blueprint $table) {
+        //    $table->id();
+        //    $table->foreignId('shift_id')->constrained();
+        //    $table->unsignedBigInteger('publisher_1_id');
+        //    $table->unsignedBigInteger('publisher_2_id');
+        //    $table->unsignedBigInteger('publisher_3_id');
+        //    $table->timestamps();
+        //
+        //    $table->foreign('publisher_1_id')->references('id')->on('users')->restrictOnDelete();
+        //    $table->foreign('publisher_2_id')->references('id')->on('users')->restrictOnDelete();
+        //    $table->foreign('publisher_3_id')->references('id')->on('users')->restrictOnDelete();
+        //
+        //});
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('reservations');
+        //Schema::dropIfExists('reservations');
     }
 };
