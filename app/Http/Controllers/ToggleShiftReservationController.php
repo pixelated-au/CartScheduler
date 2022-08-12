985cc2e227eebe02e3867102669ebe0307d5c6fc
ian <ian@pixelated.com.au>
2022-08-12 23:53:36 +1000

    Is now not allowing duplicate shifts to occur

--- app/Http/Controllers/ToggleShiftReservationController.php
+++ app/Http/Controllers/ToggleShiftReservationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Actions\ErrorApiResource;
 use App\Models\Location;
 use App\Models\Shift;
+use App\Models\ShiftUser;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Http\Request;
@@ -15,17 +16,7 @@ class ToggleShiftReservationController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $data = $this->validate($request, [
-            'location'   => ['required', 'integer', 'exists:locations,id'],
-            'shift'      => ['required', 'integer', 'exists:shifts,id'],
-            'do_reserve' => ['required', 'boolean'],
-            'date'       => [
-                'required',
-                'date',
-                'after_or_equal:today',
-                'before_or_equal:last day of next month'
-            ],
-        ]);
+        $data = $this->getValidated($request);
 
         $shiftDate = Carbon::parse($data['date']);
 
@@ -65,5 +56,42 @@ class ToggleShiftReservationController extends Controller
         $shift->users()->attach($request->user(), ['shift_date' => $shiftDate]);
 
         return response(200);
+    }
+
+    protected function getValidated(Request $request): array
+    {
+        return $this->validate($request, [
+            'location'   => ['required', 'integer', 'exists:locations,id'],
+            'shift'      => [
+                'required',
+                'integer',
+                'exists:shifts,id',
+                function ($attribute, $value, $fail) use ($request) {
+                    $data = $request->all();
+                    if ($data['do_reserve']) {
+                        $shiftDate           = Carbon::parse($data['date']);
+                        $currentShiftsOnDate = ShiftUser::with(['shift', 'shift.location'])
+                                                        ->where('user_id', $request->user()->id)
+                                                        ->where('shift_date', $shiftDate->toDateString())
+                                                        ->get();
+                        // only validate if adding a shift
+                        $theShift   = Shift::find($data['shift']);
+                        $shiftStart = $theShift->start_time;
+                        $shiftEnd   = $theShift->end_time;
+                        $foundMatch = $currentShiftsOnDate->first(fn(ShiftUser $currentShift) => $currentShift->shift->start_time >= $shiftStart && $currentShift->shift->end_time <= $shiftEnd);
+                        if ($foundMatch) {
+                            $fail("Sorry, You already have another shift at this time on this day at {$foundMatch->shift->location->name}.");
+                        }
+                    }
+                }
+            ],
+            'do_reserve' => ['required', 'boolean'],
+            'date'       => [
+                'required',
+                'date',
+                'after_or_equal:today',
+                'before_or_equal:last day of next month',
+            ],
+        ]);
     }
 }
