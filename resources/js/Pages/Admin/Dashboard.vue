2995e566fc22642f740f94c937cd2ec55e3721ed
ian <ian@pixelated.com.au>
2022-09-20 14:19:32 +1000

    Text update

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -17,7 +17,8 @@
 <template>
     <AppLayout title="Dashboard">
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Admin Dashboard</h2>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                Administration Dashboard </h2>
         </template>
 
         <div class="py-2 sm:py-12">
