a0829a9076408d051d1e50a6e0c2e067bca3b593
ian <ian@pixelated.com.au>
2022-09-22 09:43:02 +1000

    Adding the ability to force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -28,5 +29,8 @@ class AppServiceProvider extends ServiceProvider
         Model::preventLazyLoading(!app()->isProduction());
         Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
         Schema::defaultStringLength(191);
+        if (config('app.is_https')) {
+            URL::forceScheme('https');
+        }
     }
 }
--- config/app.php
+++ config/app.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Facade;
 
 return [
-    'foo' => 'bar',
+    'is_https' => env('FORCE_HTTPS', false),
     /*
     |--------------------------------------------------------------------------
     | Application Name
