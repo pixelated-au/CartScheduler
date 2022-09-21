4fff3e429ed8b484f75945fe7d3683d58a095cb3
ian <ian@pixelated.com.au>
2022-09-21 11:32:55 +1000

    Testing deployment 1

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -18,7 +18,7 @@
     <AppLayout title="Dashboard">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                Administration Dashboard </h2>
+                Administration Dashboard ABC</h2>
         </template>
 
         <div class="py-2 sm:py-12">
