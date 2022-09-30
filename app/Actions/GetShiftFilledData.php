2c8cc5643ea3b4d45f86979e2383744164aa75ad
ian <ian@pixelated.com.au>
2022-09-30 15:56:00 +1000

    Fixed a bug when there were shifts of different max_volunteers

--- app/Actions/GetShiftFilledData.php
+++ app/Actions/GetShiftFilledData.php
@@ -24,25 +24,27 @@ class GetShiftFilledData
 
                     SELECT dates.date,
                            COUNT(shift_user.id)                AS shifts_filled,
-                           (SELECT COUNT(shifts.id) * locations.max_volunteers
+                           (SELECT SUM(shifts_available.total_shifts)
+                           FROM (SELECT COUNT(shifts.id) * locations.max_volunteers AS total_shifts
                             FROM shifts
                                      JOIN locations on locations.id = shifts.location_id
-                            WHERE CASE
+                            WHERE locations.is_enabled = 1
+                              AND shifts.is_enabled = 1
+                              AND CASE
                                       WHEN DAYOFWEEK(dates.date) = 1 THEN shifts.day_sunday
                                       WHEN DAYOFWEEK(dates.date) = 2 THEN shifts.day_monday
                                       WHEN DAYOFWEEK(dates.date) = 3 THEN shifts.day_tuesday
                                       WHEN DAYOFWEEK(dates.date) = 4 THEN shifts.day_wednesday
                                       WHEN DAYOFWEEK(dates.date) = 5 THEN shifts.day_thursday
                                       WHEN DAYOFWEEK(dates.date) = 6 THEN shifts.day_friday
-                                      WHEN DAYOFWEEK(dates.date) = 7 THEN shifts.day_saturday
-                                      END = 1
-                            GROUP BY locations.max_volunteers) as shifts_available
+                                      WHEN DAYOFWEEK(dates.date) = 7 THEN shifts.day_saturday END = 1
+                            GROUP BY locations.max_volunteers) as shifts_available) AS shifts_available
                     FROM dates
                              LEFT JOIN shift_user ON dates.date = shift_user.shift_date
                              LEFT JOIN shifts ON shifts.id = shift_user.shift_id
+                    WHERE shifts.is_enabled = 1
                     GROUP BY dates.date, shifts_available
                     ORDER BY dates.date")
         );
-        //                    WHERE date + INTERVAL 1 DAY <= (SELECT LAST_DAY(DATE_ADD(NOW(), INTERVAL 1 MONTH))))
     }
 }
