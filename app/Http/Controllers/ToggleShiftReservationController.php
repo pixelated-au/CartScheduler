622ddc6edc99778479ed8768d0243f478a74fd60
ian <ian@pixelated.com.au>
2022-12-06 09:32:05 +1100

    Changed the comparison date to zulu timezone

--- app/Http/Controllers/ToggleShiftReservationController.php
+++ app/Http/Controllers/ToggleShiftReservationController.php
@@ -23,8 +23,9 @@ class ToggleShiftReservationController extends Controller
         $location = Location::with([
             'shifts'       => fn(HasMany $query) => $query->where('shifts.id', '=', $data['shift']),
             'shifts.users' => fn(BelongsToMany $query) => $query->wherePivot(
-                'shift_date', $shiftDate->toDateString()
-            )
+                'shift_date',
+                $shiftDate->toDateString(),
+            ),
         ])->findOrFail($data['location']);
 
         /** @var \App\Models\Shift $shift */
@@ -64,30 +65,32 @@ class ToggleShiftReservationController extends Controller
                         $requestedShift       = Shift::find($data['shift']);
                         $requestedShiftPeriod = CarbonPeriod::create(
                             $requestedShift->start_time,
-                            $requestedShift->end_time
+                            $requestedShift->end_time,
                         );
 
                         $foundOverlappingShift = $userShiftsOnDate->first(
                             fn(ShiftUser $currentShift) => $requestedShiftPeriod->overlaps(CarbonPeriod::create(
                                 $currentShift->shift->start_time,
-                                $currentShift->shift->end_time
-                            ))
+                                $currentShift->shift->end_time,
+                            ),
+                            ),
                         );
 
                         if ($foundOverlappingShift) {
                             $start = Carbon::parse($foundOverlappingShift->shift->start_time)->format('h:i a');
                             $end   = Carbon::parse($foundOverlappingShift->shift->end_time)->format('h:i a');
-                            $fail("Sorry, you already have another shift that overlaps this shift at {$foundOverlappingShift->shift->location->name} between $start and $end.");
+                            $fail("Sorry, you already have another shift that overlaps this shift at {$foundOverlappingShift->shift->location->name} between $start and $end.",
+                            );
                         }
                     }
-                }
+                },
             ],
             'do_reserve' => ['required', 'boolean'],
             'date'       => [
                 'required',
                 'date',
                 'after_or_equal:today',
-                'before_or_equal:last day of next month',
+                'before_or_equal:last day of next month zulu',
             ],
         ]);
     }
