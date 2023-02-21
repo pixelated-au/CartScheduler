03f039d1c928af31a3853fc68022920654ca3402
ian <ian@pixelated.com.au>
2023-02-21 11:04:39 +1100

    Updated to make the relationships more direct

--- database/migrations/2022_08_10_160810_create_reports_table.php
+++ database/migrations/2022_08_10_160810_create_reports_table.php
@@ -9,7 +9,10 @@ return new class extends Migration {
     {
         Schema::create('reports', static function (Blueprint $table) {
             $table->id();
-            $table->foreignId('shift_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('shift_id')
+                  ->constrained('shift_user', 'shift_id')
+                  ->onDelete('cascade')
+                  ->onUpdate('cascade');
             $table->foreignId('report_submitted_user_id')->constrained('shift_user', 'user_id')->onDelete('cascade');
             $table->date('shift_date');
             $table->smallInteger('placements_count')->unsigned()->default(0);
