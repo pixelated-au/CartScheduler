418e0eeaf40ce9c243764bd1252f40b9798a67e1
ian <ian@pixelated.com.au>
2023-02-22 09:19:02 +1100

    Added in info on the 'about command'

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Codedge\Updater\UpdaterManager;
+use Composer\InstalledVersions;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Console\AboutCommand;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
@@ -14,9 +17,15 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
-        //
+        AboutCommand::add('CartScheduler', static fn(UpdaterManager $updater) => [
+            'Self Updater'  => class_exists(InstalledVersions::class) ? InstalledVersions::getPrettyVersion('bugsnag/bugsnag-laravel') : '<fg=yellow;options=bold>-</>',
+            'Inertia'       => class_exists(InstalledVersions::class) ? InstalledVersions::getPrettyVersion('inertiajs/inertia-laravel') : '<fg=yellow;options=bold>-</>',
+            'Excel Support' => class_exists(InstalledVersions::class) ? InstalledVersions::getPrettyVersion('maatwebsite/excel') : '<fg=yellow;options=bold>-</>',
+            '<fg=bright-magenta>CartScheduler Version</>'
+                            => '<fg=bright-magenta>' . $updater->source()->getVersionInstalled() . '</>',
+        ]);
     }
 
     /**
@@ -24,7 +33,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         Model::preventLazyLoading(!app()->isProduction());
         Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
