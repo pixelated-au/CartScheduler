9ed6a626953d47abe4633b6791ad462b13082bc0
Ian Heggaton <ian@pixelated.com.au>
2023-05-05 13:39:32 +1000

    Fixed a lazy loading exception

--- app/Actions/ValidateVolunteerIsAllowedToBeRosteredAction.php
+++ app/Actions/ValidateVolunteerIsAllowedToBeRosteredAction.php
@@ -11,7 +11,7 @@ class ValidateVolunteerIsAllowedToBeRosteredAction
     public function execute(Location $location, User $user): void
     {
         if ($location->requires_brother && $user->gender === 'female') {
-            $shiftVolunteers = $location->shifts->first()->users;
+            $shiftVolunteers = $location->shifts->first()->load('users')->users;
             $hasBro          = $shiftVolunteers->contains(fn($volunteer) => $volunteer->gender === 'male');
 
             if (!$hasBro && $shiftVolunteers->count() >= $location->max_volunteers - 1) {
