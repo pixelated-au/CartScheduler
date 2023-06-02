845559c61750788a6f4b4e118240e1555766d3c9
Ian Heggaton <ian@pixelated.com.au>
2023-06-02 10:48:51 +1000

    Added support for favicons

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<!--suppress HtmlUnknownTarget -->
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
@@ -21,6 +22,18 @@
             document.documentElement.classList.remove('dark')
         }
     </script>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png?v=1">
+    <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png?v=1">
+    <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png?v=1">
+    <link rel="manifest" href="/icons/site.webmanifest?v=1">
+    <link rel="mask-icon" href="/icons/safari-pinned-tab.svg?v=1" color="#5bbad5">
+    <link rel="shortcut icon" href="/icons/favicon.ico?v=1">
+    <meta name="apple-mobile-web-app-title" content="Cart Scheduler">
+    <meta name="application-name" content="Cart Scheduler">
+    <meta name="msapplication-TileColor" content="#00aba9">
+    <meta name="msapplication-config" content="/icons/browserconfig.xml?v=1">
+    <meta name="theme-color" content="#ffffff">
 </head>
 <body class="font-sans antialiased dark:bg-slate-800">
 @inertia
