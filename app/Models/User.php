269bc00e7d6037b8d96b01718017d8a23825ef41
Ian Heggaton <ian@pixelated.com.au>
2023-06-09 10:11:17 +1000

    Added deleted_at to $hidden

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Mail;
@@ -61,6 +62,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
+        'deleted_at',
         'password',
         'remember_token',
         'two_factor_recovery_codes',
@@ -139,6 +141,11 @@ class User extends Authenticatable
     public function bookings(): HasMany
     {
         return $this->HasMany(ShiftUser::class);
+    }
+
+    public function availability(): HasOne
+    {
+        return $this->hasOne(UserAvailability::class);
     }
 
     public function getActivitylogOptions(): LogOptions
