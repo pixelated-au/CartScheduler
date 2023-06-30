d7336b2859731e6cb642f13c7cbeb76bdbc7d9f1
Ian Heggaton <ian@pixelated.com.au>
2023-06-30 10:00:57 +1000

    Add 'max_volunteers_per_location' configuration option to the 'cart-scheduler.php' file.

--- config/cart-scheduler.php
+++ config/cart-scheduler.php
@@ -6,4 +6,5 @@ return [
     'release_weekly_shifts_on_day'      => env('CA_SHIFT_RESERVATION_WEEK_RELEASE_DAY', 'SUN'),
     'release_new_shifts_at_time'        => env('CA_SHIFT_RESERVATION_NEW_RELEASE_TIME', '00:00:00'),
     'do_release_shifts_daily'           => env('CA_SHIFT_RESERVATION_DAILY_RELEASE_SCHEDULE', false),
+    'max_volunteers_per_location'       => (int)env('CA_MAX_VOLUNTEERS_PER_SHIFT', 4),
 ];
