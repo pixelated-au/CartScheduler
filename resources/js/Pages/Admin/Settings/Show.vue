95d06cfee5034e04afd183ab9abdc91e34d12146
Ian Heggaton <ian@pixelated.com.au>
2023-07-28 16:26:55 +1000

    Add software update functionality to the Show.vue component, including importing the SoftwareUpdateForm component, updating the UpdateAllowedSettingsUsersForm component with a new CSS class, and adding new components and functionality for creating an update section, getting the most recent version of the app, scheduling update checks, storing updates in the 'settings', and creating a button and link for checking updates and showing the changelog.

--- resources/js/Pages/Admin/Settings/Show.vue
+++ resources/js/Pages/Admin/Settings/Show.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
+import SoftwareUpdateForm from "@/Pages/Admin/Settings/Partials/SoftwareUpdateForm.vue";
 import UpdateAllowedSettingsUsersForm from "@/Pages/Admin/Settings/Partials/UpdateAllowedSettingsUsersForm.vue";
 import UpdateGeneralSettingsForm from "@/Pages/Admin/Settings/Partials/UpdateGeneralSettingsForm.vue";
 
@@ -24,7 +25,14 @@ defineProps({
                 </ul>
             </div>
             <UpdateGeneralSettingsForm :settings="settings" class="mb-10 md:mb-3"/>
-            <UpdateAllowedSettingsUsersForm :settings="settings"/>
+            <UpdateAllowedSettingsUsersForm :settings="settings" class="mb-10 md:mb-3"/>
+            <SoftwareUpdateForm :settings="settings"/>
+            CREATE AN UPDATE SECTION HERE
+            create a console item to get the most recent version of the app
+            create a schedule to check for updates
+            store the update in the 'settings'
+            create a button to check for updates
+            create a link that will show the changelog
         </div>
     </AppLayout>
 </template>
