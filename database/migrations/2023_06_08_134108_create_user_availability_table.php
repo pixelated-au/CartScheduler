fee4984145533fa5678c084bbcd5d8f17834cdad
Ian Heggaton <ian@pixelated.com.au>
2023-08-04 11:15:14 +1000

    Refactor user availability table to use enums and add support for all days of the week.

--- database/migrations/2023_06_08_134108_create_user_availability_table.php
+++ database/migrations/2023_06_08_134108_create_user_availability_table.php
@@ -1,31 +1,32 @@
 <?php
 
+use App\Enums\AvailabilityPeriods;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    const DAY_AVAILABILITIES = [
-        'morning',
-        'afternoon',
-        'evening',
-        'half-day',
-        'full-day',
-        'not-available',
-    ];
-
     public function up(): void
     {
         Schema::create('user_availabilities', function (Blueprint $table) {
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->primary('user_id');
 
-            $table->set('day_monday', self::DAY_AVAILABILITIES);
-            $table->set('day_tuesday', self::DAY_AVAILABILITIES);
-            $table->set('day_wednesday', self::DAY_AVAILABILITIES);
-            $table->set('day_thursday', self::DAY_AVAILABILITIES);
-            $table->set('day_friday', self::DAY_AVAILABILITIES);
+            $availabilities = AvailabilityPeriods::getHourPartValues();
 
+            $table->set('day_monday', $availabilities);
+            $table->set('day_tuesday', $availabilities);
+            $table->set('day_wednesday', $availabilities);
+            $table->set('day_thursday', $availabilities);
+            $table->set('day_friday', $availabilities);
+            $table->set('day_saturday', $availabilities);
+            $table->set('day_sunday', $availabilities);
+
+            $table->tinyInteger('num_mondays')->unsigned()->default(0);
+            $table->tinyInteger('num_tuesdays')->unsigned()->default(0);
+            $table->tinyInteger('num_wednesdays')->unsigned()->default(0);
+            $table->tinyInteger('num_thursdays')->unsigned()->default(0);
+            $table->tinyInteger('num_fridays')->unsigned()->default(0);
             $table->tinyInteger('num_saturdays')->unsigned()->default(0);
             $table->tinyInteger('num_sundays')->unsigned()->default(0);
 
