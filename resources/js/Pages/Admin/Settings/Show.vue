d815f6a9582da9b614c5619db96a880262fe0475
Ian Heggaton <ian@pixelated.com.au>
2023-08-04 16:27:34 +1000

    Reordered components

--- resources/js/Pages/Admin/Settings/Show.vue
+++ resources/js/Pages/Admin/Settings/Show.vue
@@ -17,16 +17,16 @@ defineProps({
         </template>
 
         <div class="max-w-7xl mx-auto pt-10 pb-5 sm:px-6 lg:px-8">
-<!--            <div class="bg-white dark:bg-slate-900 dark:text-white shadow-xl sm:rounded-lg sm:p-6 sm:mb-4">-->
-<!--                <ul>-->
-<!--                    <li v-for="(setting, name, idx) in settings" :key="idx">-->
-<!--                        {{ name }}: {{ setting }}-->
-<!--                    </li>-->
-<!--                </ul>-->
-<!--            </div>-->
+            <!--            <div class="bg-white dark:bg-slate-900 dark:text-white shadow-xl sm:rounded-lg sm:p-6 sm:mb-4">-->
+            <!--                <ul>-->
+            <!--                    <li v-for="(setting, name, idx) in settings" :key="idx">-->
+            <!--                        {{ name }}: {{ setting }}-->
+            <!--                    </li>-->
+            <!--                </ul>-->
+            <!--            </div>-->
+            <SoftwareUpdateForm :settings="settings" class="mb-10 md:mb-3"/>
             <UpdateGeneralSettingsForm :settings="settings" class="mb-10 md:mb-3"/>
             <UpdateAllowedSettingsUsersForm :settings="settings" class="mb-10 md:mb-3"/>
-            <SoftwareUpdateForm :settings="settings"/>
         </div>
     </AppLayout>
 </template>
