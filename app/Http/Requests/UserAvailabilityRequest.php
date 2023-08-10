e70a7a2999c6eacab7b7052d4318170fbce55b6b
Ian Heggaton <ian@pixelated.com.au>
2023-08-10 14:28:58 +1000

    Add validation rules for user availability on each day of the week and maximum number of days available.

--- app/Http/Requests/UserAvailabilityRequest.php
+++ app/Http/Requests/UserAvailabilityRequest.php
@@ -9,7 +9,30 @@ class UserAvailabilityRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'day_monday' => ['required', 'array'],
+            'day_monday.*' => ['required', 'integer', 'min:0', 'max:23'],
+            'day_tuesday' => ['required', 'array'],
+            'day_tuesday.*' => ['required', 'integer', 'min:0', 'max:23'],
+            'day_wednesday' => ['required', 'array'],
+            'day_wednesday.*' => ['required', 'integer', 'min:0', 'max:23'],
+            'day_thursday' => ['required', 'array'],
+            'day_thursday.*' => ['required', 'integer', 'min:0', 'max:23'],
+            'day_friday' => ['required', 'array'],
+            'day_friday.*' => ['required', 'integer', 'min:0', 'max:23'],
+            'day_saturday' => ['required', 'array'],
+            'day_saturday.*' => ['required', 'integer', 'min:0', 'max:23'],
+            'day_sunday' => ['required', 'array'],
+            'day_sunday.*' => ['required', 'integer', 'min:0', 'max:23'],
 
+            'num_mondays' => ['required', 'integer', 'min:0', 'max:4'],
+            'num_tuesdays' => ['required', 'integer', 'min:0', 'max:4'],
+            'num_wednesdays' => ['required', 'integer', 'min:0', 'max:4'],
+            'num_thursdays' => ['required', 'integer', 'min:0', 'max:4'],
+            'num_fridays' => ['required', 'integer', 'min:0', 'max:4'],
+            'num_saturdays' => ['required', 'integer', 'min:0', 'max:4'],
+            'num_sundays' => ['required', 'integer', 'min:0', 'max:4'],
+
+            'comments' => ['nullable', 'string'],
         ];
     }
 
