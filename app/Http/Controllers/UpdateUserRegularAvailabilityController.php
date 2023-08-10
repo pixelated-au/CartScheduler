d39c19620a396f86d52c765d83eda27008a5bdc9
Ian Heggaton <ian@pixelated.com.au>
2023-08-10 14:29:08 +1000

    Refactor UpdateUserRegularAvailabilityController to use UserAvailabilityRequest and add detailed user availability updates.

--- app/Http/Controllers/UpdateUserRegularAvailabilityController.php
+++ app/Http/Controllers/UpdateUserRegularAvailabilityController.php
@@ -2,16 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
-use Illuminate\Http\Request;
+use App\Http\Requests\UserAvailabilityRequest;
 use Illuminate\Support\Facades\Redirect;
-use Inertia\Inertia;
 
 class UpdateUserRegularAvailabilityController extends Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke(UserAvailabilityRequest $request)
     {
-        $this->authorize('update', $request->user());
+        $user = $request->user();
+        $this->authorize('update', $user);
+
+        $user->availability->day_monday    = $request->validated('day_monday');
+        $user->availability->day_tuesday   = $request->validated('day_tuesday');
+        $user->availability->day_wednesday = $request->validated('day_wednesday');
+        $user->availability->day_thursday  = $request->validated('day_thursday');
+        $user->availability->day_friday    = $request->validated('day_friday');
+        $user->availability->day_saturday  = $request->validated('day_saturday');
+        $user->availability->day_sunday    = $request->validated('day_sunday');
+
+        $user->availability->num_mondays    = $request->validated('num_mondays');
+        $user->availability->num_tuesdays   = $request->validated('num_tuesdays');
+        $user->availability->num_wednesdays = $request->validated('num_wednesdays');
+        $user->availability->num_thursdays  = $request->validated('num_thursdays');
+        $user->availability->num_fridays    = $request->validated('num_fridays');
+        $user->availability->num_saturdays  = $request->validated('num_saturdays');
+        $user->availability->num_sundays    = $request->validated('num_sundays');
+
+        $user->availability->comments = $request->validated('comments');
+
+        $user->availability->save();
 
         session()->flash('flash.banner', 'Your availability has been updated.');
         session()->flash('flash.bannerStyle', 'success');
