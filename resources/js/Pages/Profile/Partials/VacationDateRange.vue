fed97cd707f07a9e9013b9e5f8b5d186dfca5812
Ian Heggaton <ian@pixelated.com.au>
2023-09-08 16:37:41 +1000

    Introduce new component VacationDateRange.vue for handling vacation date selections.

---
+++ resources/js/Pages/Profile/Partials/VacationDateRange.vue
@@ -0,0 +1,49 @@
+<script setup>
+import Datepicker from "@vuepic/vue-datepicker";
+import {intlFormat, parseISO} from "date-fns";
+import formatISO from "date-fns/formatISO";
+import {computed, inject} from 'vue';
+
+const props = defineProps({
+    startDate: {
+        type: String,
+    },
+    endDate: {
+        type: String,
+    },
+})
+
+const isDarkMode = inject('darkMode', false)
+
+const emit = defineEmits([
+    'update:startDate',
+    'update:endDate',
+])
+
+const parseDate = (date) => formatISO(date, {representation: 'date'});
+
+/**
+ * @param {Date} date
+ */
+const formatDate = (date) => intlFormat(date, {day: 'numeric', month: 'numeric', year: 'numeric'});
+
+/**
+ * @param {Array<Date, Date>} dates
+ * @returns {string}
+ */
+const formatDateRange = (dates) => formatDate(dates[0]) + ' - ' +  formatDate(dates[1]);
+
+const dates = computed({
+    get: () => {
+        return [props.startDate, props.endDate]
+    },
+    set: (value) => {
+        emit('update:startDate', parseDate(value[0]))
+        emit('update:endDate', parseDate(value[1]))
+    }
+})
+</script>
+
+<template>
+    <Datepicker range auto-apply v-model="dates" :enable-time-picker="false" :format="formatDateRange" :clearable="false" :dark="isDarkMode" class="vacation"/>
+</template>
