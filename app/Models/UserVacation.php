49e2ca015f073139035ab18708c25eeba141fe67
Ian Heggaton <ian@pixelated.com.au>
2023-09-08 13:07:58 +1000

    Implement Prunable trait in UserVacation model to remove outdated records and improve date parsing with Carbon.

--- app/Models/UserVacation.php
+++ app/Models/UserVacation.php
@@ -2,18 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
 use Illuminate\Support\Carbon;
 
 class UserVacation extends Model
 {
-    use HasFactory;
+    use HasFactory, Prunable;
+
+    public function prunable(): Builder
+    {
+        /** @noinspection PhpIncompatibleReturnTypeInspection */
+        return static::where('end_date', '<', now()->subMonths(3));
+    }
+
     protected function startDate(): Attribute
     {
         return Attribute::make(
-            get: fn($value) => $value->format('Y-m-d'),
+            get: fn($value) => Carbon::parse($value)->format('Y-m-d'),
             set: fn($value) => $this->attributes['start_date'] = Carbon::parse($value)->startOfDay()
         );
     }
@@ -21,7 +30,7 @@ class UserVacation extends Model
     protected function endDate(): Attribute
     {
         return Attribute::make(
-            get: fn($value) => $value->format('Y-m-d'),
+            get: fn($value) => Carbon::parse($value)->format('Y-m-d'),
             set: fn($value) => $this->attributes['end_date'] = Carbon::parse($value)->endOfDay()
         );
     }
