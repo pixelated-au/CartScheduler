5a548100c3e17900be4d60b05235832e1f3b221f
Ian Heggaton <ian@pixelated.com.au>
2023-09-08 12:46:15 +1000

    Introduces new UserVacationFactory.php to manage user vacation data.

---
+++ database/factories/UserVacationFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\AvailabilityHours;
+use App\Models\UserAvailability;
+use App\Models\UserVacation;
+use Carbon\CarbonImmutable;
+use DateInterval;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Collection;
+
+class UserVacationFactory extends Factory
+{
+    protected $model = UserVacation::class;
+
+    protected Collection $dayParts;
+
+    /**
+     * @throws \Exception
+     */
+    public function definition(): array
+    {
+        $startDate = CarbonImmutable::instance($this->faker->dateTimeBetween('now', '+6 months'));
+        return [
+            'user_id'     => $this->faker->randomNumber(),
+            'start_date'  => $startDate,
+            'end_date'    => $startDate->add($this->faker->numberBetween(1, 30), 'day'),
+            'description' => $this->faker->sentence(),
+            'created_at'  => Carbon::now(),
+            'updated_at'  => Carbon::now(),
+        ];
+    }
+}
