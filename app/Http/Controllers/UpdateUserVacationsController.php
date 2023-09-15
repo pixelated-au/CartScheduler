d67e78f0c1264481fe57f16e359a5d09911d2e27
Ian Heggaton <ian@pixelated.com.au>
2023-09-15 18:14:41 +1000

    Refactor UpdateUserVacationsController to handle user vacation requests and update user vacations accordingly.

--- app/Http/Controllers/UpdateUserVacationsController.php
+++ app/Http/Controllers/UpdateUserVacationsController.php
@@ -2,18 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserVacationRequest;
 use App\Models\User;
+use App\Models\UserVacation;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 
 class UpdateUserVacationsController extends Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke(UserVacationRequest $request)
     {
         $this->authorize('update', $request->user());
 
-        session()->flash('flash.banner', 'Your temporary unavailable dates have been updated.');
+        /** @var User $user */
+        $user = $request->user();
+        $vacations = $request->validated('vacations');
+
+        foreach ($vacations as $vacation) {
+            if (isset($vacation['id'])) {
+                /** @var \App\Models\UserVacation $userVacation */
+                $userVacation = $user->vacations()->find($vacation['id']);
+                $userVacation->start_date = $vacation['start_date'];
+                $userVacation->end_date = $vacation['end_date'];
+                $userVacation->description = $vacation['description'];
+                $userVacation->save();
+            } else {
+                $userVacation = new UserVacation();
+                $userVacation->start_date = $vacation['start_date'];
+                $userVacation->end_date = $vacation['end_date'];
+                $userVacation->description = $vacation['description'];
+                $userVacation->user_id = $user->id;
+                $userVacation->save();
+            }
+        }
+
+
+        session()->flash('flash.banner', 'Your availability has been updated.');
         session()->flash('flash.bannerStyle', 'success');
 
         return Redirect::route('user.availability');
