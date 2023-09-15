f4cada873ce73732e99ae562af00ca5ba08bf402
Ian Heggaton <ian@pixelated.com.au>
2023-09-15 17:57:50 +1000

    Initial

---
+++ resources/js/Components/Icons/CloseCircle.vue
@@ -0,0 +1,12 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16">
+        <path
+            d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10Zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm0-9.414 2.828-2.829 1.415 1.415L13.414 12l2.829 2.828-1.415 1.415L12 13.414l-2.828 2.829-1.415-1.415L10.586 12 7.757 9.172l1.415-1.415L12 10.586Z"/>
+    </svg>
+</template>
+
+<style lang="scss" scoped>
+path {
+    @apply fill-slate-700 dark:fill-slate-400;
+}
+</style>
