b299135ffd20771fda88778e6a40d922ef28acbf
Ian Heggaton <ian@pixelated.com.au>
2023-09-29 10:45:44 +1000

    Add new UserVacationResource.php

---
+++ app/Http/Resources/UserVacationResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/** @mixin \App\Models\UserVacation */
+class UserVacationResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'id'          => $this->id,
+            'user_id'     => $this->user_id,
+            'start_date'  => $this->start_date,
+            'end_date'    => $this->end_date,
+            'description' => $this->description,
+        ];
+    }
+}
