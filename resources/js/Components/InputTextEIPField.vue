a235db01fe57c2649e5c407a4efa023f0ec1fe72
Ian Heggaton <ian@pixelated.com.au>
2023-09-29 10:44:35 +1000

    Refactor InputTextEIPField.vue to include an emptyValue prop with a default value of an empty string.

--- resources/js/Components/InputTextEIPField.vue
+++ resources/js/Components/InputTextEIPField.vue
@@ -1,16 +1,19 @@
 <script setup>
 import JetInput from "@/Jetstream/Input.vue";
-import {useVModel} from "@vueuse/core";
 import {computed, ref} from 'vue'
 
 const props = defineProps({
-  modelValue: [Number, String],
+    modelValue: [Number, String],
     inputClass: String,
+    emptyValue: {
+        type: String,
+        default: '',
+    },
 })
 
 const myModel = computed({
-  get: () => props.modelValue,
-  set: (value) => emit('update:modelValue', value)
+    get: () => props.modelValue,
+    set: (value) => emit('update:modelValue', value)
 })
 
 const emit = defineEmits(['update:modelValue'])
@@ -22,9 +25,10 @@ const hideDescription = () => doEditDescription.value = false;
 
 <template>
     <div v-if="!doEditDescription" class="cursor-pointer"
-         @click="doEditDescription = true"><span class="underline decoration-dashed">{{ modelValue }}</span>
+         @click="doEditDescription = true"><span class="underline decoration-dashed">{{ modelValue?.trim() || emptyValue }}</span>
     </div>
-    <JetInput v-else v-model="myModel" autofocus class="px-2 py-1" :class="inputClass" :ref="inputDescription" @blur="hideDescription" @keydown.esc="hideDescription"/>
+    <JetInput v-else v-model="myModel" autofocus class="px-2 py-1" :class="inputClass" :ref="inputDescription"
+              @blur="hideDescription" @keydown.esc="hideDescription"/>
 </template>
 
 <style scoped>
