5f8f8c12957400da20cb0780d9bf91de41bdb699
Ian Heggaton <ian@pixelated.com.au>
2023-10-20 10:52:32 +1100

    Added in marital status and year of baptism

--- database/migrations/2023_10_20_090502_update_user_table.php
+++ database/migrations/2023_10_20_090502_update_user_table.php
@@ -8,9 +8,14 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->enum('appointment', ['elder', 'ministerial servant'])->nullable()->after('mobile_phone');
+            $table->year('year_of_baptism')->after('mobile_phone')->nullable();
+            $table->enum('marital_status', ['married', 'single', 'divorced', 'separated', 'widowed'])->default('single')->after('year_of_baptism');
+            $table->unsignedBigInteger('spouse_id')->nullable()->after('marital_status');
+            $table->enum('appointment', ['elder', 'ministerial servant'])->nullable()->after('spouse_id');
             $table->enum('serving_as', ['field missionary', 'special pioneer', 'bethel family member', 'regular pioneer', 'publisher'])->default('publisher')->after('appointment');
             $table->boolean('responsible_brother')->default(false)->after('serving_as')->comment('Indicates if the volunteer has been trained to oversee a shift');
+
+            $table->foreign('spouse_id')->references('id')->on('users')->nullOnDelete();
         });
     }
 };
