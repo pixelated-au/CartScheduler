46b0b776ea272d0c67c4147fea17175b283f6fe0
Ian Heggaton <ian@pixelated.com.au>
2023-10-20 10:31:09 +1100

    Added in responsible brother field

--- database/migrations/2023_10_20_090502_update_user_table.php
+++ database/migrations/2023_10_20_090502_update_user_table.php
@@ -10,6 +10,7 @@ return new class extends Migration {
         Schema::table('users', function (Blueprint $table) {
             $table->enum('appointment', ['elder', 'ministerial servant'])->nullable()->after('mobile_phone');
             $table->enum('serving_as', ['field missionary', 'special pioneer', 'bethel family member', 'regular pioneer', 'publisher'])->default('publisher')->after('appointment');
+            $table->boolean('responsible_brother')->default(false)->after('serving_as')->comment('Indicates if the volunteer has been trained to oversee a shift');
         });
     }
 };
