ca06760f8b7b5ba9df7103fc2cce1a44ad27a7ee
Ian Heggaton <ian@pixelated.com.au>
2023-10-20 12:50:36 +1100

    Added in new fields

--- app/Http/Resources/UserAdminResource.php
+++ app/Http/Resources/UserAdminResource.php
@@ -12,19 +12,31 @@ use Illuminate\Http\Resources\Json\JsonResource;
  * @property mixed $gender
  * @property mixed $mobile_phone
  * @property mixed $is_enabled
+ * @property mixed $year_of_baptism
+ * @property mixed $appointment
+ * @property mixed $serving_as
+ * @property mixed $marital_status
+ * @property \App\Models\User $spouse
+ * @property mixed $spouse_id
  */
 class UserAdminResource extends JsonResource
 {
     public function toArray($request): array
     {
         return [
-            'id'           => $this->id,
-            'name'         => $this->name,
-            'email'        => $this->email,
-            'role'         => $this->role,
-            'gender'       => $this->gender,
-            'mobile_phone' => $this->mobile_phone,
-            'is_enabled'   => $this->is_enabled,
+            'id'              => $this->id,
+            'name'            => $this->name,
+            'email'           => $this->email,
+            'role'            => $this->role,
+            'gender'          => $this->gender,
+            'mobile_phone'    => $this->mobile_phone,
+            'year_of_baptism' => $this->year_of_baptism,
+            'appointment'     => $this->appointment,
+            'serving_as'      => $this->serving_as,
+            'marital_status'  => $this->marital_status,
+            'spouse_name'     => $this->whenHas('spouse_id', fn() => $this->spouse->name),
+            'spouse_id'       => $this->whenHas('spouse_id', fn() => $this->spouse_id),
+            'is_enabled'      => $this->is_enabled,
         ];
     }
 }
