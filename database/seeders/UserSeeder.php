f20949e93c3c3fff82cf5280a292889b24410171
Ian Heggaton <ian@pixelated.com.au>
2023-10-20 11:14:38 +1100

    Added in spouses

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,6 +12,15 @@ class UserSeeder extends Seeder
         User::unsetEventDispatcher();// stops the UserAccountCreated mail from being sent
         User::factory()->devUser()->create();
         User::factory()->adminRoleUser()->count(3)->create();
+        $husband = User::factory()->create([
+            'name' => 'Husband',
+        ]);
+        $wife = User::factory()->create([
+            'name' => 'Wife',
+            'spouse_id' => $husband->id,
+        ]);
+        $husband->spouse_id = $wife->id;
+        $husband->save();
         User::factory()->userRoleUser()->count(400)->create();
     }
 }
