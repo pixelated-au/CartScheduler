8e4b6d32ac1fd03326ead86f7916e4c5e88dd50d
Ian Heggaton <ian@pixelated.com.au>
2023-10-20 19:54:02 +1100

    Update user table to make the 'marital_status' and 'serving_as' fields nullable instead of having default values.

--- database/migrations/2023_10_20_090502_update_user_table.php
+++ database/migrations/2023_10_20_090502_update_user_table.php
@@ -9,10 +9,10 @@ return new class extends Migration {
     {
         Schema::table('users', function (Blueprint $table) {
             $table->year('year_of_baptism')->after('mobile_phone')->nullable();
-            $table->enum('marital_status', ['married', 'single', 'divorced', 'separated', 'widowed'])->default('single')->after('year_of_baptism');
+            $table->enum('marital_status', ['married', 'single', 'divorced', 'separated', 'widowed'])->nullable()->after('year_of_baptism');
             $table->unsignedBigInteger('spouse_id')->nullable()->after('marital_status');
             $table->enum('appointment', ['elder', 'ministerial servant'])->nullable()->after('spouse_id');
-            $table->enum('serving_as', ['field missionary', 'special pioneer', 'bethel family member', 'circuit overseer', 'regular pioneer', 'publisher'])->default('publisher')->after('appointment');
+            $table->enum('serving_as', ['field missionary', 'special pioneer', 'bethel family member', 'circuit overseer', 'regular pioneer', 'publisher'])->nullable()->after('appointment');
             $table->boolean('responsible_brother')->default(false)->after('serving_as')->comment('Indicates if the volunteer has been trained to oversee a shift');
 
             $table->foreign('spouse_id')->references('id')->on('users')->nullOnDelete();
