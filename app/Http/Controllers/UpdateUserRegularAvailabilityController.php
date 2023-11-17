b4258adc840be2015ef4cfa3de335a89f280c814
Ian Heggaton <ian@pixelated.com.au>
2023-11-17 19:59:54 +1100

    Refactor user availability update to handle non-existing availability

--- app/Http/Controllers/UpdateUserRegularAvailabilityController.php
+++ app/Http/Controllers/UpdateUserRegularAvailabilityController.php
@@ -19,25 +19,28 @@ class UpdateUserRegularAvailabilityController extends Controller
             $isOther = true;
             $user = User::findOrFail($request->validated('user_id'));
         }
+        $user->load('availability');
+        $availability = $user->availability ?? $user->availability()->create();
 
-        $user->availability->num_mondays    = $request->validated('num_mondays');
-        $user->availability->num_tuesdays   = $request->validated('num_tuesdays');
-        $user->availability->num_wednesdays = $request->validated('num_wednesdays');
-        $user->availability->num_thursdays  = $request->validated('num_thursdays');
-        $user->availability->num_fridays    = $request->validated('num_fridays');
-        $user->availability->num_saturdays  = $request->validated('num_saturdays');
-        $user->availability->num_sundays    = $request->validated('num_sundays');
+        $availability->num_mondays    = $request->validated('num_mondays');
+        $availability->num_tuesdays   = $request->validated('num_tuesdays');
+        $availability->num_wednesdays = $request->validated('num_wednesdays');
+        $availability->num_thursdays  = $request->validated('num_thursdays');
+        $availability->num_fridays    = $request->validated('num_fridays');
+        $availability->num_saturdays  = $request->validated('num_saturdays');
+        $availability->num_sundays    = $request->validated('num_sundays');
 
-        $user->availability->day_monday    = $request->validated('day_monday');
-        $user->availability->day_tuesday   = $request->validated('day_tuesday');
-        $user->availability->day_wednesday = $request->validated('day_wednesday');
-        $user->availability->day_thursday  = $request->validated('day_thursday');
-        $user->availability->day_friday    = $request->validated('day_friday');
-        $user->availability->day_saturday  = $request->validated('day_saturday');
-        $user->availability->day_sunday    = $request->validated('day_sunday');
+        $availability->day_monday    = $request->validated('day_monday');
+        $availability->day_tuesday   = $request->validated('day_tuesday');
+        $availability->day_wednesday = $request->validated('day_wednesday');
+        $availability->day_thursday  = $request->validated('day_thursday');
+        $availability->day_friday    = $request->validated('day_friday');
+        $availability->day_saturday  = $request->validated('day_saturday');
+        $availability->day_sunday    = $request->validated('day_sunday');
 
-        $user->availability->comments = $request->validated('comments');
+        $availability->comments = $request->validated('comments');
 
+        $user->availability = $availability;
         $user->availability->save();
 
         session()->flash('flash.banner', $isOther ? 'Volunteer availability has been updated.' : 'Your availability has been updated.');
