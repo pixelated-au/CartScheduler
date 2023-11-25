92d8e2230f6af97b9b049826e792756f64bd367d
Ian Heggaton <ian@pixelated.com.au>
2023-11-26 10:38:18 +1100

    Refactor AvailableShiftsController to limit shifts for restricted users

--- app/Http/Controllers/AvailableShiftsController.php
+++ app/Http/Controllers/AvailableShiftsController.php
@@ -7,10 +7,10 @@ use App\Actions\GetMaxShiftReservationDateAllowed;
 use App\Http\Resources\LocationResource;
 use App\Models\Location;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class AvailableShiftsController extends Controller
 {
@@ -22,9 +22,12 @@ class AvailableShiftsController extends Controller
 
     public function __invoke(Request $request, bool $canViewHistorical = false)
     {
-        if (!Auth::user()) {
+        $user = $request->user();
+        if (!$user) {
             abort(403);
         }
+
+        // TODO This appears to be working but we need to limit the locations if a user is restricted
 
         $startDate = ($canViewHistorical
             ? Carbon::today()->subMonths(1)->startOfMonth()
@@ -33,17 +36,23 @@ class AvailableShiftsController extends Controller
 
         // Locations are the list of locations in the 'accordion' menu
         $locations = Location::with([
-            'shifts' => fn(HasMany $query) => $query
+            'shifts'       => fn(HasMany $query) => $query
                 ->where('shifts.is_enabled', true)
                 ->orderBy('shifts.start_time'),
             'shifts.users' => fn(BelongsToMany $query) => $query
                 ->where('shift_user.shift_date', '>=', $startDate)
+                ->when($user->is_restricted, fn($query) => $query
+                    ->where(fn(Builder $query) => $query
+                        ->whereRaw('shift_user.shift_id in (select shift_id from shift_user where user_id = ?)', [$user->id])
+                        ->whereRaw('shift_user.shift_date in (select shift_date from shift_user where user_id = ?)', [$user->id])
+                    )
+                )
         ])
             ->where('is_enabled', true)
             ->get();
 
         $endDate = $this->getMaxShiftReservationDateAllowed->execute()->format('Y-m-d');
-        $shifts  = $this->getFreeShiftsData->execute($startDate, $endDate);
+        $shifts  = $this->getFreeShiftsData->execute($startDate, $endDate, $user);
 
         return [
             'shifts'             => $shifts,
