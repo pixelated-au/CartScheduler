5b00692d237c3d0f1060befafb9d34b222ce588f
Ian Heggaton <ian@pixelated.com.au>
2023-12-01 14:56:20 +1100

    Separated UserResource because it was continaing too much data

--- app/Http/Controllers/GetAvailableUsersForShiftController.php
+++ app/Http/Controllers/GetAvailableUsersForShiftController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Actions\GetAvailableUsersForShift;
-use App\Http\Resources\UserResource;
+use App\Http\Resources\ExtendedUserResource;
 use App\Models\Shift;
 use Illuminate\Http\Request;
 
@@ -17,11 +17,11 @@ class GetAvailableUsersForShiftController extends Controller
     public function __invoke(Request $request, Shift $shift)
     {
         $request->validate([
-            'date' => ['required', 'date'],
+            'date'    => ['required', 'date'],
             'showAll' => ['nullable', 'boolean'],
         ]);
 
-        return UserResource::collection(
+        return ExtendedUserResource::collection(
             $this->getUsersForShift->execute($shift, $request->date('date'), $request->boolean('showAll', false)),
         );
     }
