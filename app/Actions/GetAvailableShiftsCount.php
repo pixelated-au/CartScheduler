a8ce35d949c676994342840dee27af47f5f343ab
Ian Heggaton <ian@pixelated.com.au>
2023-12-08 14:37:37 +1100

    Refactor SQL query in GetAvailableShiftsCount action
    The SQL query in the 'execute' method of GetAvailableShiftsCount action has been refactored for better performance. This revision makes the query more efficient by grouping the 'volunteer_count' within a subquery and removing the repetitive calls for each shift date.

--- app/Actions/GetAvailableShiftsCount.php
+++ app/Actions/GetAvailableShiftsCount.php
@@ -24,66 +24,65 @@ class GetAvailableShiftsCount
     public function execute(string $startDate, string $endDate): Collection
     {
         $query = /** @lang MySQL */
-            "
-                WITH RECURSIVE dates (date) AS
+            "WITH RECURSIVE dates (date) AS
                    (SELECT :startDate
                     UNION ALL
                     SELECT date + INTERVAL 1 DAY
                     FROM dates
                     WHERE date + INTERVAL 1 DAY <= :endDate)
 
-                SELECT dates.date,
-                       (SELECT COUNT(*)
-                       FROM shift_user
-                       LEFT JOIN shifts s ON s.id = shift_user.shift_id
-                       LEFT JOIN locations l ON l.id = s.location_id
-                       WHERE l.is_enabled = TRUE
-                         AND shift_user.shift_date = dates.date
-                         AND (
-                           s.available_from IS NULL
-                           OR s.available_from <= dates.date
-                         )
-                         AND (
-                           s.available_to IS NULL
-                           OR s.available_to >= dates.date
-                         )
-                         AND CASE
-                           WHEN DAYOFWEEK(dates.date) = 1 THEN s.day_sunday
-                           WHEN DAYOFWEEK(dates.date) = 2 THEN s.day_monday
-                           WHEN DAYOFWEEK(dates.date) = 3 THEN s.day_tuesday
-                           WHEN DAYOFWEEK(dates.date) = 4 THEN s.day_wednesday
-                           WHEN DAYOFWEEK(dates.date) = 5 THEN s.day_thursday
-                           WHEN DAYOFWEEK(dates.date) = 6 THEN s.day_friday
-                           WHEN DAYOFWEEK(dates.date) = 7 THEN s.day_saturday
-                         END = 1
-                       ) AS volunteer_count,
-                       (SELECT CAST(SUM(l.max_volunteers) AS UNSIGNED)
-                        FROM locations l
-                        LEFT JOIN shifts s ON s.location_id = l.id
-                        WHERE l.is_enabled = TRUE
-                         AND (
-                           s.available_from IS NULL
-                           OR s.available_from <= dates.date
-                         )
-                         AND (
-                           s.available_to IS NULL
-                           OR s.available_to >= dates.date
-                         )
-                         AND CASE
-                           WHEN DAYOFWEEK(dates.date) = 1 THEN s.day_sunday
-                           WHEN DAYOFWEEK(dates.date) = 2 THEN s.day_monday
-                           WHEN DAYOFWEEK(dates.date) = 3 THEN s.day_tuesday
-                           WHEN DAYOFWEEK(dates.date) = 4 THEN s.day_wednesday
-                           WHEN DAYOFWEEK(dates.date) = 5 THEN s.day_thursday
-                           WHEN DAYOFWEEK(dates.date) = 6 THEN s.day_friday
-                           WHEN DAYOFWEEK(dates.date) = 7 THEN s.day_saturday
-                         END = 1
-                       ) AS max_allowed,
-                IF((SELECT volunteer_count) < (SELECT max_allowed), TRUE, FALSE) AS has_availability
+SELECT dates.date,
+       (SELECT CAST(SUM(l.max_volunteers) AS UNSIGNED)
+        FROM locations l
+                 LEFT JOIN shifts s ON s.location_id = l.id
+        WHERE l.is_enabled = TRUE
+          AND (
+            s.available_from IS NULL
+                OR s.available_from <= dates.date
+            )
+          AND (
+            s.available_to IS NULL
+                OR s.available_to >= dates.date
+            )
+          AND CASE
+                  WHEN DAYOFWEEK(dates.date) = 1 THEN s.day_sunday
+                  WHEN DAYOFWEEK(dates.date) = 2 THEN s.day_monday
+                  WHEN DAYOFWEEK(dates.date) = 3 THEN s.day_tuesday
+                  WHEN DAYOFWEEK(dates.date) = 4 THEN s.day_wednesday
+                  WHEN DAYOFWEEK(dates.date) = 5 THEN s.day_thursday
+                  WHEN DAYOFWEEK(dates.date) = 6 THEN s.day_friday
+                  WHEN DAYOFWEEK(dates.date) = 7 THEN s.day_saturday
+                  END = 1)                                              AS max_allowed,
+       IFNULL(count_query.volunteer_count, 0)                           AS volunteer_count,
+       IF((SELECT volunteer_count) < (SELECT max_allowed), TRUE, FALSE) AS has_availability
+FROM dates
+         LEFT JOIN (SELECT shift_date,
+                           COUNT(*) AS volunteer_count
+                    FROM shift_user
+                             LEFT JOIN shifts s ON s.id = shift_user.shift_id
+                             LEFT JOIN locations l ON l.id = s.location_id AND l.is_enabled = TRUE
+                    WHERE (
+                        s.available_from IS NULL
+                            OR s.available_from <= shift_date
+                        )
+                      AND (
+                        s.available_to IS NULL
+                            OR s.available_to >= shift_date
+                        )
+                      AND CASE
+                              WHEN DAYOFWEEK(shift_date) = 1 THEN s.day_sunday
+                              WHEN DAYOFWEEK(shift_date) = 2 THEN s.day_monday
+                              WHEN DAYOFWEEK(shift_date) = 3 THEN s.day_tuesday
+                              WHEN DAYOFWEEK(shift_date) = 4 THEN s.day_wednesday
+                              WHEN DAYOFWEEK(shift_date) = 5 THEN s.day_thursday
+                              WHEN DAYOFWEEK(shift_date) = 6 THEN s.day_friday
+                              WHEN DAYOFWEEK(shift_date) = 7 THEN s.day_saturday
+                              END = 1
+                    GROUP BY shift_date) AS count_query ON count_query.shift_date = dates.date
 
-                FROM dates
-                ORDER BY dates.date
-                ";
+
+GROUP BY dates.date
+ORDER BY dates.date";
 
         $params = ['startDate' => $startDate, 'endDate' => $endDate];
 
