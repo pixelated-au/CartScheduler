aa114c5ebc9478a951e2ee16e4102b8230827424
Ian Heggaton <ian@pixelated.com.au>
2023-12-23 09:50:28 +1100

    Refactor code formatting in LocationChoiceResource
    Improved the code readability in LocationChoiceResource by adjusting the formatting of 'id' and 'name' array assignments. This change doesn't affect the functionality, but improves its visual appearance, making the code easier to understand and maintain.

--- app/Http/Resources/LocationChoiceResource.php
+++ app/Http/Resources/LocationChoiceResource.php
@@ -13,8 +13,8 @@ class LocationChoiceResource extends JsonResource
     public function toArray($request): array
     {
         return [
-            'id'               => $this->id,
-            'name'             => $this->name,
+            'id'   => $this->id,
+            'name' => $this->name,
         ];
     }
 }
