c4f04e9eeca8e7bed22615fbbde742a6224e4f9c
Ian Heggaton <ian@pixelated.com.au>
2024-02-02 17:10:42 +1100

    Refactor GetShiftFilledData to use Carbon and improve SQL efficiency
    Utilized the Carbon library for handling dates in the GetShiftFilledData action. This enhancement improves the readability and maintainability of the code. In addition, the SQL query has been refactored to consider shift availability and locations' enabled state, providing a more efficient and optimized data retrieval process.

--- app/Actions/GetShiftFilledData.php
+++ app/Actions/GetShiftFilledData.php
@@ -2,49 +2,67 @@
 
 namespace App\Actions;
 
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class GetShiftFilledData
 {
     public function execute(string $duration = null): array
     {
-        $interval = match ($duration) {
-            'fortnight' => 'DATE_ADD(NOW(), INTERVAL 2 WEEK))',
-            default => 'LAST_DAY(DATE_ADD(NOW(), INTERVAL 1 MONTH)))',
-        };
+        $start     = Carbon::today();
+        $startDate = $start->format('Y-m-d');
+        $endDate   = (match ($duration) {
+            'fortnight' => $start->addWeeks(2),
+            default => $start->addMonth(),
+        })->format('Y-m-d');
 
-        return DB::select(DB::raw(/** @lang MySQL */ "WITH RECURSIVE dates (date) AS
-                   (SELECT MIN(shift_date)
-                    FROM shift_user
-                    WHERE shift_date >= CURRENT_DATE()
-                    UNION ALL
-                    SELECT date + INTERVAL 1 DAY
-                    FROM dates
-                    WHERE date + INTERVAL 1 DAY <= (SELECT $interval)
+        $params = ['startDate' => $startDate, 'endDate' => $endDate];
 
-                    SELECT dates.date,
-                           COUNT(shift_user.id)                AS shifts_filled,
-                           IFNULL(
-                               (SELECT SUM(locations.max_volunteers)
-                            FROM shifts
-                                     JOIN locations on locations.id = shifts.location_id
-                            WHERE locations.is_enabled = 1
-                              AND shifts.is_enabled = 1
-                              AND CASE
-                                      WHEN DAYOFWEEK(dates.date) = 1 THEN shifts.day_sunday
-                                      WHEN DAYOFWEEK(dates.date) = 2 THEN shifts.day_monday
-                                      WHEN DAYOFWEEK(dates.date) = 3 THEN shifts.day_tuesday
-                                      WHEN DAYOFWEEK(dates.date) = 4 THEN shifts.day_wednesday
-                                      WHEN DAYOFWEEK(dates.date) = 5 THEN shifts.day_thursday
-                                      WHEN DAYOFWEEK(dates.date) = 6 THEN shifts.day_friday
-                                      WHEN DAYOFWEEK(dates.date) = 7 THEN shifts.day_saturday
-                                      END = 1),
-                               0) as shifts_available
-                    FROM dates
-                             LEFT JOIN shift_user ON dates.date = shift_user.shift_date
-                             LEFT JOIN shifts ON shifts.id = shift_user.shift_id
-                    GROUP BY dates.date, shifts_available
-                    ORDER BY dates.date")
+        $rawQuery = DB::raw(/** @lang MySQL */ "WITH RECURSIVE dates (date) AS
+                 (SELECT :startDate
+                  UNION ALL
+                  SELECT date + INTERVAL 1 DAY
+                  FROM dates
+                  WHERE date + INTERVAL 1 DAY <= :endDate)
+
+SELECT dates.date
+     , COUNT(shift_user_id) AS shifts_filled
+     , IFNULL(
+        (SELECT SUM(locations.max_volunteers)
+         FROM shifts
+                JOIN locations
+                   ON locations.id = shifts.location_id
+             AND locations.is_enabled = 1
+
+         WHERE shifts.is_enabled = 1
+           AND (shifts.available_from IS NULL OR shifts.available_from >= dates.date)
+           AND (shifts.available_to IS NULL OR shifts.available_to <= dates.date)
+           AND CASE
+                 WHEN DAYOFWEEK(dates.date) = 1 THEN shifts.day_sunday
+                 WHEN DAYOFWEEK(dates.date) = 2 THEN shifts.day_monday
+                 WHEN DAYOFWEEK(dates.date) = 3 THEN shifts.day_tuesday
+                 WHEN DAYOFWEEK(dates.date) = 4 THEN shifts.day_wednesday
+                 WHEN DAYOFWEEK(dates.date) = 5 THEN shifts.day_thursday
+                 WHEN DAYOFWEEK(dates.date) = 6 THEN shifts.day_friday
+                 WHEN DAYOFWEEK(dates.date) = 7 THEN shifts.day_saturday
+                 END = 1),
+        0)                  AS shifts_available
+FROM dates
+       LEFT JOIN (SELECT shift_user.id AS shift_user_id, shift_user.shift_date AS shift_date
+                  FROM shift_user
+                         JOIN shifts
+                            ON shifts.id = shift_user.shift_id
+                      AND shifts.is_enabled = 1
+                      AND (shifts.available_from IS NULL OR shifts.available_from >= shift_date)
+                      AND (shifts.available_to IS NULL OR shifts.available_to <= shift_date)
+                         JOIN locations
+                            ON locations.id = shifts.location_id
+                      AND locations.is_enabled = 1) AS derived
+          ON dates.date = derived.shift_date
+
+GROUP BY dates.date, shifts_available
+ORDER BY dates.date"
         );
+        return DB::select($rawQuery, $params);
     }
 }
