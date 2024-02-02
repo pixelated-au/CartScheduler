56a7eca8e5824f53de1db3abc0742a06faa3ba9a
Ian Heggaton <ian@pixelated.com.au>
2024-02-02 18:40:22 +1100

    Refactor date handling and improve SQL efficiency in GetShiftFilledData
    Incorporated Carbon library for date parsing in GetShiftFilledData, improving clarity and ease of maintenance. Also, the underlying SQL query was refactored to address shift availability and location activation, thus optimizing data retrieval for better performance and efficiency.

--- app/Http/Controllers/AdminAvailableShiftsController.php
+++ app/Http/Controllers/AdminAvailableShiftsController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Bugsnag\BugsnagLaravel\Facades\Bugsnag;
 use Carbon\Carbon;
 use Carbon\Exceptions\InvalidFormatException;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Http\Request;
@@ -24,16 +25,33 @@ class AdminAvailableShiftsController extends Controller
         }
 
         try {
-            $selectedDate = Carbon::parse($shiftDate);
+            $selectedDate = Carbon::parse($shiftDate)->format('Y-m-d');
         } catch (InvalidFormatException $e) {
             Bugsnag::notifyException($e);
-            $selectedDate = Carbon::today();
+            $selectedDate = Carbon::today()->format('Y-m-d');
         }
 
         // Locations are the list of locations in the 'accordion' menu
         $locations = Location::with([
             'shifts'       => fn(HasMany $query) => $query
                 ->where('shifts.is_enabled', true)
+                ->where(fn(Builder $query) => $query
+                    ->whereNull('shifts.available_from')
+                    ->orWhere('shifts.available_from', '<=', $selectedDate)
+                )
+                ->where(fn(Builder $query) => $query
+                    ->whereNull('shifts.available_to')
+                    ->orWhere('shifts.available_to', '>=', $selectedDate)
+                )
+                ->whereRaw("CASE
+                                    WHEN DAYOFWEEK('$selectedDate') = 1 THEN shifts.day_sunday
+                                    WHEN DAYOFWEEK('$selectedDate') = 2 THEN shifts.day_monday
+                                    WHEN DAYOFWEEK('$selectedDate') = 3 THEN shifts.day_tuesday
+                                    WHEN DAYOFWEEK('$selectedDate') = 4 THEN shifts.day_wednesday
+                                    WHEN DAYOFWEEK('$selectedDate') = 5 THEN shifts.day_thursday
+                                    WHEN DAYOFWEEK('$selectedDate') = 6 THEN shifts.day_friday
+                                    WHEN DAYOFWEEK('$selectedDate') = 7 THEN shifts.day_saturday
+                                    END = 1")
                 ->orderBy('shifts.start_time'),
             'shifts.users' => fn(BelongsToMany $query) => $query
                 ->where('shift_user.shift_date', '=', $selectedDate)
