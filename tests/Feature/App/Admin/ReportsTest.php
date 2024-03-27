ce645ba31c440cc35604f6943b12330ea0e2a1c3
Ian Heggaton <ian@pixelated.com.au>
2024-03-28 07:13:11 +1100

    Refactor admin report retrieval tests
    Updated the test case 'test_admin_can_retrieve_reports' in the 'ReportsTest' class which was previously skipped. The test now verifies if only admins can retrieve reports and non-admin users are forbidden. It includes necessary factory states to create users, locations, shifts and reports for testing purposes.

--- tests/Feature/App/Admin/ReportsTest.php
+++ tests/Feature/App/Admin/ReportsTest.php
@@ -2,15 +2,56 @@
 
 namespace Tests\Feature\App\Admin;
 
+use App\Models\Location;
+use App\Models\Report;
+use App\Models\Shift;
+use App\Models\ShiftUser;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Inertia\Testing\AssertableInertia;
 use Tests\TestCase;
 
 class ReportsTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_admin_can_retrieve_reports(): void
+    public function test_only_admin_can_retrieve_reports(): void
     {
-        $this->markTestSkipped('Not implemented yet.');
+        $admin = User::factory()->adminRoleUser()->create(['is_enabled' => true]);
+        $user  = User::factory()->userRoleUser()->create(['is_enabled' => true]);
+
+        Location::factory()
+            ->state(['max_volunteers' => 3])
+            ->has(Shift::factory()
+                ->everyDay9am()
+                ->hasAttached(
+                    User::factory()
+                        ->userRoleUser()
+                        ->count(3)
+                        ->state(['is_enabled' => true])
+                    , ['shift_date' => '2023-01-03']
+                )
+            )
+            ->create();
+
+        $shiftIds = ShiftUser::all()->map(fn(ShiftUser $shiftUser) => ['shift_id' => $shiftUser->shift_id]);
+
+        Report::factory()
+            ->count($shiftIds->count())
+            ->sequence(...$shiftIds->toArray())
+            ->create();
+
+        $this->actingAs($admin)
+            ->get("/admin/reports")
+            ->assertSuccessful()
+            ->assertInertia(fn(AssertableInertia $page) => $page
+                ->component('Admin/Reports/List')
+                ->has('reports.data', 3)
+            );
+
+        // Confirm that non-admin cannot access reports
+        $this->actingAs($user)
+            ->get("/admin/reports")
+            ->assertForbidden();
     }
 }
