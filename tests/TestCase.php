c2e1c02cd0389a3941510b70cf6e528338c7ad62
Ian Heggaton <ian@pixelated.com.au>
2024-03-30 07:43:03 +1100

    Remove configuration setter method from ShiftsTest
    The configuration setter method 'setConfig' has been removed from the ShiftsTest file. This method was mainly used to set various configuration options for the 'cart-scheduler' in a testing context. Updates to testing practices and the system have rendered this method unnecessary.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,28 @@
 
 namespace Tests;
 
+use App\Enums\DBPeriod;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Config;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * @param int $duration A number representing days or months. i.e., 14 could = 14 days or 2 could = 2 months
+     * @param \App\Enums\DBPeriod $period Days or Months
+     * @param bool $dailyRelease If true, new shifts are released daily
+     * @param string $dayOfWeekRelease This is only used for month releases. Set to the day shifts are to be released
+     * @param string $timeOfDayRelease What time of the day shall new shifts be released?
+     * @return void
+     */
+    protected function setConfig(int $duration, DBPeriod $period, bool $dailyRelease, string $dayOfWeekRelease, string $timeOfDayRelease): void
+    {
+        Config::set('cart-scheduler.shift_reservation_duration', $duration);
+        Config::set('cart-scheduler.shift_reservation_duration_period', $period->value);
+        Config::set('cart-scheduler.do_release_shifts_daily', $dailyRelease);
+        Config::set('cart-scheduler.release_weekly_shifts_on_day', $dayOfWeekRelease);
+        Config::set('cart-scheduler.release_new_shifts_at_time', $timeOfDayRelease);
+    }
 }
