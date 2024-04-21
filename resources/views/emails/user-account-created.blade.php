3062d72f9548766e95f6140649ed470dbf1d1820
Ian Heggaton <ian@pixelated.com.au>
2024-04-22 09:33:29 +1000

    Update email template to reflect the change in terminology from 'website' to 'web application' and 'Account Created' to 'Account Activation'.

--- resources/views/emails/user-account-created.blade.php
+++ resources/views/emails/user-account-created.blade.php
@@ -1,9 +1,9 @@
 {{--@formatter:off--}}
 @component('mail::message')
 
-# {{ config('app.name') }} Account Created
+# {{ config('app.name') }} Account Activation
 
-Dear {{$user->name}}, an account has been created for you on the {{ config('app.name') }} Public Witnessing website.
+Dear {{$user->name}}, an account has been created for you on the {{ config('app.name') }} Public Witnessing web application.
 
 If you feel this is an error, please [contact us immediately](mailto:{{ config('mail.support.address') }}?subject=Mistaken%20User%20Account). Otherwise, please use the verification link below.
 
