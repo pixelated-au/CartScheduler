2e8c99bc080acc030042bded8510cd350900d650
Ian Heggaton <ian@pixelated.com.au>
2024-04-22 13:32:47 +1000

    Update admin user update validation logic
    Updated the logic of "IsAdminForUpdateOfUserAction" method and also modified the exceptions thrown. Now, the action checks if the user is an Admin before attempting to update another user's profile. An 'Unauthorized' response is thrown if the user doesn't exist or is not an Admin.

--- app/Actions/IsAdminForUpdateOfUserAction.php
+++ app/Actions/IsAdminForUpdateOfUserAction.php
@@ -2,15 +2,18 @@
 
 namespace App\Actions;
 
+use App\Enums\Role;
 use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Response;
 
 class IsAdminForUpdateOfUserAction
 {
     /**
      * @param \Illuminate\Foundation\Http\FormRequest $request - Expects an option 'user_id' to be set if admin is editing another user's availability.
      * @return array{isAdminEdit: bool, user: User} Returns an associative array with 'isAdminEdit' indicating if admin is editing the user, and 'user' as the User instance.
-     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
      */
     public function execute(FormRequest $request): array
     {
@@ -18,7 +21,10 @@ class IsAdminForUpdateOfUserAction
         if ($request->validated('user_id')) {
             // this means admin is updating another user's availability
             $isAdminEdit = true;
-            $user        = User::findorfail($request->validated('user_id'));
+            $user        = User::where('role', '=', Role::Admin)
+                ->find($request->validated('user_id'));
+
+            abort_if(!$user, Response::HTTP_UNAUTHORIZED, 'Unauthorized');
         } else {
             $user = $request->user();
         }
