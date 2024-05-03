4b7f0e367749ee600290810fdb5503bfeae212dd
Ian Heggaton <ian@pixelated.com.au>
2024-05-04 07:47:42 +1000

    Add feature toggle for user availability
    This commit adds a feature toggle for user availability in the ShowUserAvailabilityController. If the feature is not enabled in the GeneralSettings, a validation exception is thrown. Additionally, a refresh method has been added to ensure complete user availability data is retrieved.

--- app/Http/Controllers/ShowUserAvailabilityController.php
+++ app/Http/Controllers/ShowUserAvailabilityController.php
@@ -5,22 +5,34 @@ namespace App\Http\Controllers;
 use App\Http\Resources\AvailabilityResource;
 use App\Http\Resources\UserVacationResource;
 use App\Models\UserAvailability;
+use App\Settings\GeneralSettings;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 use Inertia\Inertia;
 
 class ShowUserAvailabilityController extends Controller
 {
+    /**
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     * @throws \Illuminate\Validation\ValidationException
+     */
     public function __invoke(Request $request)
     {
+        $settings = app()->make(GeneralSettings::class);
+        if (!$settings->enableUserAvailability) {
+            throw ValidationException::withMessages(['featureDisabled' => 'Choosing availability is not enabled.']);
+        }
+
         /** @var \App\Models\User $user */
         $user = $request->user();
         $this->authorize('view', $user);
 
-        $availability = UserAvailability::firstOrCreate(['user_id' => $user->id]);
+        $availability = UserAvailability::firstOrCreate(['user_id' => $user->id])->refresh(); // refresh(), otherwise, the we get incomplete data
 
         return Inertia::render('Profile/ShowAvailability', [
-            'vacations' => UserVacationResource::collection($user->vacations->all()),
-            'availability' => AvailabilityResource::make($availability),
+            'vacations'         => UserVacationResource::collection($user->vacations->all()),
+            'availability'      => AvailabilityResource::make($availability),
             'selectedLocations' => $user->rosterLocations->pluck('id'),
         ]);
     }
