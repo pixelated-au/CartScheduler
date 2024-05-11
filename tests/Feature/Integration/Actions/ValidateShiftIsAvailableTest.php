912d7ad931fe15054d8a0d9d18bd4cb78f4fbb6e
Ian Heggaton <ian@pixelated.com.au>
2024-05-12 06:25:26 +1000

    Add shift availability validation test
    A new test file has been added to validate shift availability. The test checks for shift dates within the range of availability and expects not to throw an exception, and it also checks for dates outside this range and expects to throw a ShiftAvailabilityException.

---
+++ tests/Feature/Integration/Actions/ValidateShiftIsAvailableTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Integration\Actions;
+
+use App\Actions\ValidateShiftIsAvailableAction;
+use App\Exceptions\ShiftAvailabilityException;
+use App\Models\Location;
+use App\Models\Shift;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Carbon;
+use Tests\TestCase;
+
+class ValidateShiftIsAvailableTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private ValidateShiftIsAvailableAction $validateShiftIsAvailableAction;
+
+    protected function setUp(): void
+    {
+        $this->validateShiftIsAvailableAction = new ValidateShiftIsAvailableAction();
+        parent::setUp();
+    }
+
+    public function test_dates_within_shift_availability_are_working(): void
+    {
+        $startDate = Carbon::createFromTimeString('2023-10-01T12:00:00');
+        $endDate   = $startDate->clone()->endOfMonth();
+
+        $location = Location::factory()->create();
+
+        $shift = Shift::factory()
+            ->everyDay9am()
+            ->state([
+                'available_from' => $startDate->toDateString(),
+                'available_to'   => $endDate->toDateString()
+            ])
+            ->for($location)
+            ->create();
+
+        $this->expectNotToPerformAssertions();
+        $this->validateShiftIsAvailableAction->execute($shift, $startDate);
+        $this->validateShiftIsAvailableAction->execute($shift, $startDate->addDay());
+        $this->validateShiftIsAvailableAction->execute($shift, $startDate->setDay(15));
+        $this->validateShiftIsAvailableAction->execute($shift, $startDate->endOfMonth());
+        $this->validateShiftIsAvailableAction->execute($shift, $startDate->subDay());
+    }
+
+    public function test_dates_before_and_after_shift_availability_are_failing(): void
+    {
+        $startDate = Carbon::createFromTimeString('2023-10-01T12:00:00');
+        $endDate   = $startDate->clone()->endOfMonth();
+
+        $location = Location::factory()->create();
+
+        $shift = Shift::factory()
+            ->everyDay9am()
+            ->state([
+                'available_from' => $startDate->toDateString(),
+                'available_to'   => $endDate->toDateString()
+            ])
+            ->for($location)
+            ->create();
+
+
+        $this->assertThrows(
+            fn() => $this->validateShiftIsAvailableAction->execute($shift, $startDate->clone()->subDay()),
+            ShiftAvailabilityException::class,
+            ShiftAvailabilityException::notAvailableYet()->getMessage()
+        );
+
+        $this->assertThrows(
+            fn() => $this->validateShiftIsAvailableAction->execute($shift, $startDate->clone()->endOfMonth()->addDay()),
+            ShiftAvailabilityException::class,
+            ShiftAvailabilityException::notAvailableAnymore()->getMessage()
+        );
+    }
+}
