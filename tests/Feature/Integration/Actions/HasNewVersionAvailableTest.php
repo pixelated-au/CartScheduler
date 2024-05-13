cedad7d10be7f0e26198416cc862536081712460
Ian Heggaton <ian@pixelated.com.au>
2024-05-13 15:42:52 +1000

    Add test for HasNewVersionAvailable action
    A new unit test has been added for the HasNewVersionAvailable action. This test primarily checks whether the action is correctly identifying when a new version is available.

---
+++ tests/Feature/Integration/Actions/HasNewVersionAvailableTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Integration\Actions;
+
+use App\Actions\HasNewVersionAvailable;
+use App\Settings\GeneralSettings;
+use Tests\TestCase;
+
+class HasNewVersionAvailableTest extends TestCase
+{
+    private HasNewVersionAvailable $hasNewVersionAvailable;
+    private GeneralSettings $settings;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->hasNewVersionAvailable = $this->app->make(HasNewVersionAvailable::class);
+        $this->settings               = $this->app->make(GeneralSettings::class);
+    }
+
+    public function test_has_new_version_available_is_returning_correct_data(): void
+    {
+        $this->settings->availableVersion = '1.0.0';
+        $this->settings->currentVersion   = '1.0.0';
+        $this->settings->save();
+
+        $this->assertFalse($this->hasNewVersionAvailable->execute());
+
+        $this->settings->availableVersion = '1.0.1';
+        $this->settings->save();
+
+        $this->assertTrue($this->hasNewVersionAvailable->execute());
+    }
+}
