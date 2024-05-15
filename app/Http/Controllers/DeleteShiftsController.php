4295a7c5ebae5bf83644b109be4dc17b310925fb
Ian Heggaton <ian@pixelated.com.au>
2024-05-15 16:59:52 +1000

    Remove unused import and simplify shift deletion method in DeleteShiftsController.php.

--- app/Http/Controllers/DeleteShiftsController.php
+++ app/Http/Controllers/DeleteShiftsController.php
@@ -1,24 +1,15 @@
 <?php
-/**
- * Project: ${PROJECT_NAME}
- * Owner: Pixelated
- * Copyright: 2022
- */
 
 namespace App\Http\Controllers;
 
 use App\Models\Shift;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class DeleteShiftsController extends Controller
 {
     public function __invoke(Shift $shift)
     {
-        if ($shift->delete()) {
-            return response('', Response::HTTP_NO_CONTENT);
-        }
-
-        return response('Shift could not be deleted! Try again or please contact developer.', Response::HTTP_INTERNAL_SERVER_ERROR);
+        $shift->delete();
+        return response('', Response::HTTP_NO_CONTENT);
     }
 }
