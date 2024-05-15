ab62355ab72e0ce618230e5f3dd08dbc4f811aaf
Ian Heggaton <ian@pixelated.com.au>
2024-05-15 15:28:31 +1000

    Introduce error codes in SetUserPasswordController
    The SetUserPasswordController now uses ObfuscatedErrorCode for validation error messaging. This update improves the validation and error handling in the 'update' method. Additionally, minor optimization has been made for 'show' method where the user object is streamlined before being passed to the template.

--- app/Http/Controllers/SetUserPasswordController.php
+++ app/Http/Controllers/SetUserPasswordController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Interfaces\ObfuscatedErrorCode;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -13,6 +14,14 @@ use Inertia\Response;
 
 class SetUserPasswordController extends Controller
 {
+    public function __construct(private readonly ObfuscatedErrorCode $errorCodeAction)
+    {
+    }
+
+    /**
+     * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     */
     public function show(User $user, string $hashedEmail): Response|RedirectResponse
     {
         if (Auth::user()) {
@@ -28,31 +37,42 @@ class SetUserPasswordController extends Controller
         }
 
         return Inertia::render('Profile/SetPassword', [
-            'editUser'    => $user,
+            'editUser'    => ['id' => $user->id, 'name' => $user->name],
             'hashedEmail' => $hashedEmail,
             'siteName'    => config('app.name'),
         ]);
     }
 
+    /**
+     * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     * @throws \Illuminate\Validation\ValidationException
+     */
     public function update(Request $request): RedirectResponse
     {
         $data = $this->validate($request, [
-            'password'     => ['required', 'confirmed'],
-            'hashed_email' => ['required', 'string'],
-            'user_id'      => ['required', 'integer'],
-        ]);
+            'password_confirmation' => ['required'],
+            'password'              => ['required', 'confirmed'],
+            'hashed_email'          => ['required', 'string'],
+            'user_id'               => ['required', 'integer', 'exists:users,id'],
+        ],
+            [
+                'hashed_email.required' => $this->errorCodeAction->errorCode(100),
+                'hashed_email.string'   => $this->errorCodeAction->errorCode(200),
+                'user_id.required'      => $this->errorCodeAction->errorCode(300),
+                'user_id.integer'       => $this->errorCodeAction->errorCode(400),
+                'user_id.exists'        => $this->errorCodeAction->errorCode(500),
+            ]);
         $user = User::findOrFail($data['user_id']);
 
         if (!Hash::check($user->uuid . $user->email, base64_decode($data['hashed_email']))) {
             abort(404);
         }
-
         $user->update([
             'password' => Hash::make($data['password']),
         ]);
-
         session()->flash('flash.setPassword', "Your password has been set. Please use it to log in.");
-
         return Redirect::route('login');
     }
 }
