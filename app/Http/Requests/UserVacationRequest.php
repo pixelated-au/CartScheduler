5154b21e4d55d6d9a6ebca71e9f75c32b45e47a5
Ian Heggaton <ian@pixelated.com.au>
2024-05-17 09:32:35 +1000

    Refine request validation in UserVacationRequest
    Updated UserVacationRequest to include additional validation rules. These rules prevent non-admin users from modifying other users' vacation data. Added new error messages to correspond with these validation rules.

--- app/Http/Requests/UserVacationRequest.php
+++ app/Http/Requests/UserVacationRequest.php
@@ -3,18 +3,34 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UserVacationRequest extends FormRequest
 {
     public function rules(): array
     {
         return [
-            'user_id'                 => ['integer', 'exists:users,id'],
-            'vacations.*.id'          => ['nullable', 'integer', 'exists:user_vacations,id'],
+            'user_id'                 => ['integer', 'exists:users,id', Rule::prohibitedIf($this->user()->role !== 'admin')],
+            'vacations.*.id'          => [
+                'nullable',
+                'integer',
+                Rule::when(
+                    fn() => $this->user()->role === 'admin',
+                    static fn() => 'exists:user_vacations,id,user_id',
+                    fn() => Rule::exists('user_vacations', 'id')->where('user_id', $this->user()->id),
+                )
+            ],
             'vacations.*.start_date'  => ['required', 'date'],
             'vacations.*.end_date'    => ['required', 'date'],
             'vacations.*.description' => ['nullable', 'string', 'max:255'],
-            'deletedVacations.*.id'   => ['required', 'integer', 'exists:user_vacations,id'],
+            'deletedVacations.*.id'   => [
+                'required',
+                'integer',
+                Rule::when(
+                    fn() => $this->user()->role === 'admin',
+                    static fn() => 'exists:user_vacations,id,user_id',
+                    fn() => Rule::exists('user_vacations', 'id')->where('user_id', $this->user()->id),
+                )],
         ];
     }
 
@@ -26,11 +42,14 @@ class UserVacationRequest extends FormRequest
     public function messages(): array
     {
         return [
+            'user_id.prohibited'              => "You cannot update another user's vacations",
+            'vacations.*.id.exists'           => 'Vacation ID is invalid',
             'vacations.*.start_date.required' => 'Start date is required',
             'vacations.*.end_date.required'   => 'End date is required',
             'vacations.*.start_date.date'     => 'Start date is not a date',
             'vacations.*.end_date.date'       => 'End date is not a date',
-            'vacations.*.description.max'     => 'Description is too long. Please keep it brief'
+            'vacations.*.description.max'     => 'Description is too long. Please keep it brief',
+            'deletedVacations.*.id.exists'    => 'Vacation ID is invalid',
         ];
     }
 }
