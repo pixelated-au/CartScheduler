af84fc06a0ef5a43a75a3c960e552af7bf43fefa
Ian Heggaton <ian@pixelated.com.au>
2024-05-16 12:43:01 +1000

    Refactor AdminAvailableShiftsController to improve date handling
    Removed unused User model and simplified date parsing and formatting procedures in AdminAvailableShiftsController. This also includes the removal of unnecessary user authorization checks. Moreover, the calculation of 'freeShiftsCount' has been updated to use the updated date objects.

--- app/Http/Controllers/AdminAvailableShiftsController.php
+++ app/Http/Controllers/AdminAvailableShiftsController.php
@@ -5,14 +5,13 @@ namespace App\Http\Controllers;
 use App\Actions\GetAvailableShiftsCount;
 use App\Http\Resources\LocationResource;
 use App\Models\Location;
-use App\Models\User;
 use Bugsnag\BugsnagLaravel\Facades\Bugsnag;
-use Carbon\Carbon;
 use Carbon\Exceptions\InvalidFormatException;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class AdminAvailableShiftsController extends Controller
 {
@@ -22,18 +21,13 @@ class AdminAvailableShiftsController extends Controller
 
     public function __invoke(Request $request, string $shiftDate)
     {
-        /** @var User $user */
-        $user = $request->user();
-        if (!$user) {
-            abort(403);
-        }
-
         try {
-            $selectedDate = Carbon::parse($shiftDate)->format('Y-m-d');
+            $selectedDate = Carbon::parse($shiftDate);
         } catch (InvalidFormatException $e) {
             Bugsnag::notifyException($e);
-            $selectedDate = Carbon::today()->format('Y-m-d');
+            $selectedDate = Carbon::today();
         }
+        $formattedDate = $selectedDate->format('Y-m-d');
 
         // Locations are the list of locations in the 'accordion' menu
         $locations = Location::with([
@@ -41,20 +35,20 @@ class AdminAvailableShiftsController extends Controller
                 ->where('shifts.is_enabled', true)
                 ->where(fn(Builder $query) => $query
                     ->whereNull('shifts.available_from')
-                    ->orWhere('shifts.available_from', '<=', $selectedDate)
+                    ->orWhere('shifts.available_from', '<=', $formattedDate)
                 )
                 ->where(fn(Builder $query) => $query
                     ->whereNull('shifts.available_to')
-                    ->orWhere('shifts.available_to', '>=', $selectedDate)
+                    ->orWhere('shifts.available_to', '>=', $formattedDate)
                 )
                 ->whereRaw("CASE
-                                    WHEN DAYOFWEEK('$selectedDate') = 1 THEN shifts.day_sunday
-                                    WHEN DAYOFWEEK('$selectedDate') = 2 THEN shifts.day_monday
-                                    WHEN DAYOFWEEK('$selectedDate') = 3 THEN shifts.day_tuesday
-                                    WHEN DAYOFWEEK('$selectedDate') = 4 THEN shifts.day_wednesday
-                                    WHEN DAYOFWEEK('$selectedDate') = 5 THEN shifts.day_thursday
-                                    WHEN DAYOFWEEK('$selectedDate') = 6 THEN shifts.day_friday
-                                    WHEN DAYOFWEEK('$selectedDate') = 7 THEN shifts.day_saturday
+                                    WHEN DAYOFWEEK('$formattedDate') = 1 THEN shifts.day_sunday
+                                    WHEN DAYOFWEEK('$formattedDate') = 2 THEN shifts.day_monday
+                                    WHEN DAYOFWEEK('$formattedDate') = 3 THEN shifts.day_tuesday
+                                    WHEN DAYOFWEEK('$formattedDate') = 4 THEN shifts.day_wednesday
+                                    WHEN DAYOFWEEK('$formattedDate') = 5 THEN shifts.day_thursday
+                                    WHEN DAYOFWEEK('$formattedDate') = 6 THEN shifts.day_friday
+                                    WHEN DAYOFWEEK('$formattedDate') = 7 THEN shifts.day_saturday
                                     END = 1")
                 ->orderBy('shifts.start_time'),
             'shifts.users' => fn(BelongsToMany $query) => $query
@@ -63,12 +57,10 @@ class AdminAvailableShiftsController extends Controller
             ->where('is_enabled', true)
             ->get();
 
-        $startDate       = Carbon::today()->format('Y-m-d');
-        $endDate         = Carbon::today()->addMonths(4)->endOfMonth()->format('Y-m-d');
-        $freeShiftsCount = $user->is_unrestricted
-            ? $this->getAvailableShiftsCount->execute($startDate, $endDate)
-            : [];
+        $selectedDate->startOfMonth();
+        $endDate = $selectedDate->clone()->endOfMonth();
 
+        $freeShiftsCount = $this->getAvailableShiftsCount->execute($selectedDate->format('Y-m-d'), $endDate->format('Y-m-d'));
 
         return [
             'freeShifts' => $freeShiftsCount,
