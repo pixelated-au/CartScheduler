47dc25bc363455919cd9a42f755b84879d8ad99e
Ian Heggaton <ian@pixelated.com.au>
2024-08-09 13:30:13 +1000

    Update locations table to use Schema facade for TINYINT changes.
    Replaced direct DB statements with Schema facade methods for altering `min_volunteers` and `max_volunteers` columns to TINYINT UNSIGNED.

--- database/migrations/2023_06_30_102057_update_locations_table.php
+++ database/migrations/2023_06_30_102057_update_locations_table.php
@@ -7,12 +7,9 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-//        Schema::table('locations', function (Blueprint $table) {
-//            $table->unsignedTinyInteger('min_volunteers')->change();
-//            $table->unsignedTinyInteger('max_volunteers')->change();
-//        });
-        // This is done explicitly because the built-in Laravel (and Doctrine/DBAL) doesn't support TINYINT
-        DB::statement("ALTER TABLE `locations` CHANGE `min_volunteers` `min_volunteers` TINYINT UNSIGNED NOT NULL");
-        DB::statement("ALTER TABLE `locations` CHANGE `max_volunteers` `max_volunteers` TINYINT UNSIGNED NOT NULL");
+        Schema::table('locations', static function (Blueprint $table) {
+            $table->unsignedTinyInteger('min_volunteers')->change();
+            $table->unsignedTinyInteger('max_volunteers')->change();
+        });
     }
 };
