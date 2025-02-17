de3b6dfbb3841df0b8c989e4dc1b3b76118060d3
Ian Heggaton <ian@pixelated.com.au>
2025-02-17 14:24:55 +1100

    Refactor ToggleUserOntoShift class with readonly property and error handling
    - Mark ToggleUserOntoShift as readonly
    - Remove readonly modifier from DoShiftReservation property
    - Adjust parameter formatting in constructor
    - Add LockTimeoutException to method docblock
    - Update shiftDate assignment with null check and exception handling

--- app/Actions/ToggleUserOntoShift.php
+++ app/Actions/ToggleUserOntoShift.php
@@ -13,17 +13,17 @@ use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Cache;
 use RuntimeException;
 
-class ToggleUserOntoShift
+readonly class ToggleUserOntoShift
 {
-    public function __construct(private readonly DoShiftReservation $doShiftReservation)
+    public function __construct(private DoShiftReservation $doShiftReservation)
     {
     }
 
     /**
      * Execute the shift reservation or unreserve the shift based on the given data.
      *
-     * @param User $user The user performing the shift reservation or un-reservation.
-     * @param array{user?: int, date: string, shift: int, location: int, do_reserve: bool } $data The data required for the shift reservation or un-reservation.
+     * @param  User  $user  The user performing the shift reservation or un-reservation.
+     * @param  array{user?: int, date: string, shift: int, location: int, do_reserve: bool }  $data  The data required for the shift reservation or un-reservation.
      *     The array should contain the following keys:
      *     - 'user': (optional) The ID of the user to toggle onto the shift. If not provided, the current user ID will be used.
      *     - 'date': The date of the shift in the format 'Y-m-d'.
@@ -35,18 +35,26 @@ class ToggleUserOntoShift
      *     - ToggleReservationStatus::RESERVATION_MADE: The shift was successfully reserved.
      *     - ToggleReservationStatus::NO_AVAILABLE_SHIFTS: No available shifts to reserve.
      *     - ToggleReservationStatus::RESERVATION_REMOVED: The shift was successfully unreserved.
+     * @throws \Illuminate\Contracts\Cache\LockTimeoutException
      */
     public function execute(User $user, array $data): ToggleReservationStatus
     {
         return Cache::lock('shift_reservation', 10)->block(10, function () use ($user, $data) {
             // If 'data' contains a user key, toggle that user onto a shift. Otherwise, toggle the current user.
             $userIdToToggle = $data['user'] ?? $user->id;
-            $shiftDate      = Carbon::createFromFormat('Y-m-d', $data['date'])->setTime(12, 0);
+            $shiftDate      = Carbon::createFromFormat('Y-m-d', $data['date'])?->setTime(12, 0);
+            if (is_null($shiftDate)) {
+                throw new RuntimeException("Invalid date format. Expected a format of 'Y-m-d'.");
+            }
 
+            /** @var \App\Models\Location $location */
             $location = Location::with([
+// TODO                BUG It looks like the front-end is possibly sending the wrong shift id if the shift day isnt enabled
                 'shifts'       => fn(HasMany $query) => $query->where('shifts.id', '=', $data['shift']),
                 'shifts.users' => fn(BelongsToMany $query) => $query
                     ->wherePivot('shift_date', $shiftDate->toDateString()),
+//                THE QUERY NEEDS TO CHECK IF THE SHIFT IS AVAILABLE AT THE SAME TIME AND THE DAY IS ENABLED. IE THE SHIFT THAT THE ADMIN/USER EXPECTS
+                //EG. SHIFT 1 AVAILABLE MON-FRI 9AM - 12 SHIFT 2 AVAILABLE SAT-SUN 9AM - 12. ADMIN ASSIGNS A SHIFT TO A USER FOR SATURDAY, IT LOOKS LIKE THEY'LL BE ASSIGNED TO SHIFT 1 WHEREAS THEY SHOULD BE ASSIGNED TO SHIFT 2
             ])->findOrFail($data['location']);
 
             /** @var Shift $shift */
