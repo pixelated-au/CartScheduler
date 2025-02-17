f6fb9d668fae4758cf9f42fb3253eb0035c7c728
Ian Heggaton <ian@pixelated.com.au>
2025-02-17 14:22:36 +1100

    Refactor MoveUserToNewShiftController to use MoveUserToNewShiftRequest
    - Update constructor to streamline parameter formatting
    - Replace Request with MoveUserToNewShiftRequest in method signature
    - Remove redundant oldShift and location queries
    - Simplify location query logic by eliminating unnecessary conditions
    - Adjust shift validation actions to use newShift instead of old shift logic

--- app/Http/Controllers/MoveUserToNewShiftController.php
+++ app/Http/Controllers/MoveUserToNewShiftController.php
@@ -8,90 +8,51 @@ use App\Actions\ValidateShiftIsAvailableAction;
 use App\Actions\ValidateShiftIsNotFullAction;
 use App\Actions\ValidateVolunteerIsAllowedToBeRosteredAction;
 use App\Exceptions\ShiftAvailabilityException;
+use App\Http\Requests\MoveUserToNewShiftRequest;
 use App\Models\Location;
-use App\Models\Shift;
 use App\Models\User;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class MoveUserToNewShiftController extends Controller
 {
     public function __construct(
-        private readonly DoShiftReservation                           $doShiftReservation,
-        private readonly ValidateShiftIsAvailableAction               $validateShiftIsAvailableAction,
+        private readonly DoShiftReservation $doShiftReservation,
+        private readonly ValidateShiftIsAvailableAction $validateShiftIsAvailableAction,
         private readonly ValidateVolunteerIsAllowedToBeRosteredAction $validateVolunteerIsAllowedToBeRosteredAction,
-        private readonly ValidateShiftIsNotFullAction                 $validateShiftIsNotFullAction,
-    )
-    {
+        private readonly ValidateShiftIsNotFullAction $validateShiftIsNotFullAction,
+    ) {
     }
 
-    public function __invoke(Request $request)
+    public function __invoke(MoveUserToNewShiftRequest $request)
     {
-        $request->validate([
-            'user_id'      => ['required', 'exists:users,id'],
-            'location_id'  => ['required', 'exists:locations,id'],
-            'date'         => ['required', 'date'],
-            'old_shift_id' => ['required', 'exists:shifts,id'],
-        ]);
+        $oldShift = $request->oldShift();
+        $newShift = $request->newShift();
 
-        $oldShift  = Shift::find($request->get('old_shift_id'));
         $date      = $request->date('date')?->midDay();
-        $startTime = $date->copy()->setTimeFromTimeString($oldShift->start_time);
         $userId    = $request->integer('user_id');
 
-        $range = [
-            $startTime->copy()->sub(45, 'minutes')->format('H:i:s'),
-            $startTime->copy()->add(45, 'minutes')->format('H:i:s'),
-        ];
-
-        $location = Location::with([
-            'shifts'       => fn(HasMany $query) => $query->whereBetween('start_time', $range)
-                ->where('shifts.is_enabled', true)
-                ->where(fn(Builder $query) => $query
-                    ->whereNull('shifts.available_from')
-                    ->orWhere('shifts.available_from', '<=', $date)
-                )
-                ->where(fn(Builder $query) => $query
-                    ->whereNull('shifts.available_to')
-                    ->orWhere('shifts.available_to', '>=', $date)
-                ),
-            'shifts.users' => fn(BelongsToMany $query) => $query->wherePivot('shift_date', $date->toDateString()),
-        ])
-            ->where('id', $request->get('location_id'))
+        $location = Location::where('id', $request->get('location_id'))
             ->where('is_enabled', true)
             ->first();
 
         try {
-
-            if (!$location) {
-                return ErrorApiResource::create('Location not found',
-                    ErrorApiResource::CODE_LOCATION_NOT_FOUND, 422);
-            }
-
-            $shift = $location->shifts->first(); // Should only be 1 matching shift due to the whereBetween() query above.
-            if (!$shift) {
-                return ErrorApiResource::create('No shift found for this location at this time',
-                    ErrorApiResource::CODE_SHIFT_NOT_FOUND, 422);
-            }
-
-            $this->validateShiftIsAvailableAction->execute($shift, $date);
+            // TODO much of this can be moved into the FormRequest and maybe a custom validation rule
+            $this->validateShiftIsAvailableAction->execute($newShift, $date);
 
             $user      = User::find($userId);
-            $isAllowed = $this->validateVolunteerIsAllowedToBeRosteredAction->execute($location, $user, $shift->users);
+            $isAllowed = $this->validateVolunteerIsAllowedToBeRosteredAction->execute($location, $user,
+                $newShift->users);
             if (is_string($isAllowed)) {
                 return ErrorApiResource::create($isAllowed, ErrorApiResource::CODE_BROTHER_REQUIRED, 422);
             }
 
-            $this->validateShiftIsNotFullAction->execute($shift, $date);
+            $this->validateShiftIsNotFullAction->execute($newShift, $date);
 
             DB::transaction(
-                function () use ($date, $location, $shift, $userId, $oldShift) {
+                function () use ($date, $location, $newShift, $userId, $oldShift) {
                     $oldShift->detachUserOnDate($userId, $date);
 
-                    $this->doShiftReservation->execute($shift, $location, $userId, $date);
+                    $this->doShiftReservation->execute($newShift, $location, $userId, $date);
                 });
         } catch (ShiftAvailabilityException $e) {
             return ErrorApiResource::create($e->getMessage(), $e->getExceptionType(), 422);
