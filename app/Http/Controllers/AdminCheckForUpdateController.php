20d2646368ef35effbd85f083e28fa85fd83f8c3
Ian Heggaton <ian@pixelated.com.au>
2025-02-21 11:27:01 +1100

    Refactor AdminCheckForUpdateController to use CheckAvailableVersions
    - Replace Artisan call with CheckAvailableVersions action
    - Update constructor to include CheckAvailableVersions dependency
    - Modify logic for version comparison and updating settings
    - Ensure currentVersion is updated from config if applicable

--- app/Http/Controllers/AdminCheckForUpdateController.php
+++ app/Http/Controllers/AdminCheckForUpdateController.php
@@ -4,24 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Settings\GeneralSettings;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Artisan;
+use Pixelated\Streamline\Actions\CheckAvailableVersions;
 
 class AdminCheckForUpdateController extends Controller
 {
-    public function __construct(private readonly GeneralSettings $settings)
-    {
+    public function __construct(
+        private readonly GeneralSettings $settings,
+        private readonly CheckAvailableVersions $availableVersions,
+    ) {
     }
 
     public function __invoke(Request $request)
     {
-        $old = $this->settings->availableVersion;
-        $params = [];
-        $beta = $request->boolean('beta');
-        if ($beta) {
-            $params['--beta'] = true;
+        $old  = config('streamline.installed_version');
+        // Ensure the general settings current version is up to date,
+        if ($old && $this->settings->currentVersion && version_compare($old, $this->settings->currentVersion, '<')) {
+            $this->settings->currentVersion = $old;
         }
-        Artisan::call('cart-scheduler:has-update', $params);
-        $new = $this->settings->availableVersion;
+
+        $new = $this->availableVersions->execute(ignorePreReleases: $request->boolean('beta'));
+        $this->settings->availableVersion = $new;
+        $this->settings->save();
+
         return version_compare($old, $new, '!=');
     }
 }
