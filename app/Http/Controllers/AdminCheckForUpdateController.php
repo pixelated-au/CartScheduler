328afc4f94ebff3b41a42ae74d76d0c823c0ed83
Ian Heggaton <ian@pixelated.com.au>
2025-02-25 07:07:43 +1100

    Refactor AdminCheckForUpdateController to streamline version checks and remove unused GeneralSettings dependency

--- app/Http/Controllers/AdminCheckForUpdateController.php
+++ app/Http/Controllers/AdminCheckForUpdateController.php
@@ -2,30 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Settings\GeneralSettings;
 use Illuminate\Http\Request;
 use Pixelated\Streamline\Actions\CheckAvailableVersions;
 
 class AdminCheckForUpdateController extends Controller
 {
     public function __construct(
-        private readonly GeneralSettings $settings,
         private readonly CheckAvailableVersions $availableVersions,
     ) {
     }
 
     public function __invoke(Request $request)
     {
-        $old  = config('streamline.installed_version');
-        // Ensure the general settings current version is up to date,
-        if ($old && $this->settings->currentVersion && version_compare($old, $this->settings->currentVersion, '<')) {
-            $this->settings->currentVersion = $old;
-        }
-
-        $new = $this->availableVersions->execute(ignorePreReleases: $request->boolean('beta'));
-        $this->settings->availableVersion = $new;
-        $this->settings->save();
-
+        $old = config('streamline.installed_version');
+        $new = $this->availableVersions->execute(ignorePreReleases: !$request->boolean('beta'));
         return version_compare($old, $new, '!=');
     }
 }
