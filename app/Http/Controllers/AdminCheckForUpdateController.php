2a4e185da50c3e327209ba4d1d510363510c67d9
Ian Heggaton <ian@pixelated.com.au>
2025-03-06 15:42:31 +1100

    Refactor AdminCheckForUpdateController to force version check execution

--- app/Http/Controllers/AdminCheckForUpdateController.php
+++ app/Http/Controllers/AdminCheckForUpdateController.php
@@ -9,14 +9,14 @@ class AdminCheckForUpdateController extends Controller
 {
     public function __construct(
         private readonly CheckAvailableVersions $availableVersions,
-    ) {
-    }
+    ) {}
 
     public function __invoke(Request $request)
     {
         $old = ltrim(config('streamline.installed_version'), 'v');
-        $new = $this->availableVersions->execute(ignorePreReleases: !$request->boolean('beta'));
+        $new = $this->availableVersions->execute(force: true, ignorePreReleases: ! $request->boolean('beta'));
         $new = ltrim($new, 'v');
+
         return version_compare($old, $new, '<');
     }
 }
