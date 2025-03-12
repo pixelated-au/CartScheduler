53e63435a73c99c8a667aeb1e8fc66d62e1dbb8c
Ian Heggaton <ian@pixelated.com.au>
2025-03-12 11:58:41 +1100

    Refactor AdminRunSoftwareUpdateController to improve stream handling and local environment response
    - Add localResponse method for handling local environment updates
    - Use StreamOutput and StreamedResponse for efficient output streaming
    - Refactor runUpdateCommand for cleaner callback management
    - Ensure proper error logging and stream closure in exception handling

--- app/Http/Controllers/AdminRunSoftwareUpdateController.php
+++ app/Http/Controllers/AdminRunSoftwareUpdateController.php
@@ -11,13 +11,14 @@ use Illuminate\Support\Facades\Response;
 use Log;
 use RuntimeException;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\StreamOutput;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class AdminRunSoftwareUpdateController extends Controller
 {
     public function __construct(
         private readonly GeneralSettings $settings,
-    ) {
-    }
+    ) {}
 
     public function __invoke()
     {
@@ -26,51 +27,76 @@ class AdminRunSoftwareUpdateController extends Controller
             ob_clean();
         }
 
+        if (config('app.env') === 'local') {
+            return $this->localResponse();
+        }
+
+        $available = $this->settings->availableVersion;
+        // Create stream
+        $stream = fopen('php://output', 'wb');
+
+        $output = App::make(
+            abstract: FilterAnsiEscapeSequencesStreamedOutput::class,
+            parameters: [
+                'stream' => $stream,
+                'verbosity' => OutputInterface::VERBOSITY_VERBOSE,
+                'decorated' => true,
+            ]
+        );
+
         return Response::stream(
-            callback: function () {
-                try {
-                    $available = $this->settings->availableVersion;
-                    // Create stream
-                    $stream = fopen('php://output', 'wb');
-
-                    $output = App::make(
-                        abstract: FilterAnsiEscapeSequencesStreamedOutput::class,
-                        parameters: [
-                            'stream'    => $stream,
-                            'verbosity' => OutputInterface::VERBOSITY_VERBOSE,
-                            'decorated' => true,
-                        ]
-                    );
-
-                    $output->writeln("Running Software Update... (Version: $available).");
-                    $output->writeln("NOTE: THIS MAY TAKE A WHILE...");
-
-                    $params    = ['--force' => true, '--install-version' => $available];
-                    $exitCode = Artisan::call('streamline:run-update', $params, $output);
-
-                    if ($exitCode !== 0) {
-                        throw new RuntimeException("Command failed");
-                    }
-
-                    fclose($stream);
-                } catch (Exception $e) {
-
-                    // Log the error
-                    Log::error('Command streaming failed: ' . $e->getMessage());
-
-                    // Output error message to stream
-                    echo "Error: " . $e->getMessage();
-
-                    // Ensure stream is closed
-                    if (isset($stream) && is_resource($stream)) {
-                        fclose($stream);
-                    }
-                }
-            },
+            callback: $this->runUpdateCommand($output, $available, $stream),
             headers: [
-                'Content-Type'      => 'text/plain',
+                'Content-Type' => 'text/plain',
                 'X-Accel-Buffering' => 'no',
-                'Cache-Control'     => 'no-cache',
+                'Cache-Control' => 'no-cache',
             ]);
+    }
+
+    public function localResponse(): StreamedResponse
+    {
+        $this->settings->currentVersion = $this->settings->availableVersion;
+        $this->settings->save();
+
+        return Response::stream(function () {
+            $output = App::make(
+                abstract: StreamOutput::class,
+                parameters: ['stream' => fopen('php://output', 'wb')]
+            );
+            $output->writeln('NOTICE! Cannot run software update in local environment.');
+            $output->writeln('Faking Update to version '.$this->settings->availableVersion.'.');
+        });
+    }
+
+    protected function runUpdateCommand(StreamOutput $output, string $available, mixed $stream): callable
+    {
+        return static function () use ($available, $output, $stream) {
+            try {
+
+                $output->writeln("Running Software Update... (Version: $available).");
+                $output->writeln('NOTE: THIS MAY TAKE A WHILE...');
+
+                $params = ['--force' => true, '--install-version' => $available];
+                $exitCode = Artisan::call('streamline:run-update', $params, $output);
+
+                if ($exitCode !== 0) {
+                    throw new RuntimeException('Command failed');
+                }
+
+                fclose($stream);
+            } catch (Exception $e) {
+
+                // Log the error
+                Log::error('Command streaming failed: '.$e->getMessage());
+
+                // Output error message to stream
+                echo 'Error: '.$e->getMessage();
+
+                // Ensure stream is closed
+                if (isset($stream) && is_resource($stream)) {
+                    fclose($stream);
+                }
+            }
+        };
     }
 }
