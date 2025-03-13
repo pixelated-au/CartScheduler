c9d51b12032bef4797d1f2953723e51f2cd20d67
Ian Heggaton <ian@pixelated.com.au>
2025-03-13 14:14:19 +1100

    Add ordering by start_time to User class query

--- app/Models/User.php
+++ app/Models/User.php
@@ -138,6 +138,7 @@ class User extends Authenticatable
             'shifts' => fn(BelongsToMany $query) => $query
                 ->wherePivot('shift_date', '=', $date)
                 ->select(['location_id', ...$columns])
+                ->orderBy('start_time')
         ]);
     }
 
