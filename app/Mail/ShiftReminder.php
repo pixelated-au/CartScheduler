26efcaee178095163914f01a2c976cf8e1cf0009
Ian Heggaton <ian@pixelated.com.au>
2025-03-13 14:10:15 +1100

    Refactor ShiftReminder date logic for relative date calculation
    - Replace detailed date properties with a concise relative date calculation
    - Incorporate CarbonInterface for improved date handling and readability

--- app/Mail/ShiftReminder.php
+++ app/Mail/ShiftReminder.php
@@ -2,15 +2,13 @@
 
 namespace App\Mail;
 
-//use App\Models\User;
+use Carbon\CarbonInterface;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
-
-//use Illuminate\Support\Facades\Hash;
 
 class ShiftReminder extends Mailable
 {
@@ -35,10 +33,16 @@ class ShiftReminder extends Mailable
         return new Content(
             markdown: 'emails.upcoming-shift',
             with: [
-                'date'    => $this->date,
-                'name'    => $this->name,
-                'gender'  => $this->gender,
-                'shiftss' => $this->shifts,
+                'relativeDate' => match ((int) Carbon::now()->startOfDay()->diffInDays($this->date)) {
+                    0 => 'today',
+                    1 => 'tomorrow',
+                    2, 3, 4, 5, 6 => ($this->date->isNextWeek() ? 'next ' : 'this ') . $this->date->getTranslatedDayName(),
+                    default => $this->date->startOfDay()->from(
+                        other: Carbon::now()->startOfDay(),
+                        syntax: CarbonInterface::DIFF_RELATIVE_TO_NOW,
+                        options: Carbon::JUST_NOW | Carbon::ONE_DAY_WORDS | Carbon::TWO_DAY_WORDS | Carbon::SEQUENTIAL_PARTS_ONLY
+                    ),
+                },
             ],
         );
     }
