a697a383f1ecba1eaef760fe63bc3891d10947aa
Ian Heggaton <ian@pixelated.com.au>
2025-04-18 08:00:52 +1000

    Add Vue DevTools plugin to Vite configuration
    - Integrate vueDevTools plugin with Vite
    - Configure vueDevTools to append to "resources/js/app.js"

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import vue from '@vitejs/plugin-vue'
 import laravel from 'laravel-vite-plugin'
 import {defineConfig} from 'vite'
+import vueDevTools from "vite-plugin-vue-devtools";
 
 export default defineConfig({
     plugins: [
@@ -15,6 +16,12 @@ export default defineConfig({
                     includeAbsolute: false,
                 },
             },
+        }),
+        vueDevTools({
+            appendTo: "resources/js/app.js",
+            // Set the editor by setting the env var on your $PATH: export LAUNCH_EDITOR=[your editor path or executable]
+            // See: https://devtools.vuejs.org/getting-started/open-in-editor
+            // and https://github.com/webfansplz/vite-plugin-vue-inspector?tab=readme-ov-file#--configuration-ide--editor
         }),
     ],
     server: {
