586c14903c9dd3aef0686b7e5e80b4a3e5c91ce0
Ian Heggaton <ian@pixelated.com.au>
2025-06-05 14:26:38 +1000

    Refactor profile pages for improved UI consistency
    - UpdatePasswordForm.vue: Use template refs and inject route.
    - TwoFactorAuthenticationForm.vue: No functional changes.
    - UpdateProfileInformationForm.vue: Use template refs and inject route.
    - DialogModal.vue: No functional changes.
    - ActionSection.vue: Changed background color styles.
    - Show.vue: No functional changes.
    - LogoutOtherBrowserSessionsForm.vue: Use template refs, inject route and update styles.

--- resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue
+++ resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue
@@ -1,100 +1,98 @@
 <script setup>
-import JetActionMessage from '@/Jetstream/ActionMessage.vue';
+import { useForm } from "@inertiajs/vue3";
+import { useTemplateRef, inject } from "vue";
+import JetActionMessage from "@/Jetstream/ActionMessage.vue";
+import JetFormSection from "@/Jetstream/FormSection.vue";
+import JetInput from "@/Jetstream/Input.vue";
+import JetInputError from "@/Jetstream/InputError.vue";
+import JetLabel from "@/Jetstream/Label.vue";
 
-import JetFormSection from '@/Jetstream/FormSection.vue';
-import JetInput from '@/Jetstream/Input.vue';
-import JetInputError from '@/Jetstream/InputError.vue';
-import JetLabel from '@/Jetstream/Label.vue';
-import {useForm} from '@inertiajs/vue3';
-import {ref} from 'vue';
+const passwordInput = useTemplateRef("passwordInput");
+const currentPasswordInput = useTemplateRef("currentPasswordInput");
 
-const passwordInput = ref(null);
-const currentPasswordInput = ref(null);
+const route = inject("route");
 
 const form = useForm({
-    current_password: '',
-    password: '',
-    password_confirmation: '',
+  current_password: "",
+  password: "",
+  password_confirmation: "",
 });
 
 const updatePassword = () => {
-    form.put(route('user-password.update'), {
-        errorBag: 'updatePassword',
-        preserveScroll: true,
-        onSuccess: () => form.reset(),
-        onError: () => {
-            if (form.errors.password) {
-                form.reset('password', 'password_confirmation');
-                passwordInput.value.focus();
-            }
+  form.put(route("user-password.update"), {
+    errorBag: "updatePassword",
+    preserveScroll: true,
+    onSuccess: () => form.reset(),
+    onError: () => {
+      if (form.errors.password) {
+        form.reset("password", "password_confirmation");
+        passwordInput.value.focus();
+      }
 
-            if (form.errors.current_password) {
-                form.reset('current_password');
-                currentPasswordInput.value.focus();
-            }
-        },
-    });
+      if (form.errors.current_password) {
+        form.reset("current_password");
+        currentPasswordInput.value.focus();
+      }
+    },
+  });
 };
 </script>
 
 <template>
-    <JetFormSection @submitted="updatePassword">
-        <template #title>
-            Update Password
-        </template>
+  <JetFormSection @submitted="updatePassword">
+    <template #title>
+      Update Password
+    </template>
 
-        <template #description>
-            To ensure your account is secure, you can use a long password.
-        </template>
+    <template #description>
+      To ensure your account is secure, you can use a long password.
+    </template>
 
-        <template #form>
-            <div class="col-span-6 sm:col-span-4">
-                <JetLabel for="current_password" value="Current Password"/>
-                <JetInput
-                    id="current_password"
-                    ref="currentPasswordInput"
-                    v-model="form.current_password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    autocomplete="current-password"
-                />
-                <JetInputError :message="form.errors.current_password" class="mt-2"/>
-            </div>
+    <template #form>
+      <div class="col-span-6 sm:col-span-4">
+        <JetLabel for="current_password" value="Current Password" />
+        <JetInput
+            id="current_password"
+            ref="currentPasswordInput"
+            v-model="form.current_password"
+            type="password"
+            class="mt-1 block w-full"
+            autocomplete="current-password" />
+        <JetInputError :message="form.errors.current_password" class="mt-2" />
+      </div>
 
-            <div class="col-span-6 sm:col-span-4">
-                <JetLabel for="password" value="New Password"/>
-                <JetInput
-                    id="password"
-                    ref="passwordInput"
-                    v-model="form.password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    autocomplete="new-password"
-                />
-                <JetInputError :message="form.errors.password" class="mt-2"/>
-            </div>
+      <div class="col-span-6 sm:col-span-4">
+        <JetLabel for="password" value="New Password" />
+        <JetInput
+            id="password"
+            ref="passwordInput"
+            v-model="form.password"
+            type="password"
+            class="mt-1 block w-full"
+            autocomplete="new-password" />
+        <JetInputError :message="form.errors.password" class="mt-2" />
+      </div>
 
-            <div class="col-span-6 sm:col-span-4">
-                <JetLabel for="password_confirmation" value="Confirm Password"/>
-                <JetInput
-                    id="password_confirmation"
-                    v-model="form.password_confirmation"
-                    type="password"
-                    class="mt-1 block w-full"
-                    autocomplete="new-password"
-                />
-                <JetInputError :message="form.errors.password_confirmation" class="mt-2"/>
-            </div>
-        </template>
+      <div class="col-span-6 sm:col-span-4">
+        <JetLabel for="password_confirmation" value="Confirm Password" />
+        <JetInput
+            id="password_confirmation"
+            v-model="form.password_confirmation"
+            type="password"
+            class="mt-1 block w-full"
+            autocomplete="new-password" />
+        <JetInputError :message="form.errors.password_confirmation" class="mt-2" />
+      </div>
+    </template>
 
-        <template #actions>
-            <JetActionMessage :on="form.recentlySuccessful" class="mr-3">
-                Saved.
-            </JetActionMessage>
+    <template #actions>
+      <JetActionMessage :on="form.recentlySuccessful" class="mr-3">
+        Saved.
+      </JetActionMessage>
 
-            <PButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                Save
-            </PButton>
-        </template>
-    </JetFormSection>
+      <PButton type="submit" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+        Save
+      </PButton>
+    </template>
+  </JetFormSection>
 </template>
--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
@@ -1,186 +1,142 @@
 <script setup>
-import JetActionMessage from '@/Jetstream/ActionMessage.vue';
-
-import JetFormSection from '@/Jetstream/FormSection.vue';
-import JetInput from '@/Jetstream/Input.vue';
-import JetInputError from '@/Jetstream/InputError.vue';
-import JetLabel from '@/Jetstream/Label.vue';
-import JetSecondaryButton from '@/Jetstream/SecondaryButton.vue';
-import {Link, router, useForm, usePage} from '@inertiajs/vue3';
-import {ref} from 'vue';
+import { Link, router, useForm } from "@inertiajs/vue3";
+import { ref, useTemplateRef, inject } from "vue";
+import JetActionMessage from "@/Jetstream/ActionMessage.vue";
+import JetFormSection from "@/Jetstream/FormSection.vue";
+import JetInput from "@/Jetstream/Input.vue";
+import JetInputError from "@/Jetstream/InputError.vue";
+import JetLabel from "@/Jetstream/Label.vue";
 
 const props = defineProps({
-    user: Object,
+  user: Object,
 });
 
+const route = inject("route");
+
 const form = useForm({
-    _method: 'PUT',
-    name: props.user.name,
-    email: props.user.email,
-    photo: null,
+  _method: "PUT",
+  name: props.user.name,
+  email: props.user.email,
+  photo: null,
 });
 
 const verificationLinkSent = ref(null);
 const photoPreview = ref(null);
-const photoInput = ref(null);
+const photoInput = useTemplateRef("photoInput");
 
 const updateProfileInformation = () => {
-    if (photoInput.value) {
-        form.photo = photoInput.value.files[0];
-    }
+  if (photoInput.value) {
+    form.photo = photoInput.value.files[0];
+  }
 
-    form.post(route('user-profile-information.update'), {
-        errorBag: 'updateProfileInformation',
-        preserveScroll: true,
-        onSuccess: () => clearPhotoFileInput(),
-    });
+  form.post(route("user-profile-information.update"), {
+    errorBag: "updateProfileInformation",
+    preserveScroll: true,
+    onSuccess: () => clearPhotoFileInput(),
+  });
 };
 
 const sendEmailVerification = () => {
-    verificationLinkSent.value = true;
+  verificationLinkSent.value = true;
 };
 
 const selectNewPhoto = () => {
-    photoInput.value.click();
+  photoInput.value.click();
 };
 
 const updatePhotoPreview = () => {
-    const photo = photoInput.value.files[0];
+  const photo = photoInput.value.files[0];
 
-    if (!photo) return;
+  if (!photo) return;
 
-    const reader = new FileReader();
+  const reader = new FileReader();
 
-    reader.onload = (e) => {
-        photoPreview.value = e.target.result;
-    };
+  reader.onload = (e) => {
+    photoPreview.value = e.target.result;
+  };
 
-    reader.readAsDataURL(photo);
+  reader.readAsDataURL(photo);
 };
 
 const deletePhoto = () => {
-    router.delete(route('current-user-photo.destroy'), {
-        preserveScroll: true,
-        onSuccess: () => {
-            photoPreview.value = null;
-            clearPhotoFileInput();
-        },
-    });
+  router.delete(route("current-user-photo.destroy"), {
+    preserveScroll: true,
+    onSuccess: () => {
+      photoPreview.value = null;
+      clearPhotoFileInput();
+    },
+  });
 };
 
 const clearPhotoFileInput = () => {
-    if (photoInput.value?.value) {
-        photoInput.value.value = null;
-    }
+  if (photoInput.value?.value) {
+    photoInput.value.value = null;
+  }
 };
 </script>
 
 <template>
-    <JetFormSection @submitted="updateProfileInformation">
-        <template #title>
-            Profile Information
-        </template>
+  <JetFormSection @submitted="updateProfileInformation">
+    <template #title>
+      Profile Information
+    </template>
 
-        <template #description>
-            Update your account's profile information and email address.
-        </template>
+    <template #description>
+      Update your account's profile information and email address.
+    </template>
 
-        <template #form>
-            <!-- Profile Photo -->
-            <div v-if="$page.props.jetstream.managesProfilePhotos" class="col-span-6 sm:col-span-4">
-                <!-- Profile Photo File Input -->
-                <input
-                    ref="photoInput"
-                    type="file"
-                    class="hidden"
-                    @change="updatePhotoPreview"
-                >
+    <template #form>
+      <!-- Name -->
+      <div class="col-span-6 sm:col-span-4">
+        <JetLabel for="name" value="Name" />
+        <JetInput
+            id="name"
+            v-model="form.name"
+            type="text"
+            class="mt-1 block w-full"
+            autocomplete="name" />
+        <JetInputError :message="form.errors.name" class="mt-2" />
+      </div>
 
-                <JetLabel for="photo" value="Photo"/>
+      <!-- Email -->
+      <div class="col-span-6 sm:col-span-4">
+        <JetLabel for="email" value="Email" />
+        <JetInput
+            id="email"
+            v-model="form.email"
+            type="email"
+            class="mt-1 block w-full" />
+        <JetInputError :message="form.errors.email" class="mt-2" />
 
-                <!-- Current Profile Photo -->
-                <div v-show="! photoPreview" class="mt-2">
-                    <img :src="user.profile_photo_url" :alt="user.name" class="rounded-full h-20 w-20 object-cover">
-                </div>
+        <div v-if="$page.props.jetstream.hasEmailVerification && user.email_verified_at === null">
+          <p class="text-sm mt-2">
+            Your email address is unverified.
 
-                <!-- New Profile Photo Preview -->
-                <div v-show="photoPreview" class="mt-2">
-                    <span
-                        class="block rounded-full w-20 h-20 bg-cover bg-no-repeat bg-center"
-                        :style="'background-image: url(\'' + photoPreview + '\');'"
-                    />
-                </div>
+            <Link
+                :href="route('verification.send')"
+                method="post"
+                as="button"
+                class="underline text-gray-600 hover:text-gray-900"
+                @click.prevent="sendEmailVerification">
+              Click here to re-send the verification email.
+            </Link>
+          </p>
 
-                <JetSecondaryButton class="mt-2 mr-2" type="button" @click.prevent="selectNewPhoto">
-                    Select A New Photo
-                </JetSecondaryButton>
+          <div v-show="verificationLinkSent" class="mt-2 font-medium text-sm text-green-600">
+            A new verification link has been sent to your email address.
+          </div>
+        </div>
+      </div>
+    </template>
 
-                <JetSecondaryButton
-                    v-if="user.profile_photo_path"
-                    type="button"
-                    class="mt-2"
-                    @click.prevent="deletePhoto"
-                >
-                    Remove Photo
-                </JetSecondaryButton>
+    <template #actions>
+      <JetActionMessage :on="form.recentlySuccessful" class="mr-3">
+        Saved.
+      </JetActionMessage>
 
-                <JetInputError :message="form.errors.photo" class="mt-2"/>
-            </div>
-
-            <!-- Name -->
-            <div class="col-span-6 sm:col-span-4">
-                <JetLabel for="name" value="Name"/>
-                <JetInput
-                    id="name"
-                    v-model="form.name"
-                    type="text"
-                    class="mt-1 block w-full"
-                    autocomplete="name"
-                />
-                <JetInputError :message="form.errors.name" class="mt-2"/>
-            </div>
-
-            <!-- Email -->
-            <div class="col-span-6 sm:col-span-4">
-                <JetLabel for="email" value="Email"/>
-                <JetInput
-                    id="email"
-                    v-model="form.email"
-                    type="email"
-                    class="mt-1 block w-full"
-                />
-                <JetInputError :message="form.errors.email" class="mt-2"/>
-
-                <div v-if="$page.props.jetstream.hasEmailVerification && user.email_verified_at === null">
-                    <p class="text-sm mt-2">
-                        Your email address is unverified.
-
-                        <Link
-                            :href="route('verification.send')"
-                            method="post"
-                            as="button"
-                            class="underline text-gray-600 hover:text-gray-900"
-                            @click.prevent="sendEmailVerification"
-                        >
-                            Click here to re-send the verification email.
-                        </Link>
-                    </p>
-
-                    <div v-show="verificationLinkSent" class="mt-2 font-medium text-sm text-green-600">
-                        A new verification link has been sent to your email address.
-                    </div>
-                </div>
-            </div>
-        </template>
-
-        <template #actions>
-            <JetActionMessage :on="form.recentlySuccessful" class="mr-3">
-                Saved.
-            </JetActionMessage>
-
-            <PButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                Save
-            </PButton>
-        </template>
-    </JetFormSection>
+      <PButton type="submit" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+        Save
+      </PButton>
+    </template>
+  </JetFormSection>
 </template>
