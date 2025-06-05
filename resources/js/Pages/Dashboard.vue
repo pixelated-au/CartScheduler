179743cf23793d3be699e1bac30113deac0af426
Ian Heggaton <ian@pixelated.com.au>
2025-06-05 14:14:52 +1000

    Update Dashboard component
    - Adjust label for outstanding reports button.

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,18 +1,17 @@
 <script setup>
-import {computed, ref} from "vue";
+import { computed, ref } from "vue";
 import AppLayout from "@/Layouts/AppLayout.vue";
 import CartReservation from "@/Pages/Components/Dashboard/CartReservation.vue";
 import ReportsModal from "@/Pages/Components/Dashboard/ReportsModal.vue";
 
-
 defineProps({
-    user: Object,
+  user: Object,
 });
 
 const outstandingReportCount = ref(0);
 
 const reportsLabel = computed(() =>
-    outstandingReportCount.value === 1 ? "Report" : "Reports",);
+  outstandingReportCount.value === 1 ? "Report" : "Reports");
 
 const showReportsModal = ref(false);
 </script>
@@ -23,13 +22,14 @@ const showReportsModal = ref(false);
       <div class="flex flex-col justify-between w-full sm:flex-row">
         <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">Dashboard</h2>
         <div>
-          <PButton v-if="outstandingReportCount"
-                   icon="iconify mdi--bell"
-                   :label="`${ outstandingReportCount } ${ reportsLabel } Outstanding`"
-                   type="button"
-                   severity="warn"
-                   class="mt-3 w-full font-bold sm:mt-0 sm:w-auto"
-                   @click="showReportsModal = true"/>
+          <PButton
+              v-if="outstandingReportCount"
+              icon="iconify mdi--bell"
+              :label="`${ outstandingReportCount } ${ reportsLabel } Outstanding`"
+              type="button"
+              severity="warn"
+              class="mt-3 w-full font-bold sm:mt-0 sm:w-auto"
+              @click="showReportsModal = true"/>
         </div>
       </div>
     </template>
@@ -42,7 +42,8 @@ const showReportsModal = ref(false);
       </div>
     </div>
   </AppLayout>
-  <ReportsModal :show="showReportsModal"
-                @has-outstanding-reports="outstandingReportCount = $event"
-                @close="showReportsModal = false"/>
+  <ReportsModal
+      :show="showReportsModal"
+      @has-outstanding-reports="outstandingReportCount = $event"
+      @close="showReportsModal = false"/>
 </template>
