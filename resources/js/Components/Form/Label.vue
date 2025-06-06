6572415fa45cb5e4faf33aa8373f20b7c54c363c
Ian Heggaton <ian@pixelated.com.au>
2025-06-07 08:21:39 +1000

    Create Form Label component.
    - Label.vue: Define a reusable form label component with dynamic styling.

---
+++ resources/js/Components/Form/Label.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+    value: String,
+    isDisabled: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+const labelClasses = computed(() =>
+    props.isDisabled ? "text-gray-400 dark:text-gray-600" : "text-gray-800 dark:text-gray-200");
+</script>
+
+<template>
+<label class="block font-medium" :class="labelClasses">
+  <span v-if="value">{{ value }}</span>
+  <span v-else><slot/></span>
+</label>
+</template>
