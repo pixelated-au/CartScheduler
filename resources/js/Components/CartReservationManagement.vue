554156b1ce62f8f8839bf0984c33e55660328aa2
Ian Heggaton <ian@pixelated.com.au>
2025-06-07 08:20:11 +1000

    Update Dashboard and CartReservationManagement components
    - Update Dashboard component to include calendar update reminders.
    - Update CartReservationManagement component to improve volunteer management UI.

--- resources/js/Components/CartReservationManagement.vue
+++ resources/js/Components/CartReservationManagement.vue
@@ -1,4 +1,8 @@
 <script setup>
+import { usePage } from "@inertiajs/vue3";
+import axios from "axios";
+import { format, parse } from "date-fns";
+import { computed, reactive, ref } from "vue";
 import EmptySlot from "@/Components/Icons/EmptySlot.vue";
 import Female from "@/Components/Icons/Female.vue";
 import Male from "@/Components/Icons/Male.vue";
@@ -12,10 +16,6 @@ import JetConfirmModal from "@/Jetstream/ConfirmationModal.vue";
 import UserActionsModal from "@/Pages/Admin/Dashboard/UserActionsModal.vue";
 import useLocationFilter from "@/Pages/Admin/Locations/Composables/useLocationFilter";
 import DatePicker from "@/Pages/Components/Dashboard/DatePicker.vue";
-import {usePage} from "@inertiajs/vue3";
-import axios from "axios";
-import {format, parse} from "date-fns";
-import {computed, reactive, ref} from "vue";
 
 defineProps({
     user: Object,
@@ -46,17 +46,17 @@ const gridCols = {
 
 const locationsOnDays = ref([]);
 
-const setLocationMarkers = locations => locationsOnDays.value = locations;
+const setLocationMarkers = (locations) => locationsOnDays.value = locations;
 
 const selectedMoveUser = ref(null);
 
 /** Note this is a get/set computed property so we can set it to null when the modal is closed **/
 const showMoveUserModal = computed({
     get: () => !!selectedMoveUser.value,
-    set: value => selectedMoveUser.value = value ? selectedMoveUser.value : null,
+    set: (value) => selectedMoveUser.value = value ? selectedMoveUser.value : null,
 });
 
-const promptMoveVolunteer = (selection, volunteer, shift) => selectedMoveUser.value = {selection, volunteer, shift};
+const promptMoveVolunteer = (selection, volunteer, shift) => selectedMoveUser.value = { selection, volunteer, shift };
 
 const moveVolunteer = async (volunteerId, locationId, shiftId, oldShiftId) => {
     const timeoutId = setTimeout(() => isLoading.value = true, 1000);
@@ -84,7 +84,7 @@ const moveVolunteer = async (volunteerId, locationId, shiftId, oldShiftId) => {
     }
 };
 
-const assignVolunteer = async ({volunteerId, volunteerName, location, shift}) => {
+const assignVolunteer = async ({ volunteerId, volunteerName, location, shift }) => {
     const timeoutId = setTimeout(() => isLoading.value = true, 1000);
 
     try {
@@ -138,11 +138,11 @@ const removeVolunteer = async () => {
 
 const today = new Date();
 
-const formatTime = time => format(parse(time, "HH:mm:ss", today), "h:mm a");
+const formatTime = (time) => format(parse(time, "HH:mm:ss", today), "h:mm a");
 
 const showUserAddModal = ref(false);
 
-const rowClass = gender => {
+const rowClass = (gender) => {
     if (gender === "male") {
         return "bg-blue-100 hover:bg-blue-200 dark:bg-blue-900/40 dark:hover:bg-blue-900/60";
     } else if (gender === "female") {
@@ -168,158 +168,179 @@ const doShowAssignVolunteerModal = (shift, location) => {
 const selectedRemoveUser = ref(null);
 
 const setRemoveUser = (volunteer, shift, location, date) =>
-    selectedRemoveUser.value = {volunteer, shift, location, date};
+    selectedRemoveUser.value = { volunteer, shift, location, date };
 
 /** Note this is a get/set computed property so we can set it to null when the modal is closed **/
 const showRemoveVolunteerModal = computed({
     get: () => !!selectedRemoveUser.value,
-    set: value => selectedRemoveUser.value = value ? selectedRemoveUser.value : null,
+    set: (value) => selectedRemoveUser.value = value ? selectedRemoveUser.value : null,
 });
 
-const removeTooltip = name => `Remove ${name} from this shift`;
+const removeTooltip = (name) => `Remove ${name} from this shift`;
 
-const locationClasses = location => location.freeShifts
+const locationClasses = (location) => location.freeShifts
     ? "border-amber-600 text-amber-500 group-hover:bg-amber-500 group-hover:text-amber-800"
     : "text-gray-400 dark:text-gray-500 border-gray-400 group-hover:bg-gray-400 group-hover:text-gray-50";
 </script>
 
 <template>
-    <div class="grid grid-cols-1 gap-3 sm:grid-cols-[min-content_auto]">
-        <div class="pb-3 md:pb-0">
-            <DatePicker can-view-historical
-                        v-model:date="date"
-                        :locations="locations"
-                        :user="user"
-                        :free-shifts="freeShifts"
-                        :marker-dates="serverDates"
-                        @locations-for-day="setLocationMarkers"/>
+<div class="grid grid-cols-1 gap-3 sm:grid-cols-[min-content_auto]">
+  <div class="pb-3 md:pb-0">
+    <DatePicker
+        can-view-historical
+        v-model:date="date"
+        :locations="locations"
+        :user="user"
+        :free-shifts="freeShifts"
+        :marker-dates="serverDates"
+        @locations-for-day="setLocationMarkers"/>
+  </div>
+  <div class="text-sm">
+    <Loading v-if="isLoading" class="min-h-[200px] sm:min-h-full"/>
+    <Accordion v-show="!isLoading" :items="locations" label="name" uid="id">
+      <template #label="{ label, location }">
+        <div class="flex items-center dark:text-gray-200">
+          {{ label }}
+          <div class="flex items-center py-1.5 ml-1 group">
+            <div
+                :class="locationClasses(location)"
+                class="flex justify-center items-center mr-2 ml-1 w-5 h-5 text-xs leading-none rounded-full border transition-colors">
+              {{ location.freeShifts }}
+            </div>
+            <div class="hidden min-w-5 sm:block">
+              <div class="overflow-x-hidden w-0 text-sm text-gray-600 whitespace-nowrap transition-all group-hover:w-full dark:text-gray-400">
+                shifts remaining
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="text-sm">
-            <Loading v-if="isLoading" class="min-h-[200px] sm:min-h-full"/>
-            <Accordion v-show="!isLoading" :items="locations" label="name" uid="id">
-                <template #label="{label, location}">
-                    <div class="flex items-center dark:text-gray-200">
-                        {{ label }}
-                        <div class="flex items-center py-1.5 ml-1 group">
-                            <div :class="locationClasses(location)"
-                                class="flex justify-center items-center mr-2 ml-1 w-5 h-5 text-xs leading-none rounded-full border transition-colors">
-                                {{ location.freeShifts }}
-                            </div>
-                            <div class="hidden min-w-5 sm:block">
-                                <div class="overflow-x-hidden w-0 text-sm text-gray-600 whitespace-nowrap transition-all group-hover:w-full dark:text-gray-400">
-                                    shifts remaining
-                                </div>
-                            </div>
-                        </div>
+      </template>
+
+      <template v-slot="{ location }">
+        <div class="grid gap-x-2 gap-y-4 w-full" :class="gridCols[location.max_volunteers]">
+          <template v-for="shift in location.filterShifts" :key="shift.id">
+            <div class="self-center pl-3 dark:text-gray-100">
+              {{ formatTime(shift.start_time) }} - {{ formatTime(shift.end_time) }}
+            </div>
+            <div
+                v-for="(volunteer, index) in shift.filterVolunteers"
+                :key="index"
+                class="justify-self-center self-center">
+              <template v-if="volunteer">
+                <Male v-if="volunteer.gender === 'male'" v-tooltip="volunteer.name"/>
+                <Female v-else-if="volunteer.gender === 'female'" v-tooltip="volunteer.name"/>
+              </template>
+
+              <EmptySlot v-else v-tooltip="'Available shift'"/>
+            </div>
+            <div></div>
+            <div class="col-span-full dark:text-gray-50">
+              <div
+                  v-for="(volunteer, index) in shift.filterVolunteers"
+                  :key="index"
+                  class="p-2 border-b border-gray-400 transition duration-150 first:rounded-t-md last:rounded-b-md last:border-b-0 hover:ease-in"
+                  :class="rowClass(volunteer?.gender)">
+                <div v-if="volunteer" class="grid grid-cols-2 gap-1.5">
+                  <div class="md:mr-3">
+                    {{ volunteer.gender === 'male' ? 'Bro' : 'Sis' }}
+                    {{ volunteer.name }}
+                  </div>
+                  <div class="text-right">
+                    Ph: <a
+
+                        :href="`tel:${volunteer.mobile_phone}`"
+                        class="underline decoration-blue-800 decoration-dotted decoration-1 underline-offset-4 visited:decoration-blue-800">
+                      {{
+                        volunteer.mobile_phone
+                      }}
+                    </a>
+                  </div>
+                  <div class="grid grid-cols-2 col-span-2 gap-1.5 lg:flex lg:gap-3">
+                    <MoveUserSelectField
+                        class="inline-block"
+                        :volunteer="volunteer"
+                        :date="date"
+                        :shift="shift"
+                        :location-id="location.id"
+                        :empty-shifts-for-time="emptyShiftsForTime"
+                        @update:modelValue="promptMoveVolunteer($event, volunteer, shift)"/>
+                    <div class="text-right">
+                      <PButton
+                          severity="danger"
+                          v-tooltip="removeTooltip(volunteer.name)"
+                          @click="setRemoveUser(volunteer, shift, location, date)">
+                        <UserRemove color="#000"/>
+                      </PButton>
                     </div>
-                </template>
-                <template v-slot="{location}">
-                    <div class="grid gap-x-2 gap-y-4 w-full" :class="gridCols[location.max_volunteers]">
-                        <template v-for="shift in location.filterShifts" :key="shift.id">
-                            <div class="self-center pl-3 dark:text-gray-100">
-                                {{ formatTime(shift.start_time) }} - {{ formatTime(shift.end_time) }}
-                            </div>
-                            <div v-for="(volunteer, index) in shift.filterVolunteers"
-                                 :key="index"
-                                 class="justify-self-center self-center">
-                                <template v-if="volunteer">
-                                    <Male v-if="volunteer.gender === 'male'" v-tooltip="volunteer.name"/>
-                                    <Female v-else-if="volunteer.gender === 'female'" v-tooltip="volunteer.name"/>
-                                </template>
-                                <EmptySlot v-else v-tooltip="'Available shift'"/>
-                            </div>
-                            <div></div>
-                            <div class="col-span-full dark:text-gray-50">
-                                <div v-for="(volunteer, index) in shift.filterVolunteers"
-                                     :key="index"
-                                     class="p-2 border-b border-gray-400 transition duration-150 first:rounded-t-md last:rounded-b-md last:border-b-0 hover:ease-in"
-                                     :class="rowClass(volunteer?.gender)">
-                                    <div v-if="volunteer" class="grid grid-cols-2 gap-1.5">
-                                        <div class="md:mr-3">
-                                            {{ volunteer.gender === 'male' ? 'Bro' : 'Sis' }}
-                                            {{ volunteer.name }}
-                                        </div>
-                                        <div class="text-right">Ph: <a :href="`tel:${volunteer.mobile_phone}`"
-                                                                       class="underline decoration-blue-800 decoration-dotted decoration-1 underline-offset-4 visited:decoration-blue-800">{{
-                                                volunteer.mobile_phone
-                                            }}</a>
-                                        </div>
-                                        <div class="grid grid-cols-2 col-span-2 gap-1.5 lg:flex lg:gap-3">
-                                            <MoveUserSelectField class="inline-block"
-                                                                 :volunteer="volunteer"
-                                                                 :date="date"
-                                                                 :shift="shift"
-                                                                 :location-id="location.id"
-                                                                 :empty-shifts-for-time="emptyShiftsForTime"
-                                                                 @update:modelValue="promptMoveVolunteer($event, volunteer, shift)"/>
-                                            <div class="text-right">
-                                                <PButton severity="danger"
-                                                         v-tooltip="removeTooltip(volunteer.name)"
-                                                         @click="setRemoveUser(volunteer, shift, location, date)">
-                                                    <UserRemove color="#000"/>
-                                                </PButton>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div v-else>
-                                        <PButton severity="info"
-                                                 @click="doShowAssignVolunteerModal(shift, location)">
-                                            <UserAdd color="#fff"/>
-                                            <span class="ml-3">Add Volunteer</span>
-                                        </PButton>
-                                    </div>
-                                </div>
-                            </div>
-                        </template>
-                    </div>
-                </template>
-            </Accordion>
+                  </div>
+                </div>
+                <div v-else>
+                  <PButton
+                      severity="info"
+                      @click="doShowAssignVolunteerModal(shift, location)">
+                    <UserAdd color="#fff"/>
+                    <span class="ml-3">Add Volunteer</span>
+                  </PButton>
+                </div>
+              </div>
+            </div>
+          </template>
         </div>
+      </template>
+    </Accordion>
+  </div>
+</div>
+<UserActionsModal
+    v-model:show="showUserAddModal"
+    :date="date"
+    :shift="assignUserData.shift"
+    :location="assignUserData.location"
+    @assignVolunteer="assignVolunteer"/>
+<JetConfirmModal v-model:show="showMoveUserModal">
+  <template #title>
+    <h2 class="text-lg font-medium text-gray-900">Move user</h2>
+  </template>
+
+  <template #content>
+    <div>
+      Are you sure you want to move {{ selectedMoveUser.volunteer.name }} to
+      {{ selectedMoveUser.selection.label }}?
     </div>
-    <UserActionsModal v-model:show="showUserAddModal"
-        :date="date"
-        :shift="assignUserData.shift"
-        :location="assignUserData.location"
-        @assignVolunteer="assignVolunteer"/>
-    <JetConfirmModal v-model:show="showMoveUserModal">
-        <template #title>
-            <h2 class="text-lg font-medium text-gray-900">Move user</h2>
-        </template>
-        <template #content>
-            <div>
-                Are you sure you want to move {{ selectedMoveUser.volunteer.name }} to
-                {{ selectedMoveUser.selection.label }}?
-            </div>
-        </template>
-        <template #footer>
-            <div class="flex justify-end">
-                <PButton severity="secondary" @click="selectedMoveUser = null">Cancel</PButton>
-                <PButton @click="moveVolunteer(selectedMoveUser?.volunteer.id, selectedMoveUser?.selection.newLocationId, selectedMoveUser?.selection.newShiftId, selectedMoveUser?.shift.id)"
-                    class="ml-2">
-                    Move
-                </PButton>
-            </div>
-        </template>
-    </JetConfirmModal>
-    <JetConfirmModal v-model:show="showRemoveVolunteerModal">
-        <template #title>
-            <h2 class="text-lg font-medium text-gray-900">Remove user</h2>
-        </template>
-        <template #content>
-            <div>
-                Are you sure you want to remove {{ selectedRemoveUser.volunteer.name }} from
-                {{ selectedRemoveUser.location.name }}?
-            </div>
-        </template>
-        <template #footer>
-            <div class="flex justify-end">
-                <PButton severity="secondary" @click="selectedRemoveUser = null">Cancel</PButton>
-                <PButton style-type="warning"
-                    @click="removeVolunteer()"
-                    class="ml-2">
-                    Remove
-                </PButton>
-            </div>
-        </template>
-    </JetConfirmModal>
+  </template>
+
+  <template #footer>
+    <div class="flex justify-end">
+      <PButton severity="secondary" @click="selectedMoveUser = null">Cancel</PButton>
+      <PButton
+          @click="moveVolunteer(selectedMoveUser?.volunteer.id, selectedMoveUser?.selection.newLocationId, selectedMoveUser?.selection.newShiftId, selectedMoveUser?.shift.id)"
+          class="ml-2">
+        Move
+      </PButton>
+    </div>
+  </template>
+</JetConfirmModal>
+<JetConfirmModal v-model:show="showRemoveVolunteerModal">
+  <template #title>
+    <h2 class="text-lg font-medium text-gray-900">Remove user</h2>
+  </template>
+
+  <template #content>
+    <div>
+      Are you sure you want to remove {{ selectedRemoveUser.volunteer.name }} from
+      {{ selectedRemoveUser.location.name }}?
+    </div>
+  </template>
+
+  <template #footer>
+    <div class="flex justify-end">
+      <PButton severity="secondary" @click="selectedRemoveUser = null">Cancel</PButton>
+      <PButton
+          style-type="warning"
+          @click="removeVolunteer()"
+          class="ml-2">
+        Remove
+      </PButton>
+    </div>
+  </template>
+</JetConfirmModal>
 </template>
--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -1,84 +1,85 @@
 <script setup>
-import CartReservationManagement from '@/Components/CartReservationManagement.vue';
-import AppLayout from '@/Layouts/AppLayout.vue';
-import FilledShiftsChart from '@/Pages/Admin/Dashboard/FilledShiftsChart.vue';
-import Tags from '@/Pages/Admin/Dashboard/Tags.vue';
-import {router} from '@inertiajs/vue3';
+import { router } from "@inertiajs/vue3";
+import CartReservationManagement from "@/Components/CartReservationManagement.vue";
+import AppLayout from "@/Layouts/AppLayout.vue";
+import FilledShiftsChart from "@/Pages/Admin/Dashboard/FilledShiftsChart.vue";
+import Tags from "@/Pages/Admin/Dashboard/Tags.vue";
 
-const props = defineProps({
+defineProps({
     totalUsers: Number,
     totalLocations: Number,
     shiftFilledData: Array,
     outstandingReports: Array,
 });
 
-const go = url => router.visit(url);
+const go = (url) => router.visit(url);
 </script>
 
 <template>
-    <AppLayout title="Dashboard">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                Admin Dashboard</h2>
-        </template>
+<AppLayout title="Dashboard">
+  <template #header>
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+      Admin Dashboard
+    </h2>
+    <h1 class="text-red-500">NOW ITS TIME TO UPDATE THE CALENDAR HERE</h1>
+    <h1 class="text-red-500">NOW ITS TIME TO UPDATE THE CALENDAR HERE</h1>
+    <h1 class="text-red-500">NOW ITS TIME TO UPDATE THE CALENDAR HERE</h1>
+    <h1 class="text-red-500">NOW ITS TIME TO UPDATE THE CALENDAR HERE</h1>
+    <h1 class="text-red-500">NOW ITS TIME TO UPDATE THE CALENDAR HERE</h1>
+    <h1 class="text-red-500">NOW ITS TIME TO UPDATE THE CALENDAR HERE</h1>
+  </template>
 
-        <div class="py-2 sm:py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-slate-900 overflow-hidden shadow-xl sm:rounded-lg">
-
-                    <div class="grid gap-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 m-6">
-                        <div
-                            class="cursor-pointer bg-gray-100 dark:bg-slate-700 p-6 rounded-lg shadow-lg hover:bg-gray-200 dark:hover:bg-slate-800 transition-colors"
-                            @click="go(route('admin.reports.index'))">
-                            <h3 class="text-lg font-semibold text-gray-900 dark:text-gray-200">
-                                <span class="text-gray-600 dark:text-gray-300">{{ outstandingReports.length }}</span>
-                                <span class="text-gray-600 dark:text-gray-300">
-                                    Incomplete Reports
-                                </span>
-                            </h3>
-                            <p class="text-gray-700 dark:text-gray-300">
-                                The number of reports that participants are yet to submit. </p>
-                        </div>
-                        <div
-                            class="cursor-pointer bg-gray-100 dark:bg-slate-700 p-6 rounded-lg shadow-lg hover:bg-gray-200 dark:hover:bg-slate-800 transition-colors"
-                            @click="go(route('admin.users.index'))">
-                            <h3 class="text-lg font-semibold text-gray-900 dark:text-gray-200">
-
-                                <span class="text-gray-600 dark:text-gray-300">{{ totalUsers }}</span>
-                                Users </h3>
-                            <p class="text-gray-700 dark:text-gray-300">Total number of users in the system.</p>
-                        </div>
-                        <div
-                            class="cursor-pointer bg-gray-100 dark:bg-slate-700 p-6 rounded-lg shadow-lg hover:bg-gray-200 dark:hover:bg-slate-800 transition-colors"
-                            @click="go(route('admin.locations.index'))">
-                            <h3 class="text-lg font-semibold text-gray-900 dark:text-gray-200">
-                                <span class="text-gray-600 dark:text-gray-300">{{ totalLocations }}</span>
-                                <span class="text-gray-600 dark:text-gray-300 ml-1">Locations</span>
-                            </h3>
-                            <p class="text-gray-700 dark:text-gray-300">Total number of locations in the system.</p>
-                        </div>
-                        <div
-                            class="dashboard col-span-full sm:p-6 rounded-lg shadow-lg grid grid-cols-1 bg-white dark:bg-slate-900">
-                            <CartReservationManagement/>
-                        </div>
-                        <div
-                            class="col-span-full bg-gray-100 dark:bg-slate-700 p-6 rounded-lg shadow-lg grid grid-cols-1">
-                            <h3 class="text-lg font-semibold text-gray-900 dark:text-gray-200">
-                                <span class="text-gray-600 dark:text-gray-300">Filled Shifts</span>
-                            </h3>
-                            <p class="text-gray-700 dark:text-gray-300">For the next 14 days.</p>
-                            <FilledShiftsChart :shiftData="shiftFilledData"/>
-                        </div>
-                        <Tags/>
-                    </div>
-                </div>
-            </div>
+  <div class="py-2 sm:py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="bg-white dark:bg-slate-900 overflow-hidden shadow-xl sm:rounded-lg">
+        <div class="grid gap-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 m-6">
+          <div
+              class="cursor-pointer bg-gray-100 dark:bg-slate-700 p-6 rounded-lg shadow-lg hover:bg-gray-200 dark:hover:bg-slate-800 transition-colors"
+              @click="go(route('admin.reports.index'))">
+            <h3 class="text-lg font-semibold text-gray-900 dark:text-gray-200">
+              <span class="text-gray-600 dark:text-gray-300">{{ outstandingReports.length }}</span>
+              <span class="text-gray-600 dark:text-gray-300">
+                Incomplete Reports
+              </span>
+            </h3>
+            <p class="text-gray-700 dark:text-gray-300">
+              The number of reports that participants are yet to submit.
+            </p>
+          </div>
+          <div
+              class="cursor-pointer bg-gray-100 dark:bg-slate-700 p-6 rounded-lg shadow-lg hover:bg-gray-200 dark:hover:bg-slate-800 transition-colors"
+              @click="go(route('admin.users.index'))">
+            <h3 class="text-lg font-semibold text-gray-900 dark:text-gray-200">
+              <span class="text-gray-600 dark:text-gray-300">{{ totalUsers }}</span>
+              Users
+            </h3>
+            <p class="text-gray-700 dark:text-gray-300">Total number of users in the system.</p>
+          </div>
+          <div
+              class="cursor-pointer bg-gray-100 dark:bg-slate-700 p-6 rounded-lg shadow-lg hover:bg-gray-200 dark:hover:bg-slate-800 transition-colors"
+              @click="go(route('admin.locations.index'))">
+            <h3 class="text-lg font-semibold text-gray-900 dark:text-gray-200">
+              <span class="text-gray-600 dark:text-gray-300">{{ totalLocations }}</span>
+              <span class="text-gray-600 dark:text-gray-300 ml-1">Locations</span>
+            </h3>
+            <p class="text-gray-700 dark:text-gray-300">Total number of locations in the system.</p>
+          </div>
+          <div
+              class="dashboard col-span-full sm:p-6 rounded-lg shadow-lg grid grid-cols-1 bg-white dark:bg-slate-900">
+            <CartReservationManagement/>
+          </div>
+          <div
+              class="col-span-full bg-gray-100 dark:bg-slate-700 p-6 rounded-lg shadow-lg grid grid-cols-1">
+            <h3 class="text-lg font-semibold text-gray-900 dark:text-gray-200">
+              <span class="text-gray-600 dark:text-gray-300">Filled Shifts</span>
+            </h3>
+            <p class="text-gray-700 dark:text-gray-300">For the next 14 days.</p>
+            <FilledShiftsChart :shiftData="shiftFilledData"/>
+          </div>
+          <Tags/>
         </div>
-    </AppLayout>
+      </div>
+    </div>
+  </div>
+</AppLayout>
 </template>
-
-<style scoped>
-.shifts-chart {
-    min-height: 300px;
-}
-</style>
