693f601ff1573647fb9021dc77ed977cbcbacaad
Ian Heggaton <ian@pixelated.com.au>
2025-06-12 12:00:59 +1000

    Refactor Input component to use PrimeVue InputText
    - Replace custom input with PrimeVue InputText component.

--- resources/js/Jetstream/Input.vue
+++ resources/js/Jetstream/Input.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted } from "vue";
 import { templateRef } from "@vueuse/core";
 
 defineProps({
@@ -14,17 +14,18 @@ defineEmits(["update:modelValue"]);
 const input = templateRef();
 
 onMounted(() => {
-    if (input.value.hasAttribute("autofocus")) {
+    if (input.value?.hasAttribute("autofocus")) {
         input.value.focus();
     }
 });
 
 defineExpose({ focus: () => input.value.focus() });
+
+// TODO, this file is redundant. We need to remove it
 </script>
 
 <template>
-<input ref="input"
-       class="p-inputtext p-component p-inputnumber-input"
-       :value="modelValue"
-       @input="$emit('update:modelValue', $event.target.value)">
+<PInputText ref="input"
+            :value="modelValue"
+            @input="$emit('update:modelValue', $event.target.value)"/>
 </template>
