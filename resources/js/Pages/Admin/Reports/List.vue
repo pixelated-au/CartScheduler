6de6480ed57d3c28d854eb737a1bff39371e832f
Ian Heggaton <ian@pixelated.com.au>
2025-06-12 10:32:59 +1000

    Refactor List views for Users and Reports to use consistent styling
    - Update Admin/Users/List.vue to use `bg-panel` and `bg-panel-dark` classes for background
    - Update Admin/Reports/List.vue to use `bg-panel` and `bg-panel-dark` classes for background

--- resources/js/Pages/Admin/Reports/List.vue
+++ resources/js/Pages/Admin/Reports/List.vue
@@ -1,35 +1,35 @@
 <script setup>
-import DataTable from '@/Components/DataTable.vue';
-import Filter from '@/Components/Icons/Filter.vue';
-import JetHelpText from '@/Jetstream/HelpText.vue';
-import JetInput from '@/Jetstream/Input.vue';
-import JetLabel from '@/Jetstream/Label.vue';
-import AppLayout from '@/Layouts/AppLayout.vue';
-import {computed, ref} from 'vue';
-import headers from './Lib/ReportsDataTableHeaders.js';
+import { computed, ref } from "vue";
+import headers from "./Lib/ReportsDataTableHeaders.js";
+import DataTable from "@/Components/DataTable.vue";
+import Filter from "@/Components/Icons/Filter.vue";
+import JetHelpText from "@/Jetstream/HelpText.vue";
+import JetInput from "@/Jetstream/Input.vue";
+import JetLabel from "@/Jetstream/Label.vue";
+import AppLayout from "@/Layouts/AppLayout.vue";
 
 const props = defineProps({
     reports: Object,
     locations: Object,
 });
 
-const userSearch = ref('');
+const userSearch = ref("");
 
-const reportData = computed(() => props.reports.data.map(report => ({
+const reportData = computed(() => props.reports.data.map((report) => ({
     id: report.id,
-    location: report.metadata?.location_name || report.shift?.location?.name || '',
-    locationId: report.metadata?.locationId || report.shift?.location?.id || '',
+    location: report.metadata?.location_name || report.shift?.location?.name || "",
+    locationId: report.metadata?.locationId || report.shift?.location?.id || "",
     userExists: !!report.submitted_by?.id,
-    submittedByName: report.metadata?.submitted_by_name || report.submitted_by?.name || '',
-    submittedByPhone: report.metadata?.submitted_by_phone || report.submitted_by?.mobile_phone || '',
-    submittedByEmail: report.metadata?.submitted_by_email || report.submitted_by?.email || '',
+    submittedByName: report.metadata?.submitted_by_name || report.submitted_by?.name || "",
+    submittedByPhone: report.metadata?.submitted_by_phone || report.submitted_by?.mobile_phone || "",
+    submittedByEmail: report.metadata?.submitted_by_email || report.submitted_by?.email || "",
     date: report.shift_date,
     time: report.shift?.start_time,
     placements: report.placements_count,
     videos: report.videos_count,
     requests: report.requests_count,
     comments: report.comments,
-    tags: report.tags.map(tag => tag.name.en),
+    tags: report.tags.map((tag) => tag.name.en),
     cancelled: report.shift_was_cancelled,
     associates: report.metadata?.associates || [],
 })));
@@ -40,99 +40,98 @@ const locationFilter = ref(0);
 const filter = computed(() => {
     const filters = [];
     if (locationFilter.value) {
-        filters.push({field: 'locationId', comparison: '=', criteria: locationFilter.value});
+        filters.push({ field: "locationId", comparison: "=", criteria: locationFilter.value });
     }
     return filters;
 });
 </script>
 
 <template>
-    <AppLayout title="Reports">
-        <template #header>
-            <div>
-                <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Reports</h2>
+<AppLayout title="Reports">
+  <template #header>
+    <div>
+      <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Reports</h2>
+    </div>
+  </template>
+
+  <div class="max-w-7xl mx-auto pt-10 pb-5 sm:px-6 lg:px-8">
+    <div class="bg-panel dark:bg-panel-dark shadow-xl sm:rounded-lg sm:p-6">
+      <JetLabel for="search" value="Search on Reports"/>
+      <JetInput id="search" v-model="userSearch" type="text" class="mt-1 block w-full"/>
+      <JetHelpText>Search on location, user name, date, etc</JetHelpText>
+    </div>
+  </div>
+
+  <div class="max-w-7xl mx-auto pb-10 sm:px-6 lg:px-8">
+    <div class="bg-panel dark:bg-panel-dark shadow-xl sm:rounded-lg sm:p-6">
+      <data-table :headers="headers"
+                  :items="reportData"
+                  :search-value="userSearch"
+                  :filter-options="filter"
+                  :show-hover="false">
+        <template #header-location="{ text }">
+          <div class="relative flex items-center">
+            <div class="p-2 mr-2 cursor-pointer inline-block hover:bg-gray-200 hover:bg-opacity-50 rounded"
+                 @click.stop="showLocationFilter = !showLocationFilter">
+              <Filter/>
             </div>
+            {{ text }}
+            <div class="p-2 z-50 absolute top-12 w-64 bg-white border border-gray-300 rounded-md shadow-lg"
+                 v-if="showLocationFilter">
+              <select class="location-selector w-full rounded-md"
+                      v-model="locationFilter"
+                      name="location">
+                <option value="" selected>All</option>
+                <option v-for="location in locations.data" :key="location.id" :value="location.id">
+                  {{ location.name }}
+                </option>
+              </select>
+            </div>
+          </div>
         </template>
 
-        <div class="max-w-7xl mx-auto pt-10 pb-5 sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-900 shadow-xl sm:rounded-lg sm:p-6">
-                <JetLabel for="search" value="Search on Reports"/>
-                <JetInput id="search" v-model="userSearch" type="text" class="mt-1 block w-full"/>
-                <JetHelpText>Search on location, user name, date, etc</JetHelpText>
-            </div>
-        </div>
+        <template #item-cancelled="{ cancelled }">
+          {{ cancelled ? 'Yes' : 'No' }}
+        </template>
 
-        <div class="max-w-7xl mx-auto pb-10 sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-900 shadow-xl sm:rounded-lg sm:p-6">
-                <data-table :headers="headers"
-                            :items="reportData"
-                            :search-value="userSearch"
-                            :filter-options="filter"
-                            :show-hover="false">
-                    <template #header-location="{text}">
-                        <div class="relative flex items-center">
-                            <div
-                                class="p-2 mr-2 cursor-pointer inline-block hover:bg-gray-200 hover:bg-opacity-50 rounded"
-                                @click.stop="showLocationFilter = !showLocationFilter">
-                                <Filter/>
-                            </div>
-                            {{ text }}
-                            <div
-                                class="p-2 z-50 absolute top-12 w-64 bg-white border border-gray-300 rounded-md shadow-lg"
-                                v-if="showLocationFilter">
-                                <select class="location-selector w-full rounded-md"
-                                        v-model="locationFilter"
-                                        name="location">
-                                    <option value="" selected>All</option>
-                                    <option v-for="location in locations.data" :key="location.id" :value="location.id">
-                                        {{ location.name }}
-                                    </option>
-                                </select>
-                            </div>
-                        </div>
-                    </template>
-                    <template #item-cancelled="{ cancelled }">
-                        {{ cancelled ? 'Yes' : 'No' }}
-                    </template>
-                    <template
-                        #expand="{comments, tags, submittedByName, submittedByEmail, submittedByPhone, associates}">
-                        <div class="bg-gray-100 dark:bg-gray-800 p-3 rounded-lg grid grid-cols-3">
-                            <ul v-if="tags?.length" class="col-span-3 p-0 m-0 mb-3">
-                                <li v-for="(tag, i) in tags"
-                                    :key="i"
-                                    class="inline-flex px-2 bg-green-200 dark:bg-green-800 mr-1 mb-1 rounded-full dark:text-gray-50">
-                                    {{ tag }}
-                                </li>
-                            </ul>
-                            <div class="">
-                                <h5>Comments</h5>
-                                <div v-if="comments">{{ comments }}</div>
-                                <div v-else class="text-gray-500 dark:text-gray-300"><em>None entered</em></div>
-                            </div>
-                            <div>
-                                <h5>Submitted by: {{ submittedByName }}<br></h5>
-                                Tel: <a :href="`tel:${submittedByPhone}`">{{ submittedByPhone }}</a><br>
-                                Email: <a :href="`mailto:${submittedByEmail}`">{{ submittedByEmail }}</a>
-                            </div>
-                            <div>
-                                <h5>Associates</h5>
-                                <div v-if="associates?.length">
-                                    <ul class="p-0 m-0">
-                                        <li v-for="(associate, i) in associates"
-                                            :key="i"
-                                            class="inline-flex px-2 bg-green-200 dark:bg-purple-800 mr-1 mb-1 rounded-full dark:text-gray-50">
-                                            {{ associate.name }}
-                                        </li>
-                                    </ul>
-
-                                </div>
-                            </div>
-                        </div>
-                    </template>
-                </data-table>
+        <template
+            #expand="{ comments, tags, submittedByName, submittedByEmail, submittedByPhone, associates }">
+          <div class="bg-gray-100 dark:bg-gray-800 p-3 rounded-lg grid grid-cols-3">
+            <ul v-if="tags?.length" class="col-span-3 p-0 m-0 mb-3">
+              <li v-for="(tag, i) in tags"
+                  :key="i"
+                  class="inline-flex px-2 bg-green-200 dark:bg-green-800 mr-1 mb-1 rounded-full dark:text-gray-50">
+                {{ tag }}
+              </li>
+            </ul>
+            <div class="">
+              <h5>Comments</h5>
+              <div v-if="comments">{{ comments }}</div>
+              <div v-else class="text-gray-500 dark:text-gray-300"><em>None entered</em></div>
             </div>
-        </div>
-    </AppLayout>
+            <div>
+              <h5>Submitted by: {{ submittedByName }}<br></h5>
+              Tel: <a :href="`tel:${submittedByPhone}`">{{ submittedByPhone }}</a><br>
+              Email: <a :href="`mailto:${submittedByEmail}`">{{ submittedByEmail }}</a>
+            </div>
+            <div>
+              <h5>Associates</h5>
+              <div v-if="associates?.length">
+                <ul class="p-0 m-0">
+                  <li v-for="(associate, i) in associates"
+                      :key="i"
+                      class="inline-flex px-2 bg-green-200 dark:bg-purple-800 mr-1 mb-1 rounded-full dark:text-gray-50">
+                    {{ associate.name }}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </template>
+      </data-table>
+    </div>
+  </div>
+</AppLayout>
 </template>
 
 <style lang="scss">
--- resources/js/Pages/Admin/Users/List.vue
+++ resources/js/Pages/Admin/Users/List.vue
@@ -1,13 +1,12 @@
 <script setup>
-import DataTable from '@/Components/DataTable.vue';
-
-import JetHelpText from '@/Jetstream/HelpText.vue';
-import JetInput from '@/Jetstream/Input.vue';
-import JetLabel from '@/Jetstream/Label.vue';
-import AppLayout from '@/Layouts/AppLayout.vue';
-import {router} from '@inertiajs/vue3';
-import {ref} from 'vue';
-import headers from './Lib/UserDataTableHeaders';
+import { router } from "@inertiajs/vue3";
+import { ref } from "vue";
+import headers from "./Lib/UserDataTableHeaders";
+import DataTable from "@/Components/DataTable.vue";
+import JetHelpText from "@/Jetstream/HelpText.vue";
+import JetInput from "@/Jetstream/Input.vue";
+import JetLabel from "@/Jetstream/Label.vue";
+import AppLayout from "@/Layouts/AppLayout.vue";
 
 defineProps({
     users: Object,
@@ -15,90 +14,96 @@ defineProps({
     permissions: Object,
 });
 
-const userSearch = ref('');
+const userSearch = ref("");
 
 const onNewUser = () => {
-    router.visit(route('admin.users.create'));
+    router.visit(route("admin.users.create"));
 };
 
 const onImportUsers = () => {
-    router.visit(route('admin.users.import.show'));
+    router.visit(route("admin.users.import.show"));
 };
 
 const onDownloadUsers = async () => {
-    window.location.href = route('admin.users-as-spreadsheet');
+    window.location.href = route("admin.users-as-spreadsheet");
 };
 
 const handleSelection = (selection) => {
-    router.visit(route('admin.users.edit', selection.id));
+    router.visit(route("admin.users.edit", selection.id));
 };
 </script>
 
 <template>
-    <AppLayout title="Users">
-        <template #header>
-            <div class="flex justify-between flex-wrap md:flex-nowrap">
-                <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight w-full md:w-auto">
-                    Users</h2>
-                <div class="w-full md:w-auto">
-                    <PButton class="hidden sm:inline-flex md:mx-3 w-full md:w-auto my-3 md:my-0"
-                               style-type="secondary"
-                               outline
-                               @click="onImportUsers">
-                        Import Users
-                    </PButton>
-                    <PButton class="py-4 sm:py-2 md:mx-3 w-full md:w-auto" style-type="primary" @click="onNewUser">
-                        New User
-                    </PButton>
-                </div>
-            </div>
+<AppLayout title="Users">
+  <template #header>
+    <div class="flex justify-between flex-wrap md:flex-nowrap">
+      <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight w-full md:w-auto">
+        Users
+      </h2>
+      <div class="w-full md:w-auto">
+        <PButton class="hidden sm:inline-flex md:mx-3 w-full md:w-auto my-3 md:my-0"
+                 style-type="secondary"
+                 outline
+                 @click="onImportUsers">
+          Import Users
+        </PButton>
+        <PButton class="py-4 sm:py-2 md:mx-3 w-full md:w-auto" style-type="primary" @click="onNewUser">
+          New User
+        </PButton>
+      </div>
+    </div>
+  </template>
+
+  <div class="max-w-7xl mx-auto sm:pt-10 sm:pb-5 sm:px-6 lg:px-8 grid gap-3 grid-cols-1 sm:grid-cols-12">
+    <div class="bg-panel dark:bg-panel-dark shadow-xl sm:rounded-lg py-3 px-4 sm:p-6 sm:col-span-10">
+      <JetLabel for="search" value="Search for a user:"/>
+      <!-- Overriding background colours for usability -->
+      <JetInput id="search"
+                v-model="userSearch"
+                type="text"
+                class="mt-1 block w-full dark:bg-slate-700 sm:dark:bg-slate-800"/>
+      <JetHelpText>Search on name, email, phone, role or any field</JetHelpText>
+    </div>
+    <div class="hidden sm:flex sm:flex-wrap sm:justify-center sm:items-center col-span-2">
+      <div class="text-center">
+        <PButton outline style-type="info" @click="onDownloadUsers">
+          Download Users
+        </PButton>
+        <div class="mt-2 text-sm text-gray-800 dark:text-gray-400 text-center">
+          Download an Excel
+          Spreadsheet
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="max-w-7xl mx-auto pb-10 sm:px-6 lg:px-8">
+    <div class="bg-panel dark:bg-panel-dark shadow-xl sm:rounded-lg sm:p-6">
+      <data-table :headers="headers"
+                  :items="users.data"
+                  :search-value="userSearch"
+                  @click-row="handleSelection">
+        <template #item-email="{ email }">
+          <a :href="`mailto:${email}`">{{ email }}</a>
         </template>
 
-        <div class="max-w-7xl mx-auto sm:pt-10 sm:pb-5 sm:px-6 lg:px-8 grid gap-3 grid-cols-1 sm:grid-cols-12">
-            <div class="bg-white dark:bg-slate-900 shadow-xl sm:rounded-lg py-3 px-4 sm:p-6 sm:col-span-10">
-                <JetLabel for="search" value="Search for a user:"/>
-                <!-- Overriding background colours for usability -->
-                <JetInput id="search" v-model="userSearch" type="text"
-                          class="mt-1 block w-full dark:bg-slate-700 sm:dark:bg-slate-800"/>
-                <JetHelpText>Search on name, email, phone, role or any field</JetHelpText>
-            </div>
-            <div class="hidden sm:flex sm:flex-wrap sm:justify-center sm:items-center col-span-2">
-                <div class="text-center">
-                    <PButton outline style-type="info" @click="onDownloadUsers">
-                        Download Users
-                    </PButton>
-                    <div class="mt-2 text-sm text-gray-800 dark:text-gray-400 text-center">Download an Excel
-                        Spreadsheet
-                    </div>
-                </div>
-            </div>
+        <template #item-phone="{ mobile_phone }">
+          <a :href="`tel:${mobile_phone}`">{{ mobile_phone }}</a>
+        </template>
 
-        </div>
+        <template #item-is_enabled="{ is_enabled }">
+          <div v-if="is_enabled">Yes</div>
+          <div v-else class="text-red-600 dark:text-red-400">No</div>
+        </template>
 
-        <div class="max-w-7xl mx-auto pb-10 sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-slate-900 shadow-xl sm:rounded-lg sm:p-6">
-                <data-table :headers="headers"
-                            :items="users.data"
-                            :search-value="userSearch"
-                            @click-row="handleSelection">
-                    <template #item-email="{ email }">
-                        <a :href="`mailto:${email}`">{{ email }}</a>
-                    </template>
-                    <template #item-phone="{ mobile_phone }">
-                        <a :href="`tel:${mobile_phone}`">{{ mobile_phone }}</a>
-                    </template>
-                    <template #item-is_enabled="{ is_enabled }">
-                        <div v-if="is_enabled">Yes</div>
-                        <div v-else class="text-red-600 dark:text-red-400">No</div>
-                    </template>
-                    <template #item-is_unrestricted="{ is_unrestricted }">
-                        <div v-if="!is_unrestricted" class="text-red-600 dark:text-red-400">Yes</div>
-                        <div v-else>No</div>
-                    </template>
-                </data-table>
-            </div>
-        </div>
-    </AppLayout>
+        <template #item-is_unrestricted="{ is_unrestricted }">
+          <div v-if="!is_unrestricted" class="text-red-600 dark:text-red-400">Yes</div>
+          <div v-else>No</div>
+        </template>
+      </data-table>
+    </div>
+  </div>
+</AppLayout>
 </template>
 
 <style lang="scss">
