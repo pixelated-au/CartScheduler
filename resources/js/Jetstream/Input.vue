601bcc634f0ba61ee2713ceae591a8b17a5262df
Ian Heggaton <ian@pixelated.com.au>
2025-06-12 11:13:18 +1000

    Update Input component styles
    - Modified Input component's CSS classes.
    - Replaced ref() with templateRef()

--- resources/js/Jetstream/Input.vue
+++ resources/js/Jetstream/Input.vue
@@ -1,30 +1,30 @@
 <script setup>
 import { onMounted, ref } from "vue";
+import { templateRef } from "@vueuse/core";
 
 defineProps({
-  modelValue: {
-    type: [String, Number],
-    default: "",
-  },
+    modelValue: {
+        type: [String, Number],
+        default: "",
+    },
 });
 
 defineEmits(["update:modelValue"]);
 
-const input = ref();
+const input = templateRef();
 
 onMounted(() => {
-  if (input.value.hasAttribute("autofocus")) {
-    input.value.focus();
-  }
+    if (input.value.hasAttribute("autofocus")) {
+        input.value.focus();
+    }
 });
 
 defineExpose({ focus: () => input.value.focus() });
 </script>
 
 <template>
-  <input
-      ref="input"
-      class="border-gray-300 focus:border-indigo-300 focus:ring-1 focus:ring-indigo-200 focus:ring-opacity-50 dark:border-gray-800  dark:focus:ring-indigo-800 dark:text-gray-100 rounded-md shadow-sm bg-transparent dark:bg-transparent"
-      :value="modelValue"
-      @input="$emit('update:modelValue', $event.target.value)">
+<input ref="input"
+       class="p-inputtext p-component p-inputnumber-input"
+       :value="modelValue"
+       @input="$emit('update:modelValue', $event.target.value)">
 </template>
