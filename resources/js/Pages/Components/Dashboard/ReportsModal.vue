11567a5a86a48489277b97533e5eb7c62f2c57e6
Ian Heggaton <ian@pixelated.com.au>
2025-06-12 11:09:18 +1000

    Refactor ReportsModal component to use PrimeVue Dialog
    - Replaced Jetstream ConfirmationModal with PrimeVue PDialog.
    - Removed unnecessary `emit` call for closing the modal, now using `v-model:visible`.
    - Changed close button to directly set the `show` property to `false`.

--- resources/js/Pages/Components/Dashboard/ReportsModal.vue
+++ resources/js/Pages/Components/Dashboard/ReportsModal.vue
@@ -1,35 +1,26 @@
 <script setup>
-import useToast from '@/Composables/useToast.js';
+import { onMounted, provide, ref } from "vue";
+import useToast from "@/Composables/useToast.js";
+import ReportForm from "@/Pages/Components/Dashboard/ReportForm.vue";
 
-import ConfirmationModal from '@/Jetstream/ConfirmationModal.vue';
-import ReportForm from '@/Pages/Components/Dashboard/ReportForm.vue';
-import {onMounted, provide, ref} from 'vue';
+const show = defineModel({ required: true, type: Boolean });
 
-defineProps({
-    show: {
-        type: Boolean,
-        default: false,
-    },
-});
-
-const emit = defineEmits(['has-outstanding-reports', 'close']);
-
-const close = () => emit('close');
+const emit = defineEmits(["has-outstanding-reports"]);
 
 const toast = useToast();
 
 const outstandingReports = ref([]);
 const tags = ref([]);
-provide('report-tags', tags);
+provide("report-tags", tags);
 
 const getData = async () => {
     try {
         const [reportsResponse, tagsResponse] = await Promise.all([
-            axios.get('/outstanding-reports'),
-            axios.get('/get-report-tags'),
+            axios.get("/outstanding-reports"),
+            axios.get("/get-report-tags"),
         ]);
         outstandingReports.value = reportsResponse.data;
-        emit('has-outstanding-reports', outstandingReports.value.length);
+        emit("has-outstanding-reports", outstandingReports.value.length);
 
         tags.value = tagsResponse.data.data;
     } catch (e) {
@@ -41,31 +32,28 @@ onMounted(() => {
     getData();
 });
 
-const reportSaved = () => getData();</script>
+const reportSaved = () => getData();
+</script>
 
 <template>
-    <ConfirmationModal :show="show" @close="close">
-        <template #title>
-            <h3>Outstanding Reports</h3>
-        </template>
+<PDialog modal dismissable-mask v-model:visible="show">
+  <template #header>
+    <h3>Outstanding Reports</h3>
+  </template>
 
-        <template #content>
-            <div class="">
-                <div v-if="outstandingReports?.length" class="mt-5">
-                    <div v-for="report in outstandingReports"
-                         :key="`${report.shift_date}-${report.start_time}-${report.shift_id}-${report.user_id}`"
-                         class="pb-3 mb-20 border-b border-gray-200">
-                        <ReportForm :report="report" @saved="reportSaved()"/>
-                    </div>
-                </div>
-                <div v-else>
-                    <h4 class="my-3 text-green-700 dark:text-green-400">No more reports outstanding 🎉</h4>
-                </div>
-            </div>
-        </template>
+  <div v-if="outstandingReports?.length" class="mt-5">
+    <div v-for="report in outstandingReports"
+         :key="`${report.shift_date}-${report.start_time}-${report.shift_id}-${report.user_id}`"
+         class="pb-3 mb-20 border-b border-gray-200">
+      <ReportForm :report="report" @saved="reportSaved()" />
+    </div>
+  </div>
+  <div v-else>
+    <h4 class="my-3 text-green-700 dark:text-green-400">No more reports outstanding 🎉</h4>
+  </div>
 
-        <template #footer>
-            <PButton type="button" style-type="secondary" @click="close">Close</PButton>
-        </template>
-    </ConfirmationModal>
+  <template #footer>
+    <PButton type="button" style-type="secondary" @click="show = false">Close</PButton>
+  </template>
+</PDialog>
 </template>
