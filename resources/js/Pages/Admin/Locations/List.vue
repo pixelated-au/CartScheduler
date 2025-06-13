928251ae850dab804a6fe26efc8201e6f8fe16b6
Ian Heggaton <ian@pixelated.com.au>
2025-06-13 17:05:57 +1000

    Refactor location list component for improved UI and drag-and-drop functionality - Update List.vue to use template refs and improve animation delays

--- resources/js/Pages/Admin/Locations/List.vue
+++ resources/js/Pages/Admin/Locations/List.vue
@@ -1,11 +1,10 @@
 <script setup>
+import { router } from "@inertiajs/vue3";
+import { useSortable } from "@vueuse/integrations/useSortable";
+import { nextTick, onMounted, onUnmounted, ref, watch, useTemplateRef } from "vue";
 import DragDrop from "@/Components/Icons/DragDrop.vue";
 import useToast from "@/Composables/useToast.js";
-
-import AppLayout from '@/Layouts/AppLayout.vue';
-import {router} from '@inertiajs/vue3';
-import {useSortable} from "@vueuse/integrations/useSortable";
-import {nextTick, onMounted, onUnmounted, ref, watch} from "vue";
+import AppLayout from "@/Layouts/AppLayout.vue";
 
 const props = defineProps({
     locations: Object.data,
@@ -15,28 +14,25 @@ const toast = useToast();
 
 const onNewLocation = () => {
     if (isSortingMode.value) {
-        toast.warning('You cannot create a new location while sorting is enabled');
+        toast.warning("You cannot create a new location while sorting is enabled");
         return;
     }
-    router.visit(route('admin.locations.create'));
+    router.visit(route("admin.locations.create"));
 };
 
 const locationClicked = (location) => {
     if (isSortingMode.value) {
         return;
     }
-    router.visit(route('admin.locations.edit', {id: location.id}));
+    router.visit(route("admin.locations.edit", { id: location.id }));
 };
 
-const locationsRef = ref(null);
-if (!locationsRef) {
-    throw new Error('An unexpected error occurred. The locationsRef is not defined. Please report this to the developers.');
-}
+const locationsRef = useTemplateRef("locationsRef");
 
 const storeSortOrder = async (locations) => {
     try {
-        await axios.put(route('admin.locations.sort-order'), {
-            locations: locations.map(location => location.id),
+        await axios.put(route("admin.locations.sort-order"), {
+            locations: locations.map((location) => location.id),
         });
     } catch (e) {
         return false;
@@ -59,26 +55,26 @@ const moveArrayElement = async (evt) => {
             if (!success) {
                 const element = locationsArray.splice(to, 1)[0];
                 locationsArray.splice(from, 0, element);
-                toast.error('There was an error while saving the sort order. The order has been reverted. Please try again.');
+                toast.error("There was an error while saving the sort order. The order has been reverted. Please try again.");
                 return;
             }
-            toast.success('The sort order has been saved successfully');
+            toast.success("The sort order has been saved successfully");
         });
     }
 };
 
-const {start, stop, option} = useSortable(locationsRef, props.locations.data, {
+const { start, stop, option } = useSortable(locationsRef, props.locations.data, {
     animation: 250,
     disabled: true,
     onUpdate: moveArrayElement,
 });
 
 const pause = () => {
-    option('disabled', true);
+    option("disabled", true);
 };
 const resume = () => {
-    console.log('resume', option('disabled'), option('animation'));
-    option('disabled', false);
+    console.log("resume", option("disabled"), option("animation"));
+    option("disabled", false);
 };
 
 onMounted(() => start());
@@ -93,76 +89,75 @@ watch(isSortingMode, (value) => {
     }
 });
 
-const transitionDelayStyle = (index) => `animation-delay: -${index * 0.37}s`;
-
+const transitionDelayStyle = (index) => `animation-delay: -${(index * 0.1173)}s`;
 </script>
 
 <template>
-    <AppLayout title="Locations">
-        <template #header>
-            <div class="flex justify-between">
-                <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Locations</h2>
-                <div class="flex">
-                    <PButton outline :style-type="isSortingMode ? 'danger' : 'secondary'" class="mx-3"
-                               @click="isSortingMode = !isSortingMode">
-                        <drag-drop color="currentColor" box="16"/>
-                        <span class="ml-3">{{ isSortingMode ? 'Stop sorting' : 'Sort locations' }}</span>
-                    </PButton>
-                    <PButton class="mx-3" style-type="primary" @click="onNewLocation">
-                        New Location
-                    </PButton>
-                </div>
-            </div>
-        </template>
+<AppLayout title="Locations">
+  <template #header>
+    <div class="flex justify-between">
+      <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Locations</h2>
+      <div class="flex">
+        <PButton outline
+                 :style-type="isSortingMode ? 'danger' : 'secondary'"
+                 class="mx-3"
+                 @click="isSortingMode = !isSortingMode">
+          <drag-drop color="currentColor" box="16" />
+          <span class="ml-3">{{ isSortingMode ? "Stop sorting" : "Sort locations" }}</span>
+        </PButton>
+        <PButton class="mx-3" style-type="primary" @click="onNewLocation">
+          New Location
+        </PButton>
+      </div>
+    </div>
+  </template>
 
-
-        <div
-            ref="locationsRef"
-            class="max-w-7xl mx-auto pt-10 sm:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            <div v-for="(location, idx) in locations.data"
-                 :key="location.id"
-                 class="bg-white dark:bg-slate-700 shadow-xl sm:rounded-lg p-6 cursor-pointer hover:bg-violet-100 hover:transition-colors"
-                 :class="{'is-sorting-mode': isSortingMode}"
-                 :style="transitionDelayStyle(idx)"
-                 @click="locationClicked(location)">
-                <div class="flex flex-col justify-between h-full dark:text-gray-100">
-                    <div>
-                        <h4 class="font-semibold">{{ location.name }}</h4>
-                        <div class="line-clamp-3">{{ location.clean_description }}</div>
-                    </div>
-                    <div>
-                        <div class="border-t border-gray-100 dark:border-gray-700 mt-3 pt-3"></div>
-                        <h5 class="font-semibold text-center">Shifts</h5>
-                        <div class="text-center text-4xl">{{ location.shifts.length }}</div>
-                    </div>
-                </div>
-            </div>
+  <div ref="locationsRef"
+       class="max-w-7xl mx-auto py-10 sm:px-6 grid grid-cols-1 content-start sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
+       :class="{ '!gap-4': isSortingMode }">
+    <div v-for="(location, idx) in locations.data"
+         :key="location.id"
+         class="bg-panel dark:bg-panel-dark shadow-sm card cursor-pointer duration-500 scale-[.98] transition-all"
+         :class="{ 'is-sorting-mode !scale-100': isSortingMode }"
+         :style="transitionDelayStyle(idx)"
+         @click="locationClicked(location)">
+      <div class="flex items-start gap-8 h-full dark:text-gray-100">
+        <div>
+          <h4 class="font-semibold">{{ location.name }}</h4>
+          <div class="line-clamp-3">{{ location.clean_description }}</div>
         </div>
-    </AppLayout>
+        <div>
+          <h5 class="font-semibold text-center">Shifts</h5>
+          <div class="text-center text-6xl">{{ location.shifts.length }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</AppLayout>
 </template>
 
 <style lang="scss">
 @import 'vue3-easy-data-table/dist/style.css';
 
 .sortable-ghost {
-    @apply opacity-40 border-dashed border-2 border-gray-700 dark:border-slate-300;
+  @apply opacity-40 border-dashed border-2 border-gray-700 dark:border-slate-300;
 }
 
 .is-sorting-mode {
-    @apply select-none shadow-2xl hover:bg-white dark:hover:bg-slate-700 cursor-grab;
-    animation: shake-left-right .9s infinite linear;
+  @apply select-none shadow-md hover:bg-white dark:hover:bg-slate-700 cursor-grab;
+  @apply ease-[linear(0,_0.245_3.5%,_0.466_7.2%,_0.653_10.9%,_0.816_14.8%,_0.887_16.8%,_0.951_18.8%,_1.009_20.9%,_1.06_23%,_1.103_25.1%,_1.142_27.3%,_1.175_29.6%,_1.201_31.9%,_1.216_33.6%,_1.229_35.4%,_1.239_37.2%,_1.245_39.1%,_1.249_41.1%,_1.25_43.1%,_1.248_45.2%,_1.242_47.4%,_1.228_51.1%,_1.205_55.3%,_1.178_59.6%,_1.085_72.9%,_1.059_77.2%,_1.038_81.2%,_1.02_85.7%,_1.008_90.2%,_1.002_94.8%,_1)];
+  animation-name: shake-left-right;
+  animation-duration: 1500ms;
+  animation-timing-function: cubic-bezier(0.86, 0, 0.14, 1);
+  animation-iteration-count: infinite;
 }
 
 @keyframes shake-left-right {
-    0%, 20%, 40%, 60%, 80%, 100% {
-        rotate: -1deg;
-        //transform: rotate3d(0, 0, 1, -1deg) translate3d(0, 0, 0);
-        //transform: rotate(-1deg) translate3d(0, 0, 0);
-    }
-    10%, 30%, 50%, 70%, 90% {
-        rotate: 1deg;
-        //transform: rotate3d(0, 0, 1, 1deg) translate3d(0, 0, 0);
-        //transform: rotate(1deg) translate3d(0, 0, 0);
-    }
+  0%, 20%, 40%, 60%, 80%, 100% {
+    rotate: -.25deg;
+  }
+  10%, 30%, 50%, 70%, 90% {
+    rotate: .25deg;
+  }
 }
 </style>
