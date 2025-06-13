6290708f73266d6b3dab0a4c8b43c4ee221cbc02
Ian Heggaton <ian@pixelated.com.au>
2025-06-14 07:16:25 +1000

    Set default values for new user creation in Add.vue
    - Set default values for role, is_unrestricted, and is_enabled properties in UserProfileForm.

--- resources/js/Pages/Admin/Users/Add.vue
+++ resources/js/Pages/Admin/Users/Add.vue
@@ -1,8 +1,7 @@
 <script setup>
-
-import AppLayout from '@/Layouts/AppLayout.vue';
-import UserProfileForm from '@/Pages/Admin/Users/Partials/UserProfileForm.vue';
-import {router} from '@inertiajs/vue3';
+import { router } from "@inertiajs/vue3";
+import AppLayout from "@/Layouts/AppLayout.vue";
+import UserProfileForm from "@/Pages/Admin/Users/Partials/UserProfileForm.vue";
 
 defineProps({
     availableRoles: Array,
@@ -10,27 +9,27 @@ defineProps({
 });
 
 const listRouteAction = () => {
-    router.visit(route('admin.users.index'));
+    router.visit(route("admin.users.index"));
 };
 </script>
 
 <template>
-    <AppLayout title="Create a new User">
-        <template #header>
-            <div class="flex justify-between">
-                <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">New User</h2>
-                <PButton class="mx-3" type="button" style-type="secondary" outline @click.prevent="listRouteAction">
-                    Back
-                </PButton>
-            </div>
-        </template>
+<AppLayout title="Create a new User">
+  <template #header>
+    <div class="flex justify-between">
+      <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">New User</h2>
+      <PButton class="mx-3" type="button" style-type="secondary" outline @click.prevent="listRouteAction">
+        Back
+      </PButton>
+    </div>
+  </template>
 
-        <div>
-            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-                <UserProfileForm :user="{}" action="add"/>
-            </div>
-        </div>
-    </AppLayout>
+  <div>
+    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+      <UserProfileForm :user="{ role: 'user', is_unrestricted: false, is_enabled: false }" action="add"/>
+    </div>
+  </div>
+</AppLayout>
 </template>
 
 <style lang="scss">
