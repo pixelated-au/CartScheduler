00b5e0bf3e0489b6638d645e1e63e58626b42c70
Ian Heggaton <ian@pixelated.com.au>
2025-06-14 08:11:07 +1000

    Refactor Edit.vue for improved readability
    - Updated route call to use string literals for route name.

--- resources/js/Pages/Admin/Users/Edit.vue
+++ resources/js/Pages/Admin/Users/Edit.vue
@@ -1,11 +1,10 @@
 <script setup>
-
-import AppLayout from '@/Layouts/AppLayout.vue';
-import UserProfileForm from '@/Pages/Admin/Users/Partials/UserProfileForm.vue';
+import { router, usePage } from "@inertiajs/vue3";
+import AppLayout from "@/Layouts/AppLayout.vue";
+import UserProfileForm from "@/Pages/Admin/Users/Partials/UserProfileForm.vue";
 import ShowLocationAvailabilityForm from "@/Pages/Profile/Partials/ShowLocationAvailabilityForm.vue";
 import ShowRegularAvailabilityForm from "@/Pages/Profile/Partials/ShowRegularAvailabilityForm.vue";
 import ShowVacationsAvailabilityForm from "@/Pages/Profile/Partials/ShowVacationsAvailabilityForm.vue";
-import {router, usePage} from "@inertiajs/vue3";
 
 defineProps({
     editUser: Object,
@@ -14,45 +13,49 @@ defineProps({
 });
 
 const listRouteAction = () => {
-    router.visit(route('admin.users.index'));
+    router.visit(route("admin.users.index"));
 };
 
 const canChooseLocations = !!usePage().props.enableUserLocationChoices;
 </script>
 
 <template>
-    <AppLayout :title="`User: ${editUser.data.name}`">
-        <template #header>
-            <div class="flex justify-between">
-                <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                    User {{ editUser.data.name }} </h2>
-                <PButton class="mx-3" type="button" style-type="secondary" outline @click.prevent="listRouteAction">
-                    Back
-                </PButton>
-            </div>
-        </template>
+<AppLayout :title="`User: ${editUser.data.name}`">
+  <template #header>
+    <div class="flex justify-between">
+      <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        User {{ editUser.data.name }}
+      </h2>
+      <PButton class="mx-3" type="button" style-type="secondary" outline @click.prevent="listRouteAction">
+        Back
+      </PButton>
+    </div>
+  </template>
 
-        <div>
-            <div class="max-w-7xl mx-auto pt-10 sm:px-6 lg:px-8">
-                <UserProfileForm :user="editUser.data"/>
-            </div>
-            <template v-if="$page.props.enableUserAvailability">
-                <div class="max-w-7xl mx-auto pt-10 sm:px-6 lg:px-8">
-                    <ShowVacationsAvailabilityForm :userId="editUser.data.id" :vacations="editUser.data.vacations"
-                                                   class="mt-10 sm:mt-0"/>
-                </div>
-                <div v-if="canChooseLocations" class="max-w-7xl mx-auto pt-10 sm:px-6 lg:px-8">
-                    <ShowLocationAvailabilityForm :userId="editUser.data.id"
-                                                  :selected-locations="editUser.data.selectedLocations"
-                                                  class="mt-10 sm:mt-0"/>
-                </div>
-                <div class="max-w-7xl mx-auto pt-10 sm:px-6 lg:px-8">
-                    <ShowRegularAvailabilityForm :userId="editUser.data.id" :availability="editUser.data.availability"
-                                                 class="mt-10 sm:mt-0"/>
-                </div>
-            </template>
-        </div>
-    </AppLayout>
+  <div>
+    <div class="max-w-7xl mx-auto pt-10 sm:px-6 lg:px-8">
+      <UserProfileForm :user="editUser.data"/>
+    </div>
+
+    <template v-if="$page.props.enableUserAvailability">
+      <div class="max-w-7xl mx-auto pt-10 sm:px-6 lg:px-8">
+        <ShowVacationsAvailabilityForm :userId="editUser.data.id"
+                                       :vacations="editUser.data.vacations"
+                                       class="mt-10 sm:mt-0"/>
+      </div>
+      <div v-if="canChooseLocations" class="max-w-7xl mx-auto pt-10 sm:px-6 lg:px-8">
+        <ShowLocationAvailabilityForm :userId="editUser.data.id"
+                                      :selected-locations="editUser.data.selectedLocations"
+                                      class="mt-10 sm:mt-0"/>
+      </div>
+      <div class="max-w-7xl mx-auto pt-10 sm:px-6 lg:px-8">
+        <ShowRegularAvailabilityForm :userId="editUser.data.id"
+                                     :availability="editUser.data.availability"
+                                     class="mt-10 sm:mt-0"/>
+      </div>
+    </template>
+  </div>
+</AppLayout>
 </template>
 
 <style lang="scss">
