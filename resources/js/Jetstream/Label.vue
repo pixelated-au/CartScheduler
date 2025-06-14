2d010afae7f15a262b26f75c3f93b8a6b5bfa112
Ian Heggaton <ian@pixelated.com.au>
2025-06-15 07:51:23 +1000

    Update Label component's appearance and functionality
    - Enhance Label component with error handling and slot support.

--- resources/js/Jetstream/Label.vue
+++ resources/js/Jetstream/Label.vue
@@ -1,17 +1,53 @@
 <script setup>
-defineProps({
+import { computed } from "vue";
+import LabelError from "@/Jetstream/LabelError.vue";
+
+const props = defineProps({
     value: String,
     isDisabled: {
         type: Boolean,
         default: false,
     },
+    errorKey: {
+        type: String,
+        default: undefined,
+    },
+    form: {
+        type: Object,
+        default: undefined,
+        validator: (value, props) => typeof value === "object"
+            && !Array.isArray(value)
+            && value !== null
+            && props.errorKey,
+    },
 });
+
+const invalid = computed(() => !!props.form?.invalid(props.errorKey));
 </script>
 
 <template>
-<label class="block"
-       :class="{ 'text-gray-800 dark:text-gray-200': !isDisabled, 'text-gray-400 dark:text-gray-600': isDisabled }">
-  <span v-if="value" class="font-medium">{{ value }}</span>
-  <span v-else><slot/></span>
+<label class="grid gap-2"
+       :class="[
+         [$slots['default'] ? 'grid-cols-[max-content_1fr]' : 'grid-cols-1'],
+       ]">
+  <span class="flex items-center transition-colors duration-300"
+        :class="[
+          { 'text-neutral-800 dark:text-neutral-200': !isDisabled, 'text-neutral-400 dark:text-neutral-600': isDisabled },
+          { '!text-red-500':invalid },
+        ]">
+    <LabelError :invalid />
+    <span class="font-medium">
+      {{ value }}
+    </span>
+  </span>
+
+  <template v-if="$slots.default">
+    <span class="block">
+      <slot />
+    </span>
+    <span v-if="$slots.end" class="block col-span-2">
+      <slot name="end" />
+    </span>
+  </template>
 </label>
 </template>
