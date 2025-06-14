ba35820d1b8c8965cd42ff35a74a89a0bc26de07
Ian Heggaton <ian@pixelated.com.au>
2025-06-14 11:55:22 +1000

    Refactor SectionTitle component layout - Update layout to use flexbox for title and description

--- resources/js/Jetstream/SectionTitle.vue
+++ resources/js/Jetstream/SectionTitle.vue
@@ -1,17 +1,11 @@
 <template>
-    <div class="md:col-span-1 flex justify-between">
-        <div class="px-4 sm:px-0">
-            <h3 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                <slot name="title"/>
-            </h3>
+<div class="flex flex-col px-4 sm:px-0 gap-2">
+  <h3 class="text-lg font-medium text-neutral-900 dark:text-neutral-100">
+    <slot name="title"/>
+  </h3>
 
-            <p class="mt-1 text-sm text-gray-600 dark:text-gray-300">
-                <slot name="description"/>
-            </p>
-        </div>
-
-        <div class="px-4 sm:px-0">
-            <slot name="aside"/>
-        </div>
-    </div>
+  <p class="text-sm text-neutral-600 dark:text-neutral-300">
+    <slot name="description"/>
+  </p>
+</div>
 </template>
