394bc6a70110ca905b42e0a1e3d86024a8ef1928
Ian Heggaton <ian@pixelated.com.au>
2025-06-19 08:49:10 +1000

    Update ESLint configuration
    - Update rules for JavaScript files.
    - Update rules for Vue files.
    - Update stylistic rules.

--- eslint.config.js
+++ eslint.config.js
@@ -9,232 +9,232 @@ import stylistic from "@stylistic/eslint-plugin";
 import globals from "globals";
 
 export default defineConfig([
-    globalIgnores(["node_modules", "vendor"]),
-    ...pluginVue.configs["flat/essential"],
-    {
-        name: "eslint-defaults",
-        files: ["./*.js", "./resources/js/**/*.{js,vue}"],
-        plugins: { js, "@stylistic": stylistic },
-        extends: ["js/recommended", importPlugin.flatConfigs.recommended],
-        languageOptions: {
-            ecmaVersion: 2020,
-            globals: {
-                ...globals.browser,
-                ...globals.node,
-                route: "readonly",
-                axios: "readonly",
-            },
-        },
+  globalIgnores(["node_modules", "vendor"]),
+  {
+    name: "eslint-defaults",
+    files: ["./*.js", "./resources/js/**/*.{js,vue}"],
+    plugins: { js, "@stylistic": stylistic },
+    extends: ["js/recommended", pluginVue.configs["flat/essential"], importPlugin.flatConfigs.recommended],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        route: "readonly",
+        axios: "readonly",
+      },
     },
-    {
-        name: "eslint-css",
-        files: ["./resources/css/**/*.css"],
-        plugins: { css },
-        language: "css/css",
-        rules: {
-            "css/no-empty-blocks": "error",
-            "css/no-duplicate-imports": "error",
-            "css/no-important": "error",
-            "css/no-invalid-at-rules": "error",
-            "css/no-invalid-properties": "error",
-            "css/use-baseline": "warn",
-        },
+  },
+  {
+    name: "eslint-css",
+    files: ["./resources/css/**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    rules: {
+      "css/no-empty-blocks": "error",
+      "css/no-duplicate-imports": "error",
+      "css/no-important": "error",
+      "css/no-invalid-at-rules": "error",
+      "css/no-invalid-properties": "error",
+      "css/use-baseline": "warn",
     },
-    {
-        name: "eslint-js",
-        rules: {
-            "no-empty": ["warn", { "allowEmptyCatch": true }],
-            "no-unused-vars": "warn",
-        },
+  },
+  {
+    name: "eslint-js",
+    rules: {
+      "no-empty": ["warn", { allowEmptyCatch: true }],
+      "no-unused-expressions": ["warn", { allowShortCircuit: true, allowTernary: true }],
+      "no-unused-vars": "warn",
     },
-    {
-        name: "eslint-imports",
-        rules: {
-            "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
-            "import/default": "off",
-            "import/namespace": "off",
-            "import/no-named-as-default": "off",
-            "import/no-named-as-default-member": "off",
-            "import/no-unresolved": "off",
+  },
+  {
+    name: "eslint-imports",
+    rules: {
+      "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
+      "import/default": "off",
+      "import/namespace": "off",
+      "import/no-named-as-default": "off",
+      "import/no-named-as-default-member": "off",
+      "import/no-unresolved": "off",
       /**
        * import/order:
        * If the ordering of `eslint --fix` isn't working, it's likely because there is an unbound import.
        * Eg import 'xyz.css' as opposed to import abc from 'abc'.
        * @see https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/order.md#limitations-of---fix
        */
-            "import/order": "warn",
-        },
+      "import/order": "warn",
     },
-    {
-        name: "eslint-vue",
-        rules: {
-            "vue/block-order": [
-                "error",
-                {
-                    order: ["script:not([setup])", "script[setup]", "template", "style"],
-                },
-            ],
-            "vue/block-tag-newline": ["error", {
-                blocks: {
-                    script: {
-                        singleline: "always",
-                        multiline: "always",
-                    },
-                    template: {
-                        singleline: "always",
-                        multiline: "always",
-                        maxEmptyLines: 0,
-                    },
-                },
-            }],
-            "vue/first-attribute-linebreak": ["warn", { singleline: "ignore", multiline: "beside" }],
-            "vue/html-closing-bracket-newline": [
-                "warn",
-                {
-                    singleline: "never",
-                    multiline: "never",
-                    selfClosingTag: {
-                        singleline: "never",
-                        multiline: "never",
-                    },
-                },
-            ],
-            "vue/html-comment-indent": ["warn", 4],
-            "vue/html-comment-content-newline": [
-                "warn",
-                {
-                    singleline: "never",
-                    multiline: "ignore",
-                },
-                {
-                    exceptions: [],
-                },
-            ],
-            "vue/html-comment-content-spacing": ["warn", "always", { exceptions: ["*", "#"] }],
-            "vue/html-indent": [
-                "warn",
-                2,
-                {
-                    attribute: 2,
-                    baseIndent: 0,
-                    closeBracket: 0,
-                    alignAttributesVertically: true,
-                    ignores: [],
-                },
-            ],
-            "vue/multiline-html-element-content-newline": [
-                "error",
-                {
-                    ignoreWhenEmpty: true,
-                    ignores: ["pre", "textarea"],
-                    allowEmptyLines: false,
-                },
-            ],
-            "vue/no-irregular-whitespace": ["error"],
-            "vue/no-unused-vars": ["warn"],
-            "vue/max-attributes-per-line": ["warn", { singleline: 100, multiline: 1 }],
-            "vue/multi-word-component-names": ["off"],
-            "vue/no-multi-spaces": ["warn", { ignoreProperties: false }],
-            "vue/object-curly-spacing": ["warn", "always"],
-            "vue/prefer-use-template-ref": ["warn"],
-            "vue/padding-line-between-blocks": ["warn", "always"],
-            "vue/padding-line-between-tags": [
-                "warn",
-                [
-                    { blankLine: "always", prev: "template", next: "*" },
-                    { blankLine: "always", prev: "*", next: "template" },
-                ],
-            ],
-            "vue/script-indent": ["warn", 4, { baseIndent: 0, switchCase: 1 }],
-            "vue/singleline-html-element-content-newline": [
-                "error",
-                {
-                    ignoreWhenNoAttributes: true,
-                    ignoreWhenEmpty: true,
-                    ignores: ["pre", "textarea", "template"],
-                    externalIgnores: [],
-                },
-            ],
-            "vue/space-infix-ops": ["warn"],
-            "vue/space-unary-ops": [
-                "error",
-                {
-                    words: true,
-                    nonwords: false,
-                    overrides: { "?": true },
-                },
-            ],
+  },
+  {
+    name: "eslint-vue",
+    rules: {
+      "vue/block-order": [
+        "error",
+        {
+          order: ["script:not([setup])", "script[setup]", "template", "style"],
         },
-    },
-    {
-        name: "eslint-stylistic",
-        rules: {
-            "@stylistic/array-bracket-newline": ["off"],
-            "@stylistic/array-element-newline": ["error", { consistent: true }],
-            "@stylistic/arrow-parens": ["error", "always"],
-            "@stylistic/brace-style": ["warn", "1tbs"],
-            "@stylistic/comma-dangle": ["warn", "always-multiline"],
-            "@stylistic/comma-spacing": ["warn"],
-            "@stylistic/function-call-argument-newline": ["warn", "consistent"],
-            "@stylistic/function-paren-newline": ["warn", "consistent"],
-            "@stylistic/indent": [
-                "warn",
-                4,
-                {
-                    ignoreComments: true,
-                    SwitchCase: 1,
-                },
-            ],
-            "@stylistic/no-multiple-empty-lines": ["warn", { max: 1, maxEOF: 1, maxBOF: 0 }],
-            "@stylistic/no-trailing-spaces": ["warn"],
-            "@stylistic/member-delimiter-style": [
-                "warn",
-                {
-                    multiline: {
-                        delimiter: "semi",
-                        requireLast: true,
-                    },
-                    singleline: {
-                        delimiter: "semi",
-                        requireLast: false,
-                    },
-                },
-            ],
-            "@stylistic/multiline-ternary": ["error", "always-multiline"],
-            "@stylistic/object-curly-newline": ["off"],
-            "@stylistic/object-curly-spacing": ["error", "always"],
-            "@stylistic/padding-line-between-statements": [
-                "error",
-                { blankLine: "always", prev: "*", next: "import" },
-                { blankLine: "always", prev: "import", next: "*" },
-                { blankLine: "never", prev: "import", next: "import" },
-            ],
-            "@stylistic/quotes": ["warn", "double"],
-            "@stylistic/semi": ["warn", "always"],
-            "@stylistic/space-before-function-paren": [
-                "warn",
-                {
-                    anonymous: "never",
-                    named: "never",
-                    asyncArrow: "always",
-                },
-            ],
-            "spaced-comment": [
-                "error",
-                "always",
-                {
-                    line: {
-                        markers: ["/", "##", "todo", "TODO"],
-                        exceptions: ["-", "+"],
-                    },
-                    block: {
-                        markers: ["!"],
-                        exceptions: ["*"],
-                        balanced: true,
-                    },
-                },
-            ],
-            "@stylistic/space-unary-ops": ["error"],
+      ],
+      "vue/block-tag-newline": ["error", {
+        blocks: {
+          script: {
+            singleline: "always",
+            multiline: "always",
+          },
+          template: {
+            singleline: "always",
+            multiline: "always",
+            maxEmptyLines: 0,
+          },
         },
+      }],
+      "vue/first-attribute-linebreak": ["warn", { singleline: "ignore", multiline: "beside" }],
+      "vue/html-closing-bracket-newline": [
+        "warn",
+        {
+          singleline: "never",
+          multiline: "never",
+          selfClosingTag: {
+            singleline: "never",
+            multiline: "never",
+          },
+        },
+      ],
+      "vue/html-comment-indent": ["warn", 2],
+      "vue/html-comment-content-newline": [
+        "warn",
+        {
+          singleline: "never",
+          multiline: "ignore",
+        },
+        {
+          exceptions: [],
+        },
+      ],
+      "vue/html-comment-content-spacing": ["warn", "always", { exceptions: ["*", "#"] }],
+      "vue/html-indent": [
+        "warn",
+        2,
+        {
+          attribute: 2,
+          baseIndent: 1,
+          closeBracket: 0,
+          alignAttributesVertically: true,
+          ignores: [],
+        },
+      ],
+      "vue/multiline-html-element-content-newline": [
+        "error",
+        {
+          ignoreWhenEmpty: true,
+          ignores: ["pre", "textarea"],
+          allowEmptyLines: false,
+        },
+      ],
+      "vue/no-irregular-whitespace": ["error"],
+      "vue/no-unused-vars": ["warn"],
+      "vue/max-attributes-per-line": ["warn", { singleline: 100, multiline: 1 }],
+      "vue/multi-word-component-names": ["off"],
+      "vue/no-multi-spaces": ["warn", { ignoreProperties: false }],
+      "vue/object-curly-spacing": ["warn", "always"],
+      "vue/prefer-use-template-ref": ["warn"],
+      "vue/padding-line-between-blocks": ["warn", "always"],
+      "vue/padding-line-between-tags": [
+        "warn",
+        [
+          { blankLine: "always", prev: "template", next: "*" },
+          { blankLine: "always", prev: "*", next: "template" },
+        ],
+      ],
+      "vue/script-indent": ["warn", 2, { baseIndent: 0, switchCase: 1 }],
+      "vue/singleline-html-element-content-newline": [
+        "error",
+        {
+          ignoreWhenNoAttributes: true,
+          ignoreWhenEmpty: true,
+          ignores: ["pre", "textarea", "template"],
+          externalIgnores: [],
+        },
+      ],
+      "vue/space-infix-ops": ["warn"],
+      "vue/space-unary-ops": [
+        "error",
+        {
+          words: true,
+          nonwords: false,
+          overrides: { "?": true },
+        },
+      ],
     },
+  },
+  {
+    name: "eslint-stylistic",
+    rules: {
+      "@stylistic/array-bracket-newline": ["off"],
+      "@stylistic/array-element-newline": ["error", { consistent: true }],
+      "@stylistic/arrow-parens": ["error", "always"],
+      "@stylistic/brace-style": ["warn", "1tbs"],
+      "@stylistic/comma-dangle": ["warn", "always-multiline"],
+      "@stylistic/comma-spacing": ["warn"],
+      "@stylistic/function-call-argument-newline": ["warn", "consistent"],
+      "@stylistic/function-paren-newline": ["warn", "consistent"],
+      "@stylistic/indent": [
+        "warn",
+        2,
+        {
+          ignoreComments: true,
+          SwitchCase: 1,
+        },
+      ],
+      "@stylistic/no-multiple-empty-lines": ["warn", { max: 1, maxEOF: 1, maxBOF: 0 }],
+      "@stylistic/no-trailing-spaces": ["warn"],
+      "@stylistic/member-delimiter-style": [
+        "warn",
+        {
+          multiline: {
+            delimiter: "semi",
+            requireLast: true,
+          },
+          singleline: {
+            delimiter: "semi",
+            requireLast: false,
+          },
+        },
+      ],
+      "@stylistic/multiline-ternary": ["error", "always-multiline"],
+      "@stylistic/object-curly-newline": ["off"],
+      "@stylistic/object-curly-spacing": ["error", "always"],
+      "@stylistic/padding-line-between-statements": [
+        "error",
+        { blankLine: "always", prev: "*", next: "import" },
+        { blankLine: "always", prev: "import", next: "*" },
+        { blankLine: "never", prev: "import", next: "import" },
+      ],
+      "@stylistic/quotes": ["warn", "double"],
+      "@stylistic/semi": ["warn", "always"],
+      "@stylistic/space-before-function-paren": [
+        "warn",
+        {
+          anonymous: "never",
+          named: "never",
+          asyncArrow: "always",
+        },
+      ],
+      "spaced-comment": [
+        "error",
+        "always",
+        {
+          line: {
+            markers: ["/", "##", "todo", "TODO"],
+            exceptions: ["-", "+"],
+          },
+          block: {
+            markers: ["!"],
+            exceptions: ["*"],
+            balanced: true,
+          },
+        },
+      ],
+      "@stylistic/space-unary-ops": ["error"],
+    },
+  },
 ]);
