94dce70b4b3721b1651667518cd2beb519a1d5d1
Ian Heggaton <ian@pixelated.com.au>
2025-06-26 05:06:45 +1000

    Rename `useEChartsTheme` to `EChartsTheme` function as it's not a composable

--- resources/js/Pages/Admin/Dashboard/FilledShiftsChart.vue
+++ resources/js/Pages/Admin/Dashboard/FilledShiftsChart.vue
@@ -5,56 +5,56 @@ import { use } from "echarts/core";
 import { CanvasRenderer } from "echarts/renderers";
 import { computed, inject, provide, watch } from "vue";
 import VChart, { THEME_KEY } from "vue-echarts";
-import useEChartsTheme from "@/lib/useEChartsTheme";
+import EChartsTheme from "@/lib/eChartsTheme.js";
 
 const props = defineProps({
-    shiftData: Array,
+  shiftData: Array,
 });
 
 use([CanvasRenderer, TitleComponent, TooltipComponent, LegendComponent, BarChart, GridComponent]);
 
 const isDarkMode = inject("darkMode", false);
 
-const theme = useEChartsTheme(isDarkMode);
+const theme = EChartsTheme(isDarkMode);
 
 watch(theme, () => {
-    console.log("theme changed");
+  console.log("theme changed");
 }, { deep: true });
 
 provide(THEME_KEY, theme);
 
 const shiftsData = computed(() => ({
-    responsive: true,
-    maintainAspectRatio: false,
-    tooltip: {
-        trigger: "item",
-        formatter: "{a} <br/>{b}: {c}",
+  responsive: true,
+  maintainAspectRatio: false,
+  tooltip: {
+    trigger: "item",
+    formatter: "{a} <br/>{b}: {c}",
+  },
+  xAxis: {
+    data: props.shiftData.map((item) => item.date),
+    axisLabel: {
+      interval: 0,
+      rotate: 45,
     },
-    xAxis: {
-        data: props.shiftData.map((item) => item.date),
-        axisLabel: {
-            interval: 0,
-            rotate: 45,
-        },
+  },
+  yAxis: {},
+  series: [
+    {
+      data: props.shiftData.map((item) => item.shifts_filled),
+      type: "bar",
+      stack: "x",
+      name: "Filled Shifts",
     },
-    yAxis: {},
-    series: [
-        {
-            data: props.shiftData.map((item) => item.shifts_filled),
-            type: "bar",
-            stack: "x",
-            name: "Filled Shifts",
-        },
-        {
-            data: props.shiftData.map((item) => item.shifts_available - item.shifts_filled),
-            type: "bar",
-            stack: "x",
-            name: "Shifts Available",
-        },
-    ],
+    {
+      data: props.shiftData.map((item) => item.shifts_available - item.shifts_filled),
+      type: "bar",
+      stack: "x",
+      name: "Shifts Available",
+    },
+  ],
 }));
 </script>
 
 <template>
-<VChart class="min-h-72" :option="shiftsData"/>
+  <VChart class="min-h-72" :option="shiftsData"/>
 </template>
