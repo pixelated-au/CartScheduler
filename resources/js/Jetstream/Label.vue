cfc355338d75d1d01e82192797a5d49eba3ba58f
Ian Heggaton <ian@pixelated.com.au>
2025-06-26 10:33:49 +1000

    Format Label component for improved readability - Update Label component template for better structure.

--- resources/js/Jetstream/Label.vue
+++ resources/js/Jetstream/Label.vue
@@ -3,51 +3,51 @@ import { computed } from "vue";
 import LabelError from "@/Jetstream/LabelError.vue";
 
 const props = defineProps({
-    value: String,
-    isDisabled: {
-        type: Boolean,
-        default: false,
-    },
-    errorKey: {
-        type: String,
-        default: undefined,
-    },
-    form: {
-        type: Object,
-        default: undefined,
-        validator: (value, props) => typeof value === "object"
-            && !Array.isArray(value)
-            && value !== null
-            && props.errorKey,
-    },
+  value: String,
+  isDisabled: {
+    type: Boolean,
+    default: false,
+  },
+  errorKey: {
+    type: String,
+    default: undefined,
+  },
+  form: {
+    type: Object,
+    default: undefined,
+    validator: (value, props) => typeof value === "object"
+      && !Array.isArray(value)
+      && value !== null
+      && props.errorKey,
+  },
 });
 
 const invalid = computed(() => !!props.form?.invalid(props.errorKey));
 </script>
 
 <template>
-<label class="grid gap-2"
-       :class="[
-         [$slots['default'] ? 'grid-cols-[max-content_1fr]' : 'grid-cols-1'],
-       ]">
-  <span class="flex items-center transition-colors duration-300"
-        :class="[
-          { 'text-neutral-800 dark:text-neutral-200': !isDisabled, 'text-neutral-400 dark:text-neutral-600': isDisabled },
-          { '!text-red-500':invalid },
-        ]">
-    <LabelError :invalid />
-    <span class="font-medium">
-      {{ value }}
+  <label class="grid gap-2"
+         :class="[
+           [$slots['default'] ? 'grid-cols-[max-content_1fr]' : 'grid-cols-1'],
+         ]">
+    <span class="flex items-center transition-colors duration-300"
+          :class="[
+            { 'text-neutral-800 dark:text-neutral-200': !isDisabled, 'text-neutral-400 dark:text-neutral-600': isDisabled },
+            { '!text-red-500':invalid },
+          ]">
+      <LabelError :invalid />
+      <span class="font-medium">
+        {{ value }}
+      </span>
     </span>
-  </span>
 
-  <template v-if="$slots.default">
-    <span class="block">
-      <slot />
-    </span>
-    <span v-if="$slots.end" class="block col-span-2">
-      <slot name="end" />
-    </span>
-  </template>
-</label>
+    <template v-if="$slots.default">
+      <span class="block">
+        <slot />
+      </span>
+      <span v-if="$slots.end" class="block col-span-2">
+        <slot name="end" />
+      </span>
+    </template>
+  </label>
 </template>
