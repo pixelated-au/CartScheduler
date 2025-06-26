b25ed42c9ab9f113b92a62cf5bfb1c00f845d03f
Ian Heggaton <ian@pixelated.com.au>
2025-06-26 10:34:00 +1000

    Refactor user add and edit pages
    - Minor template formatting changes in the Add.vue class.
    - Minor template formatting changes in the Edit.vue class.

--- resources/js/Pages/Admin/Users/Add.vue
+++ resources/js/Pages/Admin/Users/Add.vue
@@ -5,34 +5,34 @@ import AppLayout from "@/Layouts/AppLayout.vue";
 import UserProfileForm from "@/Pages/Admin/Users/Partials/UserProfileForm.vue";
 
 defineProps({
-    availableRoles: Array,
-    permissions: Object,
+  availableRoles: Array,
+  permissions: Object,
 });
 
 const route = inject("route");
 
 const listRouteAction = () => {
-    router.visit(route("admin.users.index"));
+  router.visit(route("admin.users.index"));
 };
 </script>
 
 <template>
-<AppLayout title="Create a new User">
-  <template #header>
-    <div class="flex justify-between">
-      <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">New User</h2>
-      <PButton class="mx-3" type="button" style-type="secondary" outline @click.prevent="listRouteAction">
-        Back
-      </PButton>
-    </div>
-  </template>
+  <AppLayout title="Create a new User">
+    <template #header>
+      <div class="flex justify-between">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">New User</h2>
+        <PButton class="mx-3" type="button" style-type="secondary" outline @click.prevent="listRouteAction">
+          Back
+        </PButton>
+      </div>
+    </template>
 
-  <div>
-    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-      <UserProfileForm :user="{ role: 'user', is_unrestricted: false, is_enabled: false }" action="add" />
+    <div>
+      <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+        <UserProfileForm :user="{ role: 'user', is_unrestricted: false, is_enabled: false }" action="add" />
+      </div>
     </div>
-  </div>
-</AppLayout>
+  </AppLayout>
 </template>
 
 <style lang="scss">
--- resources/js/Pages/Admin/Users/Edit.vue
+++ resources/js/Pages/Admin/Users/Edit.vue
@@ -8,57 +8,57 @@ import ShowRegularAvailabilityForm from "@/Pages/Profile/Partials/ShowRegularAva
 import ShowVacationsAvailabilityForm from "@/Pages/Profile/Partials/ShowVacationsAvailabilityForm.vue";
 
 defineProps({
-    editUser: Object,
-    availableRoles: Array,
-    permissions: Object,
+  editUser: Object,
+  availableRoles: Array,
+  permissions: Object,
 });
 
 const route = inject("route");
 
 const listRouteAction = () => {
-    router.visit(route("admin.users.index"));
+  router.visit(route("admin.users.index"));
 };
 
 const canChooseLocations = !!usePage().props.enableUserLocationChoices;
 </script>
 
 <template>
-<AppLayout :title="`User: ${editUser.data.name}`">
-  <template #header>
-    <div class="flex justify-between">
-      <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-        User {{ editUser.data.name }}
-      </h2>
-      <PButton class="mx-3" type="button" style-type="secondary" outline @click.prevent="listRouteAction">
-        Back
-      </PButton>
-    </div>
-  </template>
-
-  <div>
-    <div class="max-w-7xl mx-auto pt-10 sm:px-6 lg:px-8">
-      <UserProfileForm :user="editUser.data" />
-    </div>
-
-    <template v-if="$page.props.enableUserAvailability">
-      <div class="max-w-7xl mx-auto pt-10 sm:px-6 lg:px-8">
-        <ShowVacationsAvailabilityForm :userId="editUser.data.id"
-                                       :vacations="editUser.data.vacations"
-                                       class="mt-10 sm:mt-0" />
-      </div>
-      <div v-if="canChooseLocations" class="max-w-7xl mx-auto pt-10 sm:px-6 lg:px-8">
-        <ShowLocationAvailabilityForm :userId="editUser.data.id"
-                                      :selected-locations="editUser.data.selectedLocations"
-                                      class="mt-10 sm:mt-0" />
-      </div>
-      <div class="max-w-7xl mx-auto pt-10 sm:px-6 lg:px-8">
-        <ShowRegularAvailabilityForm :userId="editUser.data.id"
-                                     :availability="editUser.data.availability"
-                                     class="mt-10 sm:mt-0" />
+  <AppLayout :title="`User: ${editUser.data.name}`">
+    <template #header>
+      <div class="flex justify-between">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+          User {{ editUser.data.name }}
+        </h2>
+        <PButton class="mx-3" type="button" style-type="secondary" outline @click.prevent="listRouteAction">
+          Back
+        </PButton>
       </div>
     </template>
-  </div>
-</AppLayout>
+
+    <div>
+      <div class="max-w-7xl mx-auto pt-10 sm:px-6 lg:px-8">
+        <UserProfileForm :user="editUser.data" />
+      </div>
+
+      <template v-if="$page.props.enableUserAvailability">
+        <div class="max-w-7xl mx-auto pt-10 sm:px-6 lg:px-8">
+          <ShowVacationsAvailabilityForm :userId="editUser.data.id"
+                                         :vacations="editUser.data.vacations"
+                                         class="mt-10 sm:mt-0" />
+        </div>
+        <div v-if="canChooseLocations" class="max-w-7xl mx-auto pt-10 sm:px-6 lg:px-8">
+          <ShowLocationAvailabilityForm :userId="editUser.data.id"
+                                        :selected-locations="editUser.data.selectedLocations"
+                                        class="mt-10 sm:mt-0" />
+        </div>
+        <div class="max-w-7xl mx-auto pt-10 sm:px-6 lg:px-8">
+          <ShowRegularAvailabilityForm :userId="editUser.data.id"
+                                       :availability="editUser.data.availability"
+                                       class="mt-10 sm:mt-0" />
+        </div>
+      </template>
+    </div>
+  </AppLayout>
 </template>
 
 <style lang="scss">
