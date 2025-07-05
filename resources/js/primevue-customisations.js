6c7ef09659da9d7118c1a4be3ad5a6820a12fd9b
Ian Heggaton <ian@pixelated.com.au>
2025-07-06 08:29:03 +1000

    Update color definitions for PrimeVue customisations
    - Update surface color palette.
    - Update primary color palette.

--- resources/js/primevue-customisations.js
+++ resources/js/primevue-customisations.js
@@ -3,43 +3,43 @@ import Theme from "@primeuix/themes/aura";
 import colors from "tailwindcss/colors";
 
 const surface = {
-    0: "#ffffff",
-    50: colors.neutral[50],
-    100: colors.neutral[100],
-    200: colors.neutral[200],
-    300: colors.neutral[300],
-    400: colors.neutral[400],
-    500: colors.neutral[500],
-    600: colors.neutral[600],
-    700: colors.neutral[700],
-    800: colors.neutral[800],
-    900: colors.neutral[900],
-    950: colors.neutral[950],
+  0: "#ffffff",
+  50: colors.neutral[50],
+  100: colors.neutral[100],
+  200: colors.neutral[200],
+  300: colors.neutral[300],
+  400: colors.neutral[400],
+  500: colors.neutral[500],
+  600: colors.neutral[600],
+  700: colors.neutral[700],
+  800: colors.neutral[800],
+  900: colors.neutral[900],
+  950: colors.neutral[950],
 };
 
 export default definePreset(Theme, {
-    semantic: {
-        primary: {
-            50: colors.purple[50],
-            100: colors.purple[100],
-            200: colors.purple[200],
-            300: colors.purple[300],
-            400: colors.purple[400],
-            500: colors.purple[500],
-            600: colors.purple[600],
-            700: colors.purple[700],
-            800: colors.purple[800],
-            900: colors.purple[900],
-            950: colors.purple[950],
-        },
-
-        colorScheme: {
-            light: {
-                surface,
-            },
-            dark: {
-                surface,
-            },
-        },
+  semantic: {
+    primary: {
+      50: colors.purple[50],
+      100: colors.purple[100],
+      200: colors.purple[200],
+      300: colors.purple[300],
+      400: colors.purple[400],
+      500: colors.purple[500],
+      600: colors.purple[600],
+      700: colors.purple[700],
+      800: colors.purple[800],
+      900: colors.purple[900],
+      950: colors.purple[950],
     },
+
+    colorScheme: {
+      light: {
+        surface,
+      },
+      dark: {
+        surface,
+      },
+    },
+  },
 });
