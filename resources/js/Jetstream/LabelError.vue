e5ed41db94422e9dde72ca30cff193c9549d78c2
Ian Heggaton <ian@pixelated.com.au>
2025-07-06 09:40:35 +1000

    Update LabelError component styles
    - Add animation to the error icon.

--- resources/js/Jetstream/LabelError.vue
+++ resources/js/Jetstream/LabelError.vue
@@ -1,15 +1,17 @@
 <script setup>
 defineProps({
-    invalid: Boolean,
+  invalid: Boolean,
 });
 </script>
 
 <template>
-<span class="grid items-center font-medium transition-[grid-template-columns] duration-300"
-      :class="[!invalid ? 'grid-cols-[0fr] delay-300' : 'grid-cols-[1fr] delay-0' ]">
-  <span class="flex items-center overflow-hidden transition-transform duration-300"
-        :class="[!invalid ? 'scale-0 delay-0' : 'scale-100 delay-300 ease-bounce-in']">
-    <span class="iconify mdi--warning-circle-outline mr-1"></span>
+  <span class="grid items-center font-medium transition-[grid-template-columns] duration-300 relative"
+        :class="[!invalid ? 'grid-cols-[0fr] delay-300' : 'grid-cols-[1fr] delay-0' ]">
+    <span class="flex items-center overflow-hidden transition-transform duration-300 z-10"
+          :class="[!invalid ? 'scale-0 delay-0' : 'scale-100 delay-300 ease-bounce-in']">
+      <span class="iconify mdi--warning-circle-outline"></span>
+    </span>
+    <span class="absolute inset-0 inline-block bubble-pop z-1 [--delay:300ms] [--scale:2] [--color:var(--p-button-text-warn-color)]"
+          :class="[ { 'animate': invalid } ]"></span>
   </span>
-</span>
 </template>
