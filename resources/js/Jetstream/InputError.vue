bb4510407b742e46175ed3d22adbe99f265c7bd3
Ian Heggaton <ian@pixelated.com.au>
2025-07-06 09:41:08 +1000

    Update InputError component styles
    - Change error message color to warning

--- resources/js/Jetstream/InputError.vue
+++ resources/js/Jetstream/InputError.vue
@@ -1,12 +1,12 @@
 <script setup>
-import { computed, watch, ref } from "vue";
 import { promiseTimeout } from "@vueuse/core";
+import { computed, watch, ref } from "vue";
 
 const props = defineProps({
-    message: {
-        type: String,
-        default: undefined,
-    },
+  message: {
+    type: String,
+    default: undefined,
+  },
 });
 
 const trimmedMessage = computed(() => (typeof props.message === "string") ? props.message.trim() : undefined);
@@ -14,21 +14,21 @@ const trimmedMessage = computed(() => (typeof props.message === "string") ? prop
 const error = ref(undefined);
 
 watch(trimmedMessage, async (message) => {
-    if (message) {
-        error.value = message;
-        return;
-    }
-    await promiseTimeout(1000);
-    error.value = undefined;
+  if (message) {
+    error.value = message;
+    return;
+  }
+  await promiseTimeout(1000);
+  error.value = undefined;
 });
 </script>
 
 <template>
-<div class="grid items-center font-medium transition-[grid-template-rows] duration-150 overflow-hidden mt-2"
-     :class="[!trimmedMessage ? 'grid-rows-[0fr] delay-150' : 'grid-rows-[1fr] delay-0' ]">
-  <div class="text-sm text-red-500 overflow-hidden transition-opacity duration-150"
-       :class="[!trimmedMessage ? 'opacity-0 delay-0' : 'opacity-100 delay-150']">
-    {{ error }}
+  <div class="grid items-center font-medium transition-[grid-template-rows] duration-150 mt-2"
+       :class="[!trimmedMessage ? 'grid-rows-[0fr] delay-150' : 'grid-rows-[1fr] delay-0' ]">
+    <div class="text-sm text-warning transition-opacity duration-150"
+         :class="[!trimmedMessage ? 'opacity-0 delay-0' : 'opacity-100 delay-150']">
+      {{ error }}
+    </div>
   </div>
-</div>
 </template>
