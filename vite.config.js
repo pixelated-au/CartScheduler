7cee3b53a58c0e8df64a32e1f8ea4e1f08e376ba
Ian Heggaton <ian@pixelated.com.au>
2025-07-07 06:14:09 +1000

    Configure path alias for resources
    - Define alias '@' to resolve to the resources/js directory.

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,30 @@
-import vue from '@vitejs/plugin-vue'
-import laravel from 'laravel-vite-plugin'
-import {defineConfig} from 'vite'
+import { resolve } from "node:path";
+import vue from "@vitejs/plugin-vue";
+import laravel from "laravel-vite-plugin";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-    server: {
-        host: '0.0.0.0',
+  plugins: [
+    laravel({
+      input: "resources/js/app.js",
+      refresh: true,
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
+        },
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./resources/js"),
     },
-    envDir: './',
-})
+  },
+  server: {
+    host: "0.0.0.0",
+  },
+  envDir: "./",
+});
