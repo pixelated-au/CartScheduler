7d91a03a9477f88b0196e748cb0177e1d47e886b
Ian Heggaton <ian@pixelated.com.au>
2025-07-11 14:57:26 +1000

    Refactor User Import page
    - Improve error message display with consistent styling
    - Update layout and styling for better user experience
    - Enhance file input element for improved visual feedback

--- resources/js/Pages/Admin/Users/Import.vue
+++ resources/js/Pages/Admin/Users/Import.vue
@@ -28,11 +28,10 @@ const validationErrors = computed(() => {
 
   const errors = [];
   for (const errorsKey in form.errors) {
-    if (Object.prototype.hasOwnProperty.call(form.errors, errorsKey)) {
-      const error = form.errors[errorsKey];
-      errors.push(error);
-    }
+    const error = form.errors[errorsKey as keyof typeof form.errors];
+    errors.push(error);
   }
+
   return errors;
 });
 
@@ -50,94 +49,81 @@ const templateFile = route("admin.user-import-template");
   <AppLayout title="Import Users">
     <template #header>
       <div class="flex justify-between">
-        <h1 class="font-semibold text-2xl text-gray-800 leading-tight">Import Users</h1>
+        <h2>Import Users</h2>
         <PButton class="mx-3" type="button" style-type="secondary" outline @click.prevent="listRouteAction">
           Back
         </PButton>
       </div>
     </template>
 
-    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-      <div v-if="validationErrors.length" class="bg-red-200 p-3 mb-6 rounded-lg h-36 overflow-auto shadow-lg">
-        <h3 class="mb-3">
-          There are {{ validationErrors.length }} problems with the spreadsheet you uploaded 🤭
-        </h3>
-        <ul class="pl-3">
+    <div class="pt-10 mx-auto max-w-7xl sm:px-6">
+      <div v-if="validationErrors.length"
+           class="overflow-auto px-5 py-3 mb-6 text-white rounded bg-warning dark:bg-warning-light min-h-10">
+        <h4>
+          Oops! it seems you have
+          {{ validationErrors.length === 1 ? 'a problem' : 'some problems' }}
+          with the spreadsheet you uploaded:
+        </h4>
+        <ul class="formatted-list">
           <li v-for="error in validationErrors" :key="error">
             {{ error }}
           </li>
         </ul>
       </div>
 
-      <div class="flex items-center justify-start px-4 py-3 bg-gray-50 dark:bg-gray-900 shadow rounded-lg">
-        <form class="w-full" @submit.prevent="uploadFile">
-          <div class="w-full">
-            <div class="text-gray-900 dark:text-gray-300 mb-5">
+      <div class="flex flex-col px-4 py-3 rounded bg-sub-panel dark:bg-sub-panel-dark std-border">
+        <form class="flex flex-col w-full" @submit.prevent="uploadFile">
+          <div class="flex flex-col gap-3 w-full">
+            <div class="flex flex-col gap-3">
               <h3>User Import</h3>
-              <div
-                  class="my-5 p-10 border border-gray-300 dark:border-gray-700 bg-gray-100 dark:bg-gray-800 rounded">
-                <ol class="ml-3 list-decimal list-outside">
-                  <li class="mb-4">
-                    Download and open the <a :href="templateFile">
-                      Template Excel
-                      file
-                    </a>
-                  </li>
-                  <li class="mb-4">
-                    <strong>Closely</strong> follow the instructions that appear at the
-                    top of the Excel file.
-                    <ul class="pl-4 list-disc list-outside">
-                      <li class="mb-2">
-                        You can copy and past data from another source into the
-                        excel file but please make sure the data conforms to the instructions.
+              <div class="px-3 py-6 rounded border std-border">
+                <ol class="formatted-list">
+                  <li>Download and open the <a :href="templateFile">Template Excel file</a></li>
+                  <li>
+                    <strong>Closely</strong> follow the instructions that appear at the top of the Excel file.
+                    <ul>
+                      <li>
+                        You can copy and past data from another source into the excel file but please make sure the data
+                        conforms to the instructions.
                       </li>
-                      <li class="mb-2">
-                        If the importer finds an email address that already belongs
-                        to a user in the system, it will update the data for that user.
-                        Otherwise, it will create a new user
+                      <li>
+                        If the importer finds an email address that already belongs to a user in the system, it will
+                        update the data for that user. Otherwise, it will create a new user.
                       </li>
-                      <li class="mb-2">
-                        Note, if you wish to update a users' email address, you'll
-                        need to do that manually by editing that user on this site.
+                      <li>
+                        Note, if you wish to update a users' email address, you'll need to do that manually by editing
+                        that user on this site.
                       </li>
                     </ul>
                   </li>
-                  <li class="mb-4">
-                    Upload the file. If there are any issues, you will be notified on
-                    what the issues are and what needs fixing.
-                    <ul class="pl-4 list-disc list-outside">
-                      <li class="mb-2">
-                        If there are issues, follow the instructions, fix them and
-                        re-upload.
-                      </li>
-                      <li class="mb-2">
-                        Note, you may need to reload this page in order for the
-                        upload to work again
-                      </li>
+                  <li>
+                    Upload the file. If there are any issues, you will be notified on what the issues are and what needs
+                    fixing.
+                    <ul>
+                      <li>If there are issues, follow the instructions, fix them and re-upload.</li>
+                      <li>Note, you may need to reload this page in order for the upload to work again.</li>
                     </ul>
                   </li>
                 </ol>
               </div>
             </div>
-            <div class="flex items-center">
+
+            <div class="flex flex-col">
               <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300" for="file-input">
                 Upload spreadsheet.<br>
               </label>
-            </div>
 
-            <input class="block w-full text-sm italic rounded-md border cursor-pointer bg-panel dark:bg-panel-dark
-                        p-1 focus:outline-none std-border hover:bg-gray-200 ease-in-out transition-colors file:text-sm
-                        file:py-2 file:px-3 file:rounded-md file:border-0 file:bg-gray-200 file:text-gray-700
-                      file:hover:ring-primary file:hover:ring-1 file:mr-3 file:cursor-pointer  file:hover:scale-[103%]
+              <input class="block w-full text-sm italic rounded border cursor-pointer bg-text-input dark:bg-text-input-dark
+                        p-1 std-border hover:bg-gray-200 ease-in-out transition-colors file:text-sm
+                        file:py-2 file:px-3 file:rounded file:border-0 file:bg-neutral-300 file:text-neutral-700 dark:file:bg-neutral-500 dark:file:hover:bg-neutral-600 dark:file:text-gray-200
+                      file:hover:ring-primary dark:file:hover:ring-primary-light file:hover:ring-1 file:mr-3 file:cursor-pointer  file:hover:scale-[103%]
                       file:hover:bg-gray-100 file:ease-in-out file:transition-[background-color,transform,box-shadow]"
-                   aria-describedby="file-input-help"
-                   id="file-input"
-                   type="file"
-                   accept="application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, text/csv"
-                   @input="handleFileChange">
-          </div>
-          <div class="w-full flex justify-between mt-3">
-            <div>
+                     aria-describedby="file-input-help"
+                     id="file-input"
+                     type="file"
+                     accept="application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, text/csv"
+                     @input="handleFileChange">
+
               <div class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file-input-help">
                 XLSX, XLS or CSV files only
               </div>
@@ -146,13 +132,15 @@ const templateFile = route("admin.user-import-template");
                 <a :href="templateFile">Template Excel file</a>
               </div>
             </div>
+          </div>
+          <div class="flex justify-end mt-3 w-full">
             <SubmitButton type="submit"
-                          :label="form.file ? 'Upload and Import' : 'Select a file to upload'"
+                          :label="form.file ? 'Upload and Import' : 'Select a file above to upload'"
                           :disabled="!form.file"
                           :processing="form.processing"
                           :success="form.wasSuccessful"
                           :failure="form.hasErrors"
-                          :errors="form.errors"/>
+                          :errors="form.errors" />
           </div>
           <div class="w-full">
             <progress v-if="form.progress" :value="form.progress.percentage" max="100">
