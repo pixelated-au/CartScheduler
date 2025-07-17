b792784502256318b879b48208dba2c6157303f5
Ian Heggaton <ian@pixelated.com.au>
2025-07-18 09:08:14 +1000

    Refactor VacationDateRange component layout
    - Update VacationDateRange to use a grid layout.
    - Replace plain labels with JetLabel components.
    - Add TypeScript support.

--- resources/js/Pages/Profile/Partials/VacationDateRange.vue
+++ resources/js/Pages/Profile/Partials/VacationDateRange.vue
@@ -1,33 +1,22 @@
-<script setup>
-import { parseISO, addDays } from "date-fns";
-import formatISO from "date-fns/formatISO";
+<script setup lang="ts">
+import { parseISO, addDays, formatISO  } from "date-fns";
 import { computed, watch, ref } from "vue";
 import JetInputError from "@/Jetstream/InputError.vue";
+import JetLabel from "@/Jetstream/Label.vue";
 
-const props = defineProps({
-  startDate: {
-    type: String,
-  },
-  endDate: {
-    type: String,
-  },
-  startError: {
-    type: String,
-    required: false,
-  },
-  endError: {
-    type: String,
-    required: false,
-  },
-});
+const props = defineProps<{
+  startDate?: string;
+  endDate?: string;
+  startError?: string;
+  endError?: string;
+}>();
 
 const emit = defineEmits([
   "update:startDate",
   "update:endDate",
 ]);
 
-const formatForIso = (date) => formatISO(date, { representation: "date" });
-
+const formatForIso = (date: Date) => formatISO(date, { representation: "date" });
 const fieldFormat = "D d M yy";
 
 const start = ref();
@@ -67,26 +56,23 @@ const maxDate = computed(() => end.value
 </script>
 
 <template>
-  <div class="grid grid-cols-[auto_minmax(0,_1fr)] gap-2 items-center sm:flex sm:space-x-4">
-    <label class="contents">
-      <span class="font-bold flex-grow-0">From:</span>
+  <div class="grid grid-cols-2 gap-4 grid-rows-1 items-center">
+    <div class="grid grid-cols-1 gap-x-2 gap-y-1">
+      <JetLabel class="font-bold" :has-error="!!startError" value="From"/>
 
-      <PDatePicker
-          v-model="start"
-          :minDate="new Date()"
-          :maxDate
-          :dateFormat="fieldFormat" />
+      <PDatePicker v-model="start"
+                   :minDate="new Date()"
+                   :maxDate
+                   :dateFormat="fieldFormat" />
       <JetInputError :message="startError" />
-    </label>
+    </div>
+    <div class="grid grid-cols-1 gap-2">
+      <JetLabel class="font-bold" :has-error="!!endError" value="To"/>
 
-    <label class="contents">
-      <span class="font-bold flex-grow-0">To:</span>
-
-      <PDatePicker
-          v-model="end"
-          :minDate
-          :dateFormat="fieldFormat" />
+      <PDatePicker v-model="end"
+                   :minDate
+                   :dateFormat="fieldFormat" />
       <JetInputError :message="endError" />
-    </label>
+    </div>
   </div>
 </template>
