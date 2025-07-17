18929d31125af8bad7ec1cbc25ff2afce4dbfb38
Ian Heggaton <ian@pixelated.com.au>
2025-07-18 09:03:46 +1000

    Refactor Jetstream FormSection component
    - Updated component to use TypeScript.

--- resources/js/Jetstream/FormSection.vue
+++ resources/js/Jetstream/FormSection.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import { computed, useSlots } from "vue";
 import JetSectionTitle from "./SectionTitle.vue";
 
@@ -8,30 +8,30 @@ const hasActions = computed(() => !!useSlots().actions);
 </script>
 
 <template>
-<div class="md:grid md:grid-cols-3 md:gap-4">
-  <JetSectionTitle>
-    <template #title>
-      <slot name="title"/>
-    </template>
+  <div class="md:grid md:grid-cols-3 md:gap-4">
+    <JetSectionTitle>
+      <template #title>
+        <slot name="title" />
+      </template>
 
-    <template #description>
-      <slot name="description"/>
-    </template>
-  </JetSectionTitle>
+      <template #description>
+        <slot name="description" />
+      </template>
+    </JetSectionTitle>
 
-  <div class="mt-5 md:mt-0 md:col-span-2">
-    <form @submit.prevent="$emit('submitted')">
-      <div class="px-4 py-5 bg-panel dark:bg-panel-dark sm:p-6 sm:rounded-bl-md sm:rounded-br-md"
-           :class="hasActions ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md'">
-        <div class="grid grid-cols-6 gap-6">
-          <slot name="form"/>
+    <div class="mt-5 md:mt-0 md:col-span-2">
+      <form @submit.prevent="$emit('submitted')">
+        <div class="px-4 py-5 bg-panel dark:bg-panel-dark sm:p-6 sm:rounded-bl-md sm:rounded-br-md"
+             :class="hasActions ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md'">
+          <div class="grid grid-cols-6 gap-6">
+            <slot name="form" />
+          </div>
         </div>
-      </div>
 
-      <div v-if="hasActions" class="flex items-center justify-end px-4 py-3 text-right sm:px-6 gap-8">
-        <slot name="actions"/>
-      </div>
-    </form>
+        <div v-if="hasActions" class="flex items-center justify-end px-4 py-3 text-right sm:px-6 gap-8">
+          <slot name="actions" />
+        </div>
+      </form>
+    </div>
   </div>
-</div>
 </template>
