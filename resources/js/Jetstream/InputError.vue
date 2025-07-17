ab7ff9c5c9f1004750d6a655289fee3162bcd93c
Ian Heggaton <ian@pixelated.com.au>
2025-07-18 09:08:00 +1000

    Refactor InputError component to use Typescript
    - Update InputError component to utilize Typescript for improved type safety and maintainability.
    - Remove unnecessary margin top from the parent div in the template.

--- resources/js/Jetstream/InputError.vue
+++ resources/js/Jetstream/InputError.vue
@@ -1,21 +1,18 @@
-<script setup>
+<script setup lang="ts">
 import { promiseTimeout } from "@vueuse/core";
 import { computed, watch, ref } from "vue";
 
-const props = defineProps({
-  message: {
-    type: String,
-    default: undefined,
-  },
-});
+const { message } = defineProps<{
+  message?: string;
+}>();
 
-const trimmedMessage = computed(() => (typeof props.message === "string") ? props.message.trim() : undefined);
+const trimmedMessage = computed(() => message?.trim() || undefined);
 
-const error = ref(undefined);
+const error = ref();
 
-watch(trimmedMessage, async (message) => {
-  if (message) {
-    error.value = message;
+watch(trimmedMessage, async (msg) => {
+  if (msg) {
+    error.value = msg;
     return;
   }
   await promiseTimeout(1000);
@@ -24,9 +21,9 @@ watch(trimmedMessage, async (message) => {
 </script>
 
 <template>
-  <div class="grid items-center font-medium transition-[grid-template-rows] duration-150 mt-2"
+  <div class="grid items-center font-medium transition-[grid-template-rows] duration-150"
        :class="[!trimmedMessage ? 'grid-rows-[0fr] delay-150' : 'grid-rows-[1fr] delay-0' ]">
-    <div class="text-sm text-warning transition-opacity duration-150"
+    <div class="text-sm transition-opacity duration-150 text-warning"
          :class="[!trimmedMessage ? 'opacity-0 delay-0' : 'opacity-100 delay-150']">
       {{ error }}
     </div>
