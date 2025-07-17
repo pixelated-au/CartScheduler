e692d0803e0e0919488f2ea37c91cb6a3d611592
Ian Heggaton <ian@pixelated.com.au>
2025-07-18 09:04:49 +1000

    Refactor ActionMessage component to use TypeScript
    - Update ActionMessage to utilize TypeScript for props definition.

--- resources/js/Jetstream/ActionMessage.vue
+++ resources/js/Jetstream/ActionMessage.vue
@@ -1,17 +1,15 @@
-<script setup>
-defineProps({
-    on: Boolean,
-});
+<script setup lang="ts">
+const { on = false } = defineProps<{
+  on?: boolean;
+}>();
 </script>
 
 <template>
-    <div>
-        <transition leave-active-class="transition ease-in delay-9000 duration-1000"
-                    leave-from-class="opacity-100"
-                    leave-to-class="opacity-0">
-            <div v-show="on" class="font-bold text-green-700">
-                <slot/>
-            </div>
-        </transition>
+  <transition leave-active-class="transition ease-in delay-9000 duration-1000"
+              leave-from-class="opacity-100"
+              leave-to-class="opacity-0">
+    <div v-show="on" class="font-bold text-green-700">
+      <slot />
     </div>
+  </transition>
 </template>
