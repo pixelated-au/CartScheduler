33a132e72dccdd5b2580320f16d83739029e004a
Ian Heggaton <ian@pixelated.com.au>
2025-08-11 09:28:47 +1000

    Update InputTextEIPField component to use PInputText
    - Replace JetInput with PInputText in InputTextEIPField.vue

--- resources/js/Components/InputTextEIPField.vue
+++ resources/js/Components/InputTextEIPField.vue
@@ -1,47 +1,41 @@
-<script setup>
+<script setup lang="ts">
 import { computed, ref } from "vue";
-import JetInput from "@/Jetstream/Input.vue";
 
-const props = defineProps({
-  modelValue: [Number, String],
-  inputClass: String,
-  emptyValue: {
-    type: String,
-    default: "",
-  },
-});
+const { modelValue, inputClass, maxlength, emptyValue="" } = defineProps<{
+  modelValue: App.Data.UserVacationData["description"];
+  inputClass: string;
+  maxlength?: number;
+  emptyValue: string;
+}>();
 
 const myModel = computed({
-  get: () => props.modelValue,
+  get: () => modelValue,
   set: (value) => emit("update:modelValue", value),
 });
 
 const emit = defineEmits(["update:modelValue"]);
 
-const inputDescription = ref(null);
 const doEditDescription = ref(false);
 const hideDescription = () => doEditDescription.value = false;
 </script>
 
 <template>
-  <div
-      v-if="!doEditDescription"
-      class="cursor-pointer"
-      @click="doEditDescription = true">
+  <div v-if="!doEditDescription"
+       class="cursor-pointer"
+       @click="doEditDescription = true">
     <span
         class="underline decoration-dashed ml-1">
       {{ modelValue?.trim() || emptyValue }}
     </span>
   </div>
-  <JetInput
-      v-else
-      v-model="myModel"
-      autofocus
-      class="px-2 py-1"
-      :class="inputClass"
-      :ref="inputDescription"
-      @blur="hideDescription"
-      @keydown.esc="hideDescription"/>
+  <PInputText v-else
+              v-model="myModel"
+              autofocus
+              maxlength="maxlength"
+              class="px-2 py-1"
+              :class="inputClass"
+              @blur="hideDescription"
+              @keydown.esc="hideDescription"/>
 </template>
 
 <style scoped>
