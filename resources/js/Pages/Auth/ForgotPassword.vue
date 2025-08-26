617853c741684439cc636f3870ed582c6d1e21d1
Ian Heggaton <ian@pixelated.com.au>
2025-08-27 09:14:39 +1000

    Update Auth pages to use PrimeVue components and add processing indicators
    - Update ForgotPassword.vue to use PrimeVue input and button components.
    - Update ResetPassword.vue to use PrimeVue input and button components.

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -1,22 +1,23 @@
-<script setup>
+<script setup lang="ts">
 import { useForm } from "@inertiajs/vue3";
-import { inject } from "vue";
 import JetAuthenticationCardLogo from "@/Jetstream/AuthenticationCardLogo.vue";
+import JetLabel from "@/Jetstream/Label.vue";
 import JetValidationErrors from "@/Jetstream/ValidationErrors.vue";
 import AuthLayout from "@/Layouts/AuthLayout.vue";
 
-defineProps({
-  status: String,
-});
+defineProps<{
+  status: string;
+}>();
 
 const form = useForm({
   email: "",
 });
 
-const route = inject("route");
-
-const submit = () => {
-  form.post(route("password.email"));
+const submit = (setProcessing: (value: boolean) => void) => {
+  form.post(route("password.email"), {
+    onBefore: () => setProcessing(true),
+    onFinish: () => setProcessing(false),
+  });
 };
 </script>
 
@@ -26,48 +27,50 @@ const submit = () => {
       <JetAuthenticationCardLogo />
     </template>
 
-    <div class="flex flex-col">
-      <div class="mb-4 text-sm text-gray-600 dark:text-gray-200">
-        Forgot your password? No problem. Just let us know your email address and we will email you a password reset
-        link that will allow you to choose a new one.
-      </div>
+    <template #default="{ setProcessing }">
+      <div class="flex flex-col">
+        <div class="mb-4 text-sm text-gray-600 dark:text-gray-200">
+          Forgot your password? No problem. Just let us know your email address and we will email you a password reset
+          link that will allow you to choose a new one.
+        </div>
 
-      <div v-if="status" class="mb-4 text-sm font-medium text-green-600">
-        {{ status }}
-      </div>
+        <div v-if="status" class="mb-4 text-sm font-medium text-green-600">
+          {{ status }}
+        </div>
 
-      <JetValidationErrors class="mb-4" />
+        <JetValidationErrors class="mb-4" />
 
-      <form @submit.prevent="submit">
-        <div class="flex flex-col gap-3 [&>div]:flex [&>div]:flex-col [&>div]:gap-1">
+        <form @submit.prevent="submit(setProcessing)">
+          <div class="flex flex-col gap-3 [&>div]:flex [&>div]:flex-col [&>div]:gap-1">
+            <div>
+              <JetLabel for="email" value="Email" />
+              <PInputText id="email"
+                          v-model="form.email"
+                          type="email"
+                          class="block mt-1 w-full"
+                          autocomplete="username"
+                          required
+                          autofocus />
+            </div>
+          </div>
+
           <div>
-            <FormLabel for="email" value="Email" />
-            <PInputText id="email"
-                        v-model="form.email"
-                        type="email"
-                        class="block mt-1 w-full"
-                        autocomplete="username"
-                        required
-                        autofocus />
+            <div class="flex gap-4 justify-end items-center mt-4">
+              <Link :href="route('login')"
+                    class="text-gray-600 underline hover:text-gray-900 dark:text-gray-200 dark:hover-text-gray-500">
+                Return to login
+              </Link>
+              <SubmitButton label="Email Password Reset Link"
+                            icon="iconify mdi--email"
+                            :processing="form.processing"
+                            :success="form.recentlySuccessful"
+                            :failure="form.hasErrors"
+                            :errors="form.errors"
+                            type="submit" />
+            </div>
           </div>
-        </div>
-
-        <div>
-          <div class="flex gap-4 justify-end items-center mt-4">
-            <Link :href="route('login')"
-                  class="text-gray-600 underline hover:text-gray-900 dark:text-gray-200 dark:hover-text-gray-500">
-              Return to login
-            </Link>
-            <SubmitButton label="Email Password Reset Link"
-                          icon="iconify mdi--email"
-                          :processing="form.processing"
-                          :success="form.recentlySuccessful"
-                          :failure="form.hasErrors"
-                          :errors="form.errors"
-                          type="submit" />
-          </div>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+    </template>
   </AuthLayout>
 </template>
--- resources/js/Pages/Auth/ResetPassword.vue
+++ resources/js/Pages/Auth/ResetPassword.vue
@@ -1,74 +1,84 @@
-<script setup>
+<script setup lang="ts">
 import { useForm } from "@inertiajs/vue3";
-import AuthLayout from "@/Layouts/AuthLayout.vue";
 import JetAuthenticationCardLogo from "@/Jetstream/AuthenticationCardLogo.vue";
-import JetInput from "@/Jetstream/Input.vue";
 import JetLabel from "@/Jetstream/Label.vue";
 import JetValidationErrors from "@/Jetstream/ValidationErrors.vue";
+import AuthLayout from "@/Layouts/AuthLayout.vue";
 
-const props = defineProps({
-    email: String,
-    token: String,
-});
+const props = defineProps<{
+  email: string;
+  token: string;
+}>();
 
 const form = useForm({
-    token: props.token,
-    email: props.email,
-    password: "",
-    password_confirmation: "",
+  token: props.token,
+  email: props.email,
+  password: "",
+  password_confirmation: "",
 });
 
-const submit = () => {
-    form.post(route("password.update"), {
-        onFinish: () => form.reset("password", "password_confirmation"),
-    });
+const submit = (setProcessing: (value: boolean) => void) => {
+  form.post(route("password.update"), {
+    onBefore: () => setProcessing(true),
+    onFinish: () => {
+      setProcessing(false);
+      form.reset("password", "password_confirmation");
+    },
+  });
 };
 </script>
 
 <template>
-<AuthLayout title="Reset Password">
-  <template #logo>
-    <JetAuthenticationCardLogo />
-  </template>
+  <AuthLayout title="Reset Password">
+    <template #logo>
+      <JetAuthenticationCardLogo />
+    </template>
 
-  <JetValidationErrors class="mb-4" />
+    <template #default="{ setProcessing }">
+      <JetValidationErrors class="mb-4" />
 
-  <form @submit.prevent="submit">
-    <div>
-      <JetLabel for="email" value="Email" />
-      <JetInput id="email"
-                v-model="form.email"
-                type="email"
-                class="mt-1 block w-full"
-                required
-                autofocus />
-    </div>
+      <form @submit.prevent="submit(setProcessing)">
+        <div>
+          <JetLabel for="email" value="Email" />
+          <PInputText id="email"
+                      v-model="form.email"
+                      type="email"
+                      class="block mt-1 w-full"
+                      autocomplete="username"
+                      required
+                      autofocus />
+        </div>
 
-    <div class="mt-4">
-      <JetLabel for="password" value="Password" />
-      <JetInput id="password"
-                v-model="form.password"
-                type="password"
-                class="mt-1 block w-full"
-                required
-                autocomplete="new-password" />
-    </div>
+        <div class="mt-4">
+          <JetLabel for="password" value="Password" />
+          <PPassword id="password"
+                     v-model="form.password"
+                     type="password"
+                     class="block mt-1 w-full"
+                     required
+                     :inputProps="{ autocomplete: 'new-password' }"/>
+        </div>
 
-    <div class="mt-4">
-      <JetLabel for="password_confirmation" value="Confirm Password" />
-      <JetInput id="password_confirmation"
-                v-model="form.password_confirmation"
-                type="password"
-                class="mt-1 block w-full"
-                required
-                autocomplete="new-password" />
-    </div>
+        <div class="mt-4">
+          <JetLabel for="password_confirmation" value="Confirm Password" />
+          <PPassword id="password_confirmation"
+                     v-model="form.password_confirmation"
+                     type="password"
+                     class="block mt-1 w-full"
+                     required
+                     :inputProps="{ autocomplete: 'new-password' }"/>
+        </div>
 
-    <div class="flex items-center justify-end mt-4">
-      <PButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-        Reset Password
-      </PButton>
-    </div>
-  </form>
-</AuthLayout>
+        <div class="flex items-center justify-end mt-4">
+          <SubmitButton label="Reset Password"
+                        icon="iconify mdi--form-textbox-password"
+                        :processing="form.processing"
+                        :success="form.recentlySuccessful"
+                        :failure="form.hasErrors"
+                        :errors="form.errors"
+                        type="submit" />
+        </div>
+      </form>
+    </template>
+  </AuthLayout>
 </template>
