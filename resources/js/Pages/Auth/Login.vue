7208993f4d867cd145d5b0c215b17b24e2cb482a
Ian Heggaton <ian@pixelated.com.au>
2025-08-27 09:14:53 +1000

    Refactor Login.vue for improved error handling and submission
    - Replaced Inertia form with reactive form for more control
    - Added axios for form submission
    - Implemented error handling using toast notifications
    - Removed JetValidationErrors and used a custom error display
    - Used a slot in AuthLayout.vue to pass processing state

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { useForm, usePage, Link  } from "@inertiajs/vue3";
-import { computed, inject, onMounted } from "vue";
+import { Link, usePage } from "@inertiajs/vue3";
+import axios, { isAxiosError } from "axios";
+import { computed, onMounted, reactive, ref } from "vue";
 import Alert from "@/Components/Alert.vue";
+import useToast from "@/Composables/useToast";
 import JetAuthenticationCardLogo from "@/Jetstream/AuthenticationCardLogo.vue";
 import JetLabel from "@/Jetstream/Label.vue";
-import JetValidationErrors from "@/Jetstream/ValidationErrors.vue";
 import AuthLayout from "@/Layouts/AuthLayout.vue";
 
 interface PageProps {
@@ -13,34 +14,49 @@ interface PageProps {
       setPassword?: string;
     };
   };
+
   [key: string]: unknown;
 }
 
 const props = defineProps<{
   canResetPassword: boolean;
-  status?: string;
+  status?: string; // This is used by Laravel Fortify
 }>();
 
-const form = useForm({
+const form = reactive({
   email: "",
   password: "",
   remember: true,
 });
 
-const route = inject("route");
+const hasErrors = ref(false);
 
-const submit = (): void => {
-  if (!route) return;
+const toast = useToast();
 
-  form.transform((data) => ({
-    ...data,
-    remember: form.remember ? "on" : "",
-  })).post(route("login"), {
-    onFinish: () => form.reset("password" as never),
-  });
+const submit = async (setProcessing: (value: boolean) => void) => {
+  setProcessing(true);
+  hasErrors.value = false;
+  try {
+    await axios.post(route("login"), form);
+    window.location.href = "/";
+  } catch (e) {
+    console.error(e);
+    if (!isAxiosError(e)) {
+      throw e;
+    }
+    if (e.status === 422) {
+      hasErrors.value = true;
+      toast.error(e.response?.data.message);
+    } else {
+      toast.error("An unexpected error occurred. This has been reported");
+    }
+  } finally {
+    setProcessing(false);
+  }
 };
 
 const setPasswordSuccess = computed<string>(() => {
+  // When the user has successfully set their password, they will be directed to the login with a message:
   return usePage<PageProps>().props.jetstream.flash?.setPassword || "";
 });
 
@@ -49,7 +65,7 @@ if (import.meta.env.DEV) {
     form.email = "admin@example.com";
     form.password = "password";
   });
-};
+}
 </script>
 
 <template>
@@ -58,55 +74,62 @@ if (import.meta.env.DEV) {
       <JetAuthenticationCardLogo />
     </template>
 
-    <JetValidationErrors class="mb-4" />
-
-    <div v-if="status" class="mb-4 text-sm font-medium text-green-600">
-      {{ status }}
-    </div>
-
-    <Alert v-if="setPasswordSuccess">
-      {{ setPasswordSuccess }}
-    </Alert>
-
-    <form @submit.prevent="submit">
-      <div class="flex flex-col gap-3 [&>div]:flex [&>div]:flex-col [&>div]:gap-1">
-        <div>
-          <JetLabel for="email" value="Email" />
-          <PInputText id="email"
-                      v-model="form.email"
-                      type="email"
-                      class="block mt-1 w-full"
-                      required
-                      autocomplete="username"
-                      autofocus />
-        </div>
-
-        <div>
-          <JetLabel for="password" value="Password" />
-          <PPassword input-id="password"
-                     v-model="form.password"
-                     :feedback="false"
-                     toggle-mask
-                     input-class="w-full"
-                     required
-                     autocomplete="current-password" />
-        </div>
-
-        <div class="mt-4 flex w-full !flex-row items-center">
-          <PCheckbox binary input-id="remember" v-model="form.remember" name="remember" />
-          <JetLabel for="remember">Remember me</JetLabel>
+    <template #default="{ processing, setProcessing }">
+      <div v-if="hasErrors">
+        <div class="font-medium my-3 text-center text-red-600">
+          Whoops! Please check your details and try again.
         </div>
       </div>
 
-      <div class="flex gap-4 justify-end items-center mt-4">
-        <Link v-if="props.canResetPassword"
-              :href="route('password.request')"
-              class="text-gray-600 underline hover:text-gray-900 dark:text-gray-200 dark:hover-text-gray-500">
-          Forgot your password?
-        </Link>
+      <Alert v-if="setPasswordSuccess || status">
+        {{ setPasswordSuccess || status }}
+      </Alert>
 
-        <PButton label="Log in" icon="iconify mdi--login" icon-pos="right" :class="{ 'opacity-25': form.processing }" :disabled="form.processing" type="submit" />
-      </div>
-    </form>
+      <form @submit.prevent="submit(setProcessing)">
+        <div class="flex flex-col gap-3 [&>div]:flex [&>div]:flex-col [&>div]:gap-1">
+          <div>
+            <JetLabel for="email" value="Email" />
+            <PInputText id="email"
+                        v-model="form.email"
+                        type="email"
+                        class="block mt-1 w-full"
+                        required
+                        autocomplete="username"
+                        autofocus />
+          </div>
+
+          <div>
+            <JetLabel for="password" value="Password" />
+            <PPassword input-id="password"
+                       v-model="form.password"
+                       :feedback="false"
+                       toggle-mask
+                       input-class="w-full"
+                       required
+                       :inputProps="{ autocomplete: 'current-password' }"/>
+          </div>
+
+          <div class="mt-4 flex w-full !flex-row items-center">
+            <PCheckbox binary input-id="remember" v-model="form.remember" name="remember" />
+            <JetLabel for="remember">Remember me</JetLabel>
+          </div>
+        </div>
+
+        <div class="flex gap-4 justify-end items-center mt-4">
+          <Link v-if="props.canResetPassword"
+                :href="route('password.request')"
+                class="text-gray-600 underline hover:text-gray-900 dark:text-gray-200 dark:hover-text-gray-500">
+            Forgot your password?
+          </Link>
+
+          <PButton label="Log in"
+                   icon="iconify mdi--login"
+                   icon-pos="right"
+                   :class="{ 'opacity-25': processing }"
+                   :disabled="processing"
+                   type="submit" />
+        </div>
+      </form>
+    </template>
   </AuthLayout>
 </template>
