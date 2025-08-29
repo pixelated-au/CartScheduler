d997a8b1901cdce753671346d190d4bcf3836ada
Ian Heggaton <ian@pixelated.com.au>
2025-08-30 07:19:38 +1000

    Update shims.d.ts to refine type definitions for Inertia props
    - Refine `InertiaProps` type to include Jetstream properties.
    - Introduce `AppPageProps` type for more specific page properties.
    - Update module declarations to align with the new types.

--- resources/js/shims.d.ts
+++ resources/js/shims.d.ts
@@ -2,11 +2,11 @@
 // noinspection JSUnusedGlobalSymbols
 
 import "vite/client";
-import type { Page, PageProps } from "@inertiajs/core";
+import type { Page, PageProps as InertiaPageProps } from "@inertiajs/core";
 import type { Axios } from "axios";
 import type {
-  ComponentCustomOptions as _ComponentCustomOptions,
-  ComponentCustomProperties as _ComponentCustomProperties,
+  ComponentCustomOptions as VueComponentCustomOptions,
+  ComponentCustomProperties as VueComponentCustomProperties,
   DefineComponent,
 } from "vue";
 import type { ToastSeverity } from "@/Composables/useToast";
@@ -24,19 +24,62 @@ export type AuthUser = {
   gender: string;
 };
 
-declare module "@vue/runtime-core" {
-  interface ComponentCustomProperties {
-    // For some reason, Ziggy needs to be in @vue/runtime-core
-    route: typeof routeFn;
-  }
+export type InertiaProps = {
+  pagePermissions: {
+    canAdmin?: true;
+    canEditSettings?: true;
+  };
+  shiftAvailability: {
+    timezone: string;
+    duration: number;
+    period: App.Enums.DBPeriod;
+    releasedDaily: boolean;
+    weekDayRelease: "SUN" | "MON" | "TUE" | "WED" | "THU" | "FRI" | "SAT";
+    systemShiftStartHour: number;
+    systemShiftEndHour: number;
+  };
+  hasUpdate?: boolean;
+  enableUserAvailability?: boolean;
+  needsToUpdateAvailability?: boolean;
+  enableUserLocationChoices?: boolean;
+  isUnrestricted?: true;
+  user: AuthUser;
+};
+
+interface Flash {
+  title?: string;
+  message?: string | undefined;
+  position?: "top" | "bottom" | "center";
+  /* @deprecated - Use message instead */
+  banner?: string;
+  /* @deprecated - Only use for 'success' messages */
+  bannerStyle?: ToastSeverity extends string ? ToastSeverity : undefined;
 }
+
+interface Jetstream {
+  jetstream: {
+    flash: Flash;
+    canCreateTeams: boolean;
+    canUpdateProfileInformation: boolean;
+    canUpdatePassword: boolean;
+    canManageTwoFactorAuthentication: boolean;
+    hasAccountDeletionFeatures: boolean;
+    hasApiFeatures: boolean;
+    hasTeamFeatures: boolean;
+    managesProfilePhotos: boolean;
+    hasEmailVerification: boolean;
+  };
+}
+
+export type AppPageProps<
+  T extends Record<string, unknown> | unknown[] = Record<string, unknown> | unknown[],
+> = InertiaProps & Jetstream & FlashMessage & T;
 
 declare module "vue" {
   const component: DefineComponent;
 
   interface ComponentCustomProperties {
     $inertia: typeof Router;
-    $page: Page;
     $headManager: ReturnType<typeof createHeadManager>;
   }
 
@@ -46,51 +89,23 @@ declare module "vue" {
 }
 
 declare module "@vue/runtime-core" {
-  interface ComponentCustomProperties extends _ComponentCustomProperties {
+  interface ComponentCustomProperties {
+    // For some reason, Ziggy needs to be in @vue/runtime-core
+    route: typeof routeFn;
+    $page: Page<AppPageProps>;
   }
 
-  interface ComponentCustomOptions extends _ComponentCustomOptions {
+  interface ComponentCustomProperties extends VueComponentCustomProperties {
+  }
+
+  interface ComponentCustomOptions extends VueComponentCustomOptions {
   }
 }
 
+declare module "@inertiajs/core" {
+  interface PageProps extends InertiaPageProps, AppPageProps {}
+}
+
 declare module "@inertiajs/vue3" {
-  interface Flash {
-    title?: string;
-    message?: string | undefined;
-    position?: "top" | "bottom" | "center";
-    /* @deprecated - Use message instead */
-    banner?: string;
-    /* @deprecated - Only use for 'success' messages */
-    bannerStyle?: ToastSeverity extends string ? ToastSeverity : undefined;
-  }
-
-  type FlashMessage = {
-    jetstream: {
-      flash: Flash;
-    };
-  };
-
-  export type InertiaProps = {
-    pagePermissions: {
-      canAdmin?: true;
-      canEditSettings?: true;
-    };
-    shiftAvailability: {
-      timezone: string;
-      duration: number;
-      period: App.Enums.DBPeriod;
-      releasedDaily: boolean;
-      weekDayRelease: "SUN" | "MON" | "TUE" | "WED" | "THU" | "FRI" | "SAT";
-      systemShiftStartHour: number;
-      systemShiftEndHour: number;
-    };
-    hasUpdate?: boolean;
-    enableUserAvailability?: boolean;
-    needsToUpdateAvailability?: boolean;
-    enableUserLocationChoices?: boolean;
-    isUnrestricted?: true;
-    user: AuthUser;
-  };
-
-  export declare function usePage<SharedProps extends PageProps & InertiaProps & FlashMessage>(): Page<SharedProps>;
+  export declare function usePage<T extends AppPageProps>(): Page<T>;
 }
