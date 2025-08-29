25ff7bf5baf15ede8a252faabe726c52ef50e6bb
Ian Heggaton <ian@pixelated.com.au>
2025-08-29 14:09:49 +1000

    Refactor Add.vue to use TypeScript and improve data structure
    - Update script setup to use TypeScript.
    - Define type for `newLocation` data.

--- resources/js/Pages/Admin/Locations/Add.vue
+++ resources/js/Pages/Admin/Locations/Add.vue
@@ -1,27 +1,23 @@
-<script setup>
+<script setup lang="ts">
 import { router } from "@inertiajs/vue3";
 import AppLayout from "@/Layouts/AppLayout.vue";
 import LocationForm from "@/Pages/Admin/Locations/Partials/LocationForm.vue";
 
-defineProps({
-  maxVolunteers: {
-    type: Number,
-  },
-});
+defineProps<{
+  maxVolunteers: number;
+}>();
 
 const listRouteAction = () => {
   router.visit(route("admin.locations.index"));
 };
-const newLocation = {
-  data: {
-    name: "",
-    description: "",
-    min_volunteers: 2,
-    max_volunteers: 3,
-    requires_brother: true,
-    is_enabled: true,
-    shifts: [],
-  },
+const newLocation: App.Data.LocationAdminData = {
+  name: "",
+  description: "",
+  min_volunteers: 2,
+  max_volunteers: 3,
+  requires_brother: true,
+  is_enabled: true,
+  shifts: [],
 };
 </script>
 
@@ -38,7 +34,7 @@ const newLocation = {
 
     <div>
       <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-        <LocationForm :location="newLocation" action="add" :max-volunteers="maxVolunteers"/>
+        <LocationForm :location="newLocation" action="add" :max-volunteers="maxVolunteers" />
       </div>
     </div>
   </AppLayout>
