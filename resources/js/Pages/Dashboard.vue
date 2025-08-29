f8a1ba1d334715f3e59713d3e51c0c6b4c6b1b4c
Ian Heggaton <ian@pixelated.com.au>
2025-08-29 13:34:31 +1000

    Update Dashboard component for type safety and minor adjustments
    - Add type definition for user prop.
    - Add language attribute to the script tag for TypeScript support.

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,46 +1,47 @@
-<script setup>
+<script setup lang="ts">
 import { computed, ref } from "vue";
 import AppLayout from "@/Layouts/AppLayout.vue";
 import CartReservation from "@/Pages/Components/Dashboard/CartReservation.vue";
 import ReportsModal from "@/Pages/Components/Dashboard/ReportsModal.vue";
+import { AuthUser } from "@/shims";
 
-defineProps({
-    user: Object,
-});
+defineProps<{
+  user: AuthUser;
+}>();
 
 const outstandingReportCount = ref(0);
 
 const reportsLabel = computed(() =>
-    outstandingReportCount.value === 1 ? "Report" : "Reports");
+  outstandingReportCount.value === 1 ? "Report" : "Reports");
 
 const showReportsModal = ref(false);
 </script>
 
 <template>
-<AppLayout title="Dashboard">
-  <template #header>
-    <div class="flex flex-col justify-between w-full sm:flex-row">
-      <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">Dashboard</h2>
-      <div>
-        <PButton v-if="outstandingReportCount"
-                 icon="iconify mdi--bell"
-                 :label="`${ outstandingReportCount } ${ reportsLabel } Outstanding`"
-                 type="button"
-                 severity="warn"
-                 class="mt-3 w-full font-bold sm:mt-0 sm:w-auto"
-                 @click="showReportsModal = true"/>
+  <AppLayout title="Dashboard">
+    <template #header>
+      <div class="flex flex-col justify-between w-full sm:flex-row">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">Dashboard</h2>
+        <div>
+          <PButton v-if="outstandingReportCount"
+                   icon="iconify mdi--bell"
+                   :label="`${ outstandingReportCount } ${ reportsLabel } Outstanding`"
+                   type="button"
+                   severity="warn"
+                   class="mt-3 w-full font-bold sm:mt-0 sm:w-auto"
+                   @click="showReportsModal = true"/>
+        </div>
       </div>
-    </div>
-  </template>
+    </template>
 
-  <div class="py-2 dashboard">
-    <div class="mx-auto max-w-7xl">
-      <div class="overflow-hidden bg-panel dark:bg-panel-dark sm:rounded-lg">
-        <CartReservation :user="user"/>
+    <div class="py-2 dashboard">
+      <div class="mx-auto max-w-7xl">
+        <div class="overflow-hidden bg-panel dark:bg-panel-dark sm:rounded-lg">
+          <CartReservation :user="user"/>
+        </div>
       </div>
     </div>
-  </div>
-</AppLayout>
-<ReportsModal v-model="showReportsModal"
-              @has-outstanding-reports="outstandingReportCount = $event"/>
+  </AppLayout>
+  <ReportsModal v-model="showReportsModal"
+                @has-outstanding-reports="outstandingReportCount = $event"/>
 </template>
