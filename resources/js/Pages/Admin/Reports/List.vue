0dd9eedb85fa2dc831127f02e95109c87b4119cf
Ian Heggaton <ian@pixelated.com.au>
2025-08-29 13:31:54 +1000

    Update Reports list component with type safety and location filtering
    - Update script setup tag to include 'lang="ts"' for TypeScript support.
    - Modify props definition to explicitly define the types for 'reports' and 'locations'.
    - Update v-for loop in the location filter to iterate over `locations` instead of `locations.data`.

--- resources/js/Pages/Admin/Reports/List.vue
+++ resources/js/Pages/Admin/Reports/List.vue
@@ -1,135 +1,135 @@
-<script setup>
+<script setup lang="ts">
 import { computed, ref } from "vue";
-import headers from "./Lib/ReportsDataTableHeaders.js";
 import DataTable from "@/Components/DataTable.vue";
 import Filter from "@/Components/Icons/Filter.vue";
 import JetHelpText from "@/Jetstream/HelpText.vue";
 import JetInput from "@/Jetstream/Input.vue";
 import JetLabel from "@/Jetstream/Label.vue";
 import AppLayout from "@/Layouts/AppLayout.vue";
+import headers from "./Lib/ReportsDataTableHeaders.js";
 
-const props = defineProps({
-    reports: Object,
-    locations: Object,
-});
+const props = defineProps<{
+  reports: Array<App.Data.ReportsData>;
+  locations: Array<App.Data.LocationAdminData>;
+}>();
 
 const userSearch = ref("");
 
-const reportData = computed(() => props.reports.data.map((report) => ({
-    id: report.id,
-    location: report.metadata?.location_name || report.shift?.location?.name || "",
-    locationId: report.metadata?.locationId || report.shift?.location?.id || "",
-    userExists: !!report.submitted_by?.id,
-    submittedByName: report.metadata?.submitted_by_name || report.submitted_by?.name || "",
-    submittedByPhone: report.metadata?.submitted_by_phone || report.submitted_by?.mobile_phone || "",
-    submittedByEmail: report.metadata?.submitted_by_email || report.submitted_by?.email || "",
-    date: report.shift_date,
-    time: report.shift?.start_time,
-    placements: report.placements_count,
-    videos: report.videos_count,
-    requests: report.requests_count,
-    comments: report.comments,
-    tags: report.tags.map((tag) => tag.name.en),
-    cancelled: report.shift_was_cancelled,
-    associates: report.metadata?.associates || [],
+const reportData = computed(() => props.reports.map((report) => ({
+  id: report.id,
+  location: report.metadata?.location_name || report.shift?.location?.name || "",
+  locationId: report.metadata?.location_id || report.shift?.location?.id || "",
+  userExists: !!report.submitted_by?.id,
+  submittedByName: report.metadata?.submitted_by_name || report.submitted_by?.name || "",
+  submittedByPhone: report.metadata?.submitted_by_phone || report.submitted_by?.mobile_phone || "",
+  submittedByEmail: report.metadata?.submitted_by_email || report.submitted_by?.email || "",
+  date: report.shift_date,
+  time: report.shift?.start_time,
+  placements: report.placements_count,
+  videos: report.videos_count,
+  requests: report.requests_count,
+  comments: report.comments,
+  tags: report.tags.map((tag) => tag.name.en),
+  cancelled: report.shift_was_cancelled,
+  associates: report.metadata?.associates || [],
 })));
 
 const showLocationFilter = ref(false);
 const locationFilter = ref(0);
 
 const filter = computed(() => {
-    const filters = [];
-    if (locationFilter.value) {
-        filters.push({ field: "locationId", comparison: "=", criteria: locationFilter.value });
-    }
-    return filters;
+  const filters = [];
+  if (locationFilter.value) {
+    filters.push({ field: "locationId", comparison: "=", criteria: locationFilter.value });
+  }
+  return filters;
 });
 </script>
 
 <template>
-<AppLayout title="Reports">
-  <template #header>
-    <div>
-      <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Reports</h2>
-    </div>
-  </template>
+  <AppLayout title="Reports">
+    <template #header>
+      <div>
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Reports</h2>
+      </div>
+    </template>
 
-  <div class="flex flex-col gap-4">
-    <div class="bg-panel dark:bg-panel-dark border border-neutral-300/75 dark:border-neutral-800 sm:rounded-lg sm:p-6">
-      <JetLabel for="search" value="Search on Reports"/>
-      <JetInput id="search" v-model="userSearch" type="text" class="mt-1 block w-full"/>
-      <JetHelpText>Search on location, user name, date, etc</JetHelpText>
-    </div>
+    <div class="flex flex-col gap-4">
+      <div class="bg-panel dark:bg-panel-dark border border-neutral-300/75 dark:border-neutral-800 sm:rounded-lg sm:p-6">
+        <JetLabel for="search" value="Search on Reports"/>
+        <JetInput id="search" v-model="userSearch" type="text" class="mt-1 block w-full"/>
+        <JetHelpText>Search on location, user name, date, etc</JetHelpText>
+      </div>
 
-    <div class="bg-panel dark:bg-panel-dark border border-neutral-300/75 dark:border-neutral-800 sm:rounded-lg sm:p-6">
-      <data-table :headers="headers"
-                  :items="reportData"
-                  :search-value="userSearch"
-                  :filter-options="filter"
-                  :show-hover="false">
-        <template #header-location="{ text }">
-          <div class="relative flex items-center">
-            <div class="p-2 mr-2 cursor-pointer inline-block hover:bg-gray-200 hover:bg-opacity-50 rounded"
-                 @click.stop="showLocationFilter = !showLocationFilter">
-              <Filter/>
-            </div>
-            {{ text }}
-            <div class="p-2 z-50 absolute top-12 w-64 bg-white border border-gray-300 rounded-md shadow-lg"
-                 v-if="showLocationFilter">
-              <select class="location-selector w-full rounded-md"
-                      v-model="locationFilter"
-                      name="location">
-                <option value="" selected>All</option>
-                <option v-for="location in locations.data" :key="location.id" :value="location.id">
-                  {{ location.name }}
-                </option>
-              </select>
-            </div>
-          </div>
-        </template>
-
-        <template #item-cancelled="{ cancelled }">
-          {{ cancelled ? 'Yes' : 'No' }}
-        </template>
-
-        <template
-            #expand="{ comments, tags, submittedByName, submittedByEmail, submittedByPhone, associates }">
-          <div class="bg-gray-100 dark:bg-gray-800 p-3 rounded-lg grid grid-cols-3">
-            <ul v-if="tags?.length" class="col-span-3 p-0 m-0 mb-3">
-              <li v-for="(tag, i) in tags"
-                  :key="i"
-                  class="inline-flex px-2 bg-green-200 dark:bg-green-800 mr-1 mb-1 rounded-full dark:text-gray-50">
-                {{ tag }}
-              </li>
-            </ul>
-            <div class="">
-              <h5>Comments</h5>
-              <div v-if="comments">{{ comments }}</div>
-              <div v-else class="text-gray-500 dark:text-gray-300"><em>None entered</em></div>
-            </div>
-            <div>
-              <h5>Submitted by: {{ submittedByName }}<br></h5>
-              Tel: <a :href="`tel:${submittedByPhone}`">{{ submittedByPhone }}</a><br>
-              Email: <a :href="`mailto:${submittedByEmail}`">{{ submittedByEmail }}</a>
-            </div>
-            <div>
-              <h5>Associates</h5>
-              <div v-if="associates?.length">
-                <ul class="p-0 m-0">
-                  <li v-for="(associate, i) in associates"
-                      :key="i"
-                      class="inline-flex px-2 bg-green-200 dark:bg-purple-800 mr-1 mb-1 rounded-full dark:text-gray-50">
-                    {{ associate.name }}
-                  </li>
-                </ul>
+      <div class="bg-panel dark:bg-panel-dark border border-neutral-300/75 dark:border-neutral-800 sm:rounded-lg sm:p-6">
+        <data-table :headers="headers"
+                    :items="reportData"
+                    :search-value="userSearch"
+                    :filter-options="filter"
+                    :show-hover="false">
+          <template #header-location="{ text }">
+            <div class="relative flex items-center">
+              <div class="p-2 mr-2 cursor-pointer inline-block hover:bg-gray-200 hover:bg-opacity-50 rounded"
+                   @click.stop="showLocationFilter = !showLocationFilter">
+                <Filter/>
+              </div>
+              {{ text }}
+              <div class="p-2 z-50 absolute top-12 w-64 bg-white border border-gray-300 rounded-md shadow-lg"
+                   v-if="showLocationFilter">
+                <select class="location-selector w-full rounded-md"
+                        v-model="locationFilter"
+                        name="location">
+                  <option value="" selected>All</option>
+                  <option v-for="location in locations" :key="location.id" :value="location.id">
+                    {{ location.name }}
+                  </option>
+                </select>
               </div>
             </div>
-          </div>
-        </template>
-      </data-table>
+          </template>
+
+          <template #item-cancelled="{ cancelled }">
+            {{ cancelled ? 'Yes' : 'No' }}
+          </template>
+
+          <template
+              #expand="{ comments, tags, submittedByName, submittedByEmail, submittedByPhone, associates }">
+            <div class="bg-gray-100 dark:bg-gray-800 p-3 rounded-lg grid grid-cols-3">
+              <ul v-if="tags?.length" class="col-span-3 p-0 m-0 mb-3">
+                <li v-for="(tag, i) in tags"
+                    :key="i"
+                    class="inline-flex px-2 bg-green-200 dark:bg-green-800 mr-1 mb-1 rounded-full dark:text-gray-50">
+                  {{ tag }}
+                </li>
+              </ul>
+              <div class="">
+                <h5>Comments</h5>
+                <div v-if="comments">{{ comments }}</div>
+                <div v-else class="text-gray-500 dark:text-gray-300"><em>None entered</em></div>
+              </div>
+              <div>
+                <h5>Submitted by: {{ submittedByName }}<br></h5>
+                Tel: <a :href="`tel:${submittedByPhone}`">{{ submittedByPhone }}</a><br>
+                Email: <a :href="`mailto:${submittedByEmail}`">{{ submittedByEmail }}</a>
+              </div>
+              <div>
+                <h5>Associates</h5>
+                <div v-if="associates?.length">
+                  <ul class="p-0 m-0">
+                    <li v-for="(associate, i) in associates"
+                        :key="i"
+                        class="inline-flex px-2 bg-green-200 dark:bg-purple-800 mr-1 mb-1 rounded-full dark:text-gray-50">
+                      {{ associate.name }}
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </template>
+        </data-table>
+      </div>
     </div>
-  </div>
-</AppLayout>
+  </AppLayout>
 </template>
 
 <style lang="scss">
