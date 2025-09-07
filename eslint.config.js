d85ffdb06685fe91ae706009a0c98ee13bc54002
Ian Heggaton <ian@pixelated.com.au>
2025-09-08 08:32:43 +1000

    Configure ESLint rules for unused variables and testing files. - Disables `@typescript-eslint/no-unused-vars` rule in the main configuration. - Adds a new ESLint configuration specifically for test files.

--- eslint.config.js
+++ eslint.config.js
@@ -84,7 +84,6 @@ export default defineConfigWithVueTs([
       "@typescript-eslint/no-floating-promises": ["warn", { ignoreVoid: true }],
       "@typescript-eslint/no-import-type-side-effects": "warn",
       "@typescript-eslint/no-unused-expressions": ["warn", { allowShortCircuit: true, allowTernary: true }],
-      "@typescript-eslint/no-unused-vars": "warn",
       "unused-imports/no-unused-imports": "error",
       "unused-imports/no-unused-vars": [
         "warn",
@@ -96,6 +95,18 @@ export default defineConfigWithVueTs([
         },
       ],
     },
+  },
+  {
+    name: "eslint-ts-testing",
+    files: ["./resources/js/**/*.test.ts"],
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "separate-type-imports",
+        disallowTypeAnnotations: false,
+      },
+    ],
   },
   {
     name: "eslint-import-x",
