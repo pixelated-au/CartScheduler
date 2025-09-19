5ff50bf264f4844cb132d5658c584e9fc688fc36
Ian Heggaton <ian@pixelated.com.au>
2025-09-19 16:15:15 +1000

    fix(useDarkMode): Refactor dark mode composable logic
    - Update `useDarkMode` composable to support explicit mode setting and system preference.
    - Introduce `getMode` helper function for determining the next mode.
    - Implement `startViewTransition` for smoother mode changes.
    - Expose `colorMode` and `system` refs for enhanced control.

--- resources/js/Composables/useDarkMode.ts
+++ resources/js/Composables/useDarkMode.ts
@@ -1,25 +1,43 @@
 import { useColorMode } from "@vueuse/core";
 import { computed, nextTick } from "vue";
+import type { ComputedRef, Ref } from "vue";
+
+export type ColorMode = "dark" | "light" | "auto";
+
+function getMode(system: ComputedRef<ColorMode>, store: Ref<ColorMode>) {
+  let mode: ColorMode = "auto";
+
+  if (system.value === "dark") {
+    if (store.value === "auto") {
+      mode = "light";
+    } else if (store.value === "light") {
+      mode = "dark";
+    }
+  } else if (system.value === "light") {
+    if (store.value === "auto") {
+      mode = "dark";
+    } else if (store.value === "dark") {
+      mode = "light";
+    }
+  }
+  return mode;
+}
 
 /**
- * A composable for managing dark mode state in a Vue application
- * @returns Object containing dark mode state and toggle function
+ * Note, this uses the VueUse function useColorMode instead of useDark because it supports 'auto' mode
  */
 export function useDarkMode() {
   const { system, store } = useColorMode();
-  const colorMode = computed(() => store.value === "auto" ? system.value : store.value);
-  const isDarkMode = computed(() => colorMode.value === "dark");
+  const isDarkMode = computed(() => store.value === "auto" ? system.value === "dark" : store.value === "dark");
 
-  const setMode = async (mode?: "dark" | "light" | "auto") => {
+  const setMode = async (mode: ColorMode) => {
     await nextTick();
-    if (mode) {
-      store.value = mode;
-      return;
-    }
-    store.value = isDarkMode.value ? "light" : "dark";
+    store.value = mode;
   };
 
-  const toggleDarkMode = async (mode?: "light" | "dark") => {
+  const toggleDarkMode = async () => {
+    const mode = getMode(system, store);
+
     if (!document.startViewTransition) {
       await setMode(mode);
       return;
@@ -33,6 +51,9 @@ export function useDarkMode() {
 
   return {
     isDarkMode,
+    colorMode: store,
+    system,
+    setMode,
     toggleDarkMode,
   };
 }
