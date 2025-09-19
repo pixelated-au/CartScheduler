864936f453ad87ff166a7ec6e67c467b0dc77ad8
Ian Heggaton <ian@pixelated.com.au>
2025-09-19 16:27:34 +1000

    test(composables): Refactor useDarkMode tests and update functionality
    - Enhance tests for `useDarkMode` composable with added scenarios and assertions.
    - Improve handling of browser view transition feature within the composable tests.
    - Adjust toggling logic for light, auto, and dark modes based on system defaults.

--- resources/js/__tests__/composables/useDarkMode.test.ts
+++ resources/js/__tests__/composables/useDarkMode.test.ts
@@ -1,21 +1,26 @@
 import { beforeEach, describe, expect, it, vi } from "vitest";
 import { ref } from "vue";
 import { useDarkMode } from "@/Composables/useDarkMode";
+import type { ColorMode } from "@/Composables/useDarkMode";
 
-vi.mock("@vueuse/core", () => {
-  return ({
-    useColorMode: () => ({
-      store: ref("light"),
-      system: ref("light"),
-    }),
-  });
-});
+const useColorMode = vi.hoisted(() => vi.fn());
+
+vi.mock("@vueuse/core", () => ({ useColorMode }));
 
 type Callback = ((callbackOptions?: ViewTransitionUpdateCallback | undefined) => ViewTransition) | undefined;
 
 const viewTransitionFunction: { value: Callback; writable: boolean } = {
   value: undefined,
   writable: true,
+};
+
+const checker = (udm: ReturnType<typeof useDarkMode>, system: Exclude<ColorMode, "auto">) => (colorMode: ColorMode) => {
+  const isDarkMode = colorMode === "auto" ? system === "dark" : colorMode === "dark";
+
+  expect(udm.isDarkMode.value).toBe(isDarkMode);
+  expect(udm.system.value).toBe(system);
+  expect(udm.colorMode.value).toBe(colorMode);
+
 };
 
 describe("useDarkMode", () => {
@@ -25,26 +30,66 @@ describe("useDarkMode", () => {
     vi.clearAllMocks();
   });
 
-  it("initializes with light mode by default", () => {
-    const { isDarkMode } = useDarkMode();
-    expect(isDarkMode.value).toBe(false);
-  });
+  it("handles browser viewTransition feature", async () => {
+    useColorMode.mockReturnValue({
+      store: ref("auto"),
+      system: ref("light"),
+    });
 
-  it("handles viewTransition", async () => {
-    document.startViewTransition = (callback: (callbackOptions?: ViewTransitionUpdateCallback | undefined) => ViewTransition) => callback();
+    document.startViewTransition = vi.fn((callback: (callbackOptions?: ViewTransitionUpdateCallback | undefined) => ViewTransition) => callback());
+    const svtSpy = vi.spyOn(document, "startViewTransition");
 
-    const { isDarkMode, toggleDarkMode } = useDarkMode();
-    expect(isDarkMode.value).toBe(false);
-    await toggleDarkMode("dark");
-    expect(isDarkMode.value).toBe(true);
-  });
+    const { toggleDarkMode } = useDarkMode();
+    expect(svtSpy).not.toHaveBeenCalled();
 
-  it("toggles between light and dark mode", async () => {
-    const { isDarkMode, toggleDarkMode } = useDarkMode();
-    expect(isDarkMode.value).toBe(false);
-    await toggleDarkMode("dark");
-    expect(isDarkMode.value).toBe(true);
     await toggleDarkMode();
-    expect(isDarkMode.value).toBe(false);
+    expect(svtSpy).toHaveBeenCalledOnce();
+
+    await toggleDarkMode();
+    expect(svtSpy).toHaveBeenCalledTimes(2);
+  });
+
+  it("toggles between light, auto and dark modes when device default is 'light'", async () => {
+    useColorMode.mockReturnValue({
+      store: ref("light"),
+      system: ref("light"),
+    });
+
+    const udm = useDarkMode();
+    const check = checker(udm, "light");
+    const { toggleDarkMode } = udm;
+
+    check("light");
+
+    await toggleDarkMode();
+    check("auto");
+
+    await toggleDarkMode();
+    check("dark");
+
+    await toggleDarkMode();
+    check("light");
+
+    await toggleDarkMode();
+    check("auto");
+  });
+
+  it("toggles between dark, auto and light modes when device default is 'dark'", async () => {
+    useColorMode.mockReturnValue({
+      store: ref("dark"),
+      system: ref("dark"),
+    });
+
+    const udm = useDarkMode();
+    const check = checker(udm, "dark");
+    const { toggleDarkMode } = udm;
+
+    check("dark");
+
+    await toggleDarkMode();
+    check("auto");
+
+    await toggleDarkMode();
+    check("light");
   });
 });
