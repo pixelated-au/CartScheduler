9813679a654157b177e393ba470dfc18d734d817
Ian Heggaton <ian@pixelated.com.au>
2025-09-21 06:24:41 +1000

    fix(store): Refactor store to use enum for labels
    - Define an enum for filter labels
    - Update LocalStore type and default values to use the new enum

--- resources/js/store.ts
+++ resources/js/store.ts
@@ -1,18 +1,39 @@
 import { createGlobalState, useStorage } from "@vueuse/core";
 
-export type LocalStore = typeof defaults;
+enum Labels {
+  Gender = "Gender",
+  Appointment = "Appointment",
+  ServingAs = "Serving As",
+  MaritalStatus = "Marital Status",
+  BirthYear = "Birth Year",
+  ResponsibleBrother = "Is Responsible Bro?",
+  MobilePhone = "Phone",
+}
 
-const defaults = {
+export type LocalStore = {
+  dismissedAvailabilityOn?: Date;
+  columnFilters: {
+    gender: { label: Labels.Gender; value: boolean };
+    appointment: { label: Labels.Appointment; value: boolean };
+    servingAs: { label: Labels.ServingAs; value: boolean };
+    maritalStatus: { label: Labels.MaritalStatus; value: boolean };
+    birthYear: { label: Labels.BirthYear; value: boolean };
+    responsibleBrother: { label: Labels.ResponsibleBrother; value: boolean };
+    mobilePhone: { label: Labels.MobilePhone; value: boolean };
+  };
+};
+
+const defaults: LocalStore = {
   dismissedAvailabilityOn: undefined,
   // used to filter admin volunteer rostering table
   columnFilters: {
-    gender: { label: "Gender", value: false },
-    appointment: { label: "Appointment", value: false },
-    servingAs: { label: "Serving As", value: false },
-    maritalStatus: { label: "Marital Status", value: false },
-    birthYear: { label: "Birth Year", value: false },
-    responsibleBrother: { label: "Is Responsible Bro?", value: false },
-    mobilePhone: { label: "Phone", value: false },
+    gender: { label: Labels.Gender, value: false },
+    appointment: { label: Labels.Appointment, value: false },
+    servingAs: { label: Labels.ServingAs, value: false },
+    maritalStatus: { label: Labels.MaritalStatus, value: false },
+    birthYear: { label: Labels.BirthYear, value: false },
+    responsibleBrother: { label: Labels.ResponsibleBrother, value: false },
+    mobilePhone: { label: Labels.MobilePhone, value: false },
   },
 };
 
