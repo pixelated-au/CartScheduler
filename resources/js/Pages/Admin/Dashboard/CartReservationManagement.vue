498b7547be3624a55f9d40ebfc324e75cd7395a4
Ian Heggaton <ian@pixelated.com.au>
2025-09-21 06:52:10 +1000

    fix(dashboard): Update styling for cart reservation accordion
    - Modify CartReservationManagement.vue to adjust accordion styling.

--- resources/js/Pages/Admin/Dashboard/CartReservationManagement.vue
+++ resources/js/Pages/Admin/Dashboard/CartReservationManagement.vue
@@ -242,7 +242,7 @@ onMounted(() => {
       </ComponentSpinner>
     </div>
     <ComponentSpinner :show="isLoading" class="min-h-[200px] sm:min-h-full">
-      <PAccordion v-if="!isLoading" v-model:value="accordionExpandIndex" class="rounded border border-b-0 border-std">
+      <PAccordion v-if="!isLoading" v-model:value="accordionExpandIndex" class="rounded border std-border">
         <PAccordionPanel v-for="location in locations" :key="location.id" :value="location.id" class="group">
           <PAccordionHeader class="relative after:absolute after:bottom-2 after:left-0 after:right-0 group-[.p-accordionpanel-active]:after:block after:h-px after:hidden after:bg-gradient-to-r after:from-transparent after:from-20% after:via-surface-500/70 after:to-transparent after:to-80%">
             <div class="flex items-center dark:text-gray-200">
