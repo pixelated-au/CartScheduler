58d926a26edeffd14acd466f352776e641336398
Ian Heggaton <ian@pixelated.com.au>
2025-09-22 10:22:45 +1000

    refactor(DarkMode): Refactor toggle button and labels
    - Update DarkMode.vue to use toggleDarkMode function
    - Rework the button structure and integrate DarkModeLabel component

--- resources/js/Layouts/Components/DarkMode.vue
+++ resources/js/Layouts/Components/DarkMode.vue
@@ -3,75 +3,36 @@ import { useElementBounding } from "@vueuse/core";
 import { computed, useTemplateRef, watch } from "vue";
 import { useDarkMode } from "@/Composables/useDarkMode.js";
 
-const { colorMode, setMode } = useDarkMode();
+const { colorMode, toggleDarkMode } = useDarkMode();
 
 const el = useTemplateRef("themeToggle");
-const op = useTemplateRef("op");
+
 const { top, left, width, height } = useElementBounding(el);
 const clip = computed(() => `circle(0% at ${left.value + (width.value / 2)}px ${top.value + (height.value / 2)}px)`);
 
 watch(clip, (val) => {
   document.documentElement.style.setProperty("--clip", val);
 });
-
-watch(colorMode, (val) => {
-  console.log("COLOR MODE:", val);
-});
-
-const toggle = (event: Event) => {
-  op.value?.toggle(event);
-};
-
-const menu = [
-  { label: "Dark", icon: "iconify mdi--moon-and-stars", command: () => setMode("dark") },
-  { label: "Light", icon: "iconify mdi--weather-sunny", command: () => setMode("light") },
-  { label: "System", icon: "iconify mdi--sun-moon-stars", command: () => setMode("auto") },
-];
 </script>
 
 <template>
   <button id="theme-toggle"
           ref="themeToggle"
           type="button"
-          class="flex items-center justify-center hover:bg-neutral-100 dark:hover:bg-neutral-700 focus:outline-none focus:ring-1 focus:ring-neutral-300 dark:focus:ring-neutral-700 rounded-md p-2.5"
-          @click="toggle">
-    <span v-if="colorMode === 'dark'" class="iconify mdi--moon-and-stars" />
-    <span v-else-if="colorMode === 'light'" class="iconify mdi--weather-sunny" />
-    <span v-else class="iconify mdi--sun-moon-stars text-neutral-400 dark:text-neutral-500" />
+          class="hover:bg-neutral-100 dark:hover:bg-neutral-700 focus:outline-none
+          focus:ring-1 focus:ring-neutral-300 dark:focus:ring-neutral-700 rounded-full p-2 dark:bg-panel-dark"
+          aria-label="Toggle dark mode"
+          @click="toggleDarkMode()">
+    <DarkModeLabel :show="colorMode === 'dark'" label="dark theme" icon="iconify mdi--moon-and-stars" />
+    <DarkModeLabel :show="colorMode === 'light'" label="light theme" icon="iconify mdi--weather-sunny" />
+    <DarkModeLabel :show="colorMode === 'auto'"
+                   label="system theme"
+                   icon="iconify mdi--sun-moon-stars"
+                   class="text-neutral-400 dark:text-neutral-400" />
   </button>
-
-  <PMenu ref="op" :model="menu" class="min-w-10" popup>
-    <template #item="{ item }">
-      <div class="flex items-center gap-2 py-1 px-3">
-        <span :class="item.icon"/>
-        <span class="label">{{item.label}}</span>
-      </div>
-    </template>
-  </PMenu>
 </template>
 
 <style lang="css">
-@media (prefers-reduced-motion: no-preference) {
-::view-transition-old(root) {
-    animation-delay: 500ms;
-}
-
-::view-transition-new(root) {
-    animation: circle-in 500ms;
-}
-}
-
-@keyframes circle-in {
-    from {
-        transform: translate3d(0, 0, 0); /* To improve performance on clip-path animation */
-        /*noinspection CssUnresolvedCustomProperty*/
-        clip-path: var(--clip);
-    }
-    to {
-        clip-path: circle(120% at 50% 0%);
-    }
-}
-
 :root {
     @media (prefers-color-scheme: light) {
         color-scheme: light;
