1e1db874eb43f2179b00380de82718d4d9e1d27f
Ian Heggaton <ian@pixelated.com.au>
2025-09-24 10:05:29 +1000

    feat(Layouts): Add NavDesktopMain component
    - Add NavDesktopMain.vue to display desktop navigation items.

---
+++ resources/js/Layouts/Components/NavDesktopMain.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import useMenuItems from "./Composables/useMenuItems";
+
+const { hasAdminMenu, mainMenuItems } = useMenuItems();
+</script>
+
+<template>
+  <div class="flex items-center sm:ml-6 sm:space-x-4">
+    <template v-for="item in mainMenuItems" :key="'desktop-main-' + item.label">
+      <NavSubmenu v-if="hasAdminMenu && item.isDropdown" :item="item" position="start" />
+      <NavMenuItem v-if="!item.isDropdown" :item="item" />
+    </template>
+  </div>
+</template>
