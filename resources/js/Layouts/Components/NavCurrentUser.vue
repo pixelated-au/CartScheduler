4ee8b142c036cd5162302480ca31ad154353659a
Ian Heggaton <ian@pixelated.com.au>
2025-09-24 10:03:17 +1000

    feat(Layouts): Add NavCurrentUser user navigation component

---
+++ resources/js/Layouts/Components/NavCurrentUser.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { usePage } from "@inertiajs/vue3";
+import useMenuItems from "./Composables/useMenuItems";
+
+const { userNavMenuItems } = useMenuItems();
+const page = usePage();
+</script>
+
+<template>
+  <NavSubmenu :items="userNavMenuItems" label="desktop-user-menu" position="end">
+    <template #button>
+      <img v-if="page.props.auth.user?.profile_photo_url"
+           class="w-8 h-8 rounded-full"
+           :src="page.props.auth.user.profile_photo_url"
+           :alt="page.props.auth.user?.name || 'User Avatar'">
+      <span v-else
+            class="inline-flex justify-center items-center w-8 h-8 rounded-full bg-neutral-300 dark:bg-neutral-600">
+        <span class="font-medium leading-none !text-current">
+          {{ page.props.auth.user?.name?.charAt(0) || "U" }}
+        </span>
+      </span>
+    </template>
+  </NavSubmenu>
+</template>
