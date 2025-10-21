6f3950aa630bb6782eea7ee7dbe0bb1288eb058b
Ian Heggaton <ian@pixelated.com.au>
2025-10-22 08:56:28 +1100

    reverted back to undefined from null

--- resources/js/Utils/dateStringToDateObject.ts
+++ resources/js/Utils/dateStringToDateObject.ts
@@ -4,19 +4,16 @@ import type { WritableComputedRef, Ref } from "vue";
 
 type DateKey = keyof Pick<App.Data.ShiftAdminData, "available_from" | "available_to">;
 
-export default function(shift: Ref<App.Data.ShiftAdminData>, dateKey: DateKey): WritableComputedRef<Date | null> {
-  return computed<Date | null>({
+export default function(shift: Ref<App.Data.ShiftAdminData>, dateKey: DateKey): WritableComputedRef<Date | undefined> {
+  return computed<Date | undefined>({
     get: () => {
-      console.log("Getting date", shift.value[dateKey]);
       if (!shift.value[dateKey]) {
-        return null;
+        return undefined;
       }
       return parse(shift.value[dateKey], "yyyy-MM-dd", new Date());
     },
-    set: (value: Date | null) => {
-      console.log("Setting date", value);
-      shift.value[dateKey] = (value ? format(value, "yyyy-MM-dd") : null);
+    set: (value: Date | undefined) => {
+      shift.value[dateKey] = (value ? format(value, "yyyy-MM-dd") : undefined);
     },
-    // set: (value: Date | null) => shift = (value ? format(value, "yyyy-MM-dd") : null),
   });
 }
