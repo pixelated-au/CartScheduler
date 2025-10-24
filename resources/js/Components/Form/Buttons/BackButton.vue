e8d21390925299252040cea10f3a74c7122d4e96
Ian Heggaton <ian@pixelated.com.au>
2025-10-25 08:23:56 +1100

    feat(BackButton): add configurable button type prop
    - Add type prop with default value to BackButton component

--- resources/js/Components/Form/Buttons/BackButton.vue
+++ resources/js/Components/Form/Buttons/BackButton.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+const { type="button" } = defineProps<{
+  type?: "button" | "submit" | "reset";
+}>();
+</script>
+
 <template>
   <PButton v-bind="$attrs"
+           :type
            severity="info"
            label="Back"
            icon="iconify mdi--arrow-left-circle-outline"
