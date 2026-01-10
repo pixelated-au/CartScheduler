1cd626985d76fd933d224efb51d1bf3887bb4a0b
Ian Heggaton <ian@pixelated.com.au>
2026-01-11 09:33:14 +1100

    feat(AppServiceProvider): Add flexibleWithEnum macro to Cache
    - Implement temporary Cache::flexibleWithEnum macro to handle backed enums as keys

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,8 +7,10 @@ use App\Http\Controllers\SetUserPasswordController;
 use App\Interfaces\ObfuscatedErrorCode;
 use App\Listeners\StreamlineInstalledVersionSetListener;
 use App\Listeners\StreamlineNextAvailableVersionUpdatedListener;
+use BackedEnum;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\URL;
@@ -22,9 +24,34 @@ class AppServiceProvider extends ServiceProvider
     {
     }
 
-
+    /** @noinspection StaticClosureCanBeUsedInspection */
     public function boot(): void
     {
+        Cache::macro('flexibleWithEnum',
+            /**
+             * TODO Delete this after Laravel fixes 'flexible' to accept enums as keys.
+             *  This will happen after Laravel v12:46
+             *
+             * Retrieve an item from the cache by key, refreshing it in the background if it is stale.
+             *
+             * @template TCacheValue
+             *
+             * @param  BackedEnum  $key
+             * @param  array{ 0: \DateTimeInterface|\DateInterval|int, 1: \DateTimeInterface|\DateInterval|int }  $ttl
+             * @param  (callable(): TCacheValue)  $callback
+             * @param  array{ seconds?: int, owner?: string }|null  $lock
+             * @param  bool  $alwaysDefer
+             * @return TCacheValue
+             */
+            fn(
+                BackedEnum $key,
+                array $ttl,
+                callable $callback,
+                ?array $lock = null,
+                bool $alwaysDefer = false
+            ) => Cache::flexible($key->value, $ttl, $callback, $lock, $alwaysDefer)
+        );
+
         Model::preventLazyLoading(!app()->isProduction());
         Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
 
