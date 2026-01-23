c21c11457bc489e086c92c4028b10d32558ce09e
Ian Heggaton <ian@pixelated.com.au>
2026-01-23 12:07:03 +1100

    refactor(reports): Optimize GetOutstandingReports database query
    - Replaced raw SQL subquery with a JOIN clause for more efficient report identification
    - Centralized date filtering with a new `whereTodayOrBefore` clause

--- app/Actions/GetOutstandingReports.php
+++ app/Actions/GetOutstandingReports.php
@@ -5,7 +5,7 @@ namespace App\Actions;
 use App\Data\OutstandingReportsData;
 use App\Models\User;
 use Illuminate\Database\Query\Builder;
-use Illuminate\Support\Carbon;
+use Illuminate\Database\Query\JoinClause;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
@@ -17,35 +17,36 @@ class GetOutstandingReports
     public function execute(?User $user = null): Collection
     {
         $reports = DB::query()
-                 ->select([
-                     'su.shift_id',
-                     'su.shift_date',
-                     'shifts.start_time',
-                     'shifts.end_time',
-                     'locations.requires_brother',
-                     'locations.name as location_name',
-                 ])
-                 ->when($user !== null, fn(Builder $query) => $query->addSelect('su.user_id'))
-                 ->from('shift_user', 'su')
-                 ->leftJoin('shifts', 'shifts.id', '=', 'su.shift_id')
-                 ->leftJoin('locations', 'locations.id', '=', 'shifts.location_id')
-                 ->where('shift_date', '<=', Carbon::now()->format('Y-m-d'))
-                 ->when($user !== null, fn(Builder $query) => $query->where('user_id', $user->id))
-                 ->whereRaw('(SELECT COUNT(*)
-                    FROM reports r
-                    WHERE r.shift_id = su.shift_id
-                    AND r.shift_date = su.shift_date) = 0')
-                 ->when($user === null, fn(Builder $query) => $query->groupBy([
-                     'su.shift_id',
-                     'su.shift_date',
-                     'shifts.start_time',
-                     'shifts.end_time',
-                     'locations.requires_brother',
-                     'locations.name',
-                 ]))
-                 ->orderBy('shift_date')
-                 ->orderBy('shifts.start_time')
-                 ->get();
+            ->select([
+                'su.shift_id',
+                'su.shift_date',
+                'shifts.start_time',
+                'shifts.end_time',
+                'locations.requires_brother',
+                'locations.name as location_name',
+            ])
+            ->when($user !== null, fn(Builder $query) => $query->addSelect('su.user_id'))
+            ->from('shift_user', 'su')
+            ->leftJoin('shifts', 'shifts.id', '=', 'su.shift_id')
+            ->leftJoin('locations', 'locations.id', '=', 'shifts.location_id')
+            ->leftJoin('reports as r', fn(JoinClause $join) => $join
+                ->on('r.shift_id', '=', 'su.shift_id')
+                ->on('r.shift_date', '=', 'su.shift_date')
+            )
+            ->whereTodayOrBefore('su.shift_date')
+            ->whereNull('r.shift_id') // Makes sure there are no reports
+            ->when($user !== null, fn(Builder $query) => $query->where('user_id', $user->id))
+            ->when($user === null, fn(Builder $query) => $query->groupBy([
+                'su.shift_id',
+                'su.shift_date',
+                'shifts.start_time',
+                'shifts.end_time',
+                'locations.requires_brother',
+                'locations.name',
+            ]))
+            ->orderBy('su.shift_date')
+            ->orderBy('shifts.start_time')
+            ->get();
 
         return OutstandingReportsData::collect($reports);
     }
